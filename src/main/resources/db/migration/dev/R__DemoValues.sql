813c3211a1844971784c4371defad16a1bbd6fc0
Steven Grimm <steven@terraformation.com>
2021-01-08 13:43:03 -0800

    Fix demo values to work with current schema

--- src/main/resources/db/migration/dev/R__DemoValues.sql
+++ src/main/resources/db/migration/dev/R__DemoValues.sql
@@ -4,11 +4,11 @@ VALUES (1, 'Terraformation')
 ON CONFLICT DO NOTHING;
 
 -- This is an API key of "dummyKey"
-INSERT INTO api_key
+INSERT INTO api_key (hash, key_part, organization_id, created_time)
 VALUES ('f7b5ee554cbb700b597979ff26bbba58197e1427', 'du..ey', 1, NOW())
 ON CONFLICT DO NOTHING;
 
 -- A dummy site
-INSERT INTO site (organization_id, name, latitude, longitude, language, timezone)
+INSERT INTO site (organization_id, name, latitude, longitude, locale, timezone)
 VALUES (1, 'Demo Site', 123.456789, -98.76543, 'en-US', 'US/Pacific')
 ON CONFLICT DO NOTHING;
