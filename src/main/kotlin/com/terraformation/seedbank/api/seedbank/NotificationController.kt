006ad114d98c8604ff84b1fe60fd9ba9721c025f
Steven Grimm <steven@terraformation.com>
2021-01-22 12:30:47 -0800

    Proposed notification listing API
    A possible definition (with dummy-data example implementation) of an API to fetch notifications.
    
    CU-k8wz10

---
+++ src/main/kotlin/com/terraformation/seedbank/api/seedbank/NotificationController.kt
@@ -0,0 +1,69 @@
+package com.terraformation.seedbank.api.seedbank
+
+import com.terraformation.seedbank.api.annotation.SeedBankAppEndpoint
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import java.time.Instant
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/seedbank/notification")
+@SeedBankAppEndpoint
+class NotificationController {
+  @ApiResponse(
+      responseCode = "200", description = "Notifications in reverse time order (newest first)")
+  @GetMapping
+  @Operation(summary = "Get a list of recent notifications")
+  fun listAll(
+      @Parameter(description = "Don't return notifications older than this; default is 1 month ago")
+      @RequestParam
+      since: Instant? = null,
+      @Parameter(description = "Return at most this many notifications; default is no limit")
+      @RequestParam
+      limit: Int? = null
+  ): NotificationListResponse {
+    return NotificationListResponse(
+        listOf(
+            NotificationPayload(
+                "xxxxx", Instant.now(), NotificationType.Alert, false, "An example alert"),
+            NotificationPayload(
+                "yyyyy",
+                Instant.now().minusSeconds(3600),
+                NotificationType.State,
+                false,
+                "An example state notification"),
+            NotificationPayload(
+                "zzzzz",
+                Instant.now().minusSeconds(7200),
+                NotificationType.Date,
+                true,
+                "An example date notification that is already read")))
+  }
+}
+
+enum class NotificationType {
+  Alert,
+  State,
+  Date
+}
+
+data class NotificationPayload(
+    @Schema(
+        description = "Unique identifier for this notification. Clients should treat it as opaque.",
+        example = "12345")
+    val id: String,
+    val timestamp: Instant,
+    val type: NotificationType,
+    @Schema(description = "If true, this notification has been marked as read.") val read: Boolean,
+    @Schema(
+        description = "Plain-text body of notification.",
+        example = "Accession XYZ is ready to be dried.")
+    val text: String
+)
+
+data class NotificationListResponse(val notifications: List<NotificationPayload>)
