780abcd11e72b47a101f272ea43f51a15a18ee33
Steven Grimm <steven@terraformation.com>
2021-02-09 14:22:47 -0800

    Add Docker entrypoint to mount disk on Balena
    Under Balena, each container needs to mount its own filesystem from the RAID
    array. Add an entrypoint script to mount the photos volume to `/data` when running
    under Balena, with the usual provisions for overriding the device name and using
    a Docker volume instead of an external disk.
    
    This required changing the OpenJDK base image to a Debian one because the default
    image didn't include the `mount` command.

---
+++ docker/entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Set up the filesystem for file (photo) storage when running under Balena.
+#
+
+set -e -o pipefail
+
+if [ "$BALENA" = 1 ]; then
+    case "$TERRAWARE_USE_INTERNAL_STORAGE" in
+        1|y*|t*|Y*|T*)
+            if [ -d /data ]; then
+                rmdir /data
+            fi
+            if [ -L /data ]; then
+                rm /data
+            fi
+            ln -s /file-storage-volume /data
+            ;;
+
+        *)
+            DISK_DEVICE="${TERRAWARE_FILE_STORAGE_DISK:-/dev/md0p3}"
+            if [ -L /data ]; then
+                rm /data
+            fi
+            mkdir -p /data
+            mount "$DISK_DEVICE" /data
+            ;;
+    esac
+fi
+
+exec "$@"
