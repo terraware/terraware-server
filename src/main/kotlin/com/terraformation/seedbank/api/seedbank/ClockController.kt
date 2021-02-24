74006a6b2b0f91b52ca6c435df853740105d4f81
Steven Grimm <steven@terraformation.com>
2021-02-24 13:32:09 -0800

    Add barebones UI for server clock manipulation
    `/api/test/clock` will serve up a simple HTML page showing the current server time and a button to advance the clock by some number of days.
    
    CU-nh35yv[completed]

--- src/main/kotlin/com/terraformation/seedbank/api/seedbank/ClockController.kt
+++ src/main/kotlin/com/terraformation/seedbank/api/seedbank/ClockController.kt
@@ -3,29 +3,52 @@ package com.terraformation.seedbank.api.seedbank
 import com.terraformation.seedbank.api.SuccessResponsePayload
 import com.terraformation.seedbank.api.annotation.SeedBankAppEndpoint
 import com.terraformation.seedbank.services.DatabaseBackedClock
+import io.swagger.v3.oas.annotations.Hidden
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import java.time.Duration
 import java.time.Instant
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 import javax.validation.constraints.Min
 import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Profile("default", "apidoc")
-@RequestMapping("/api/v1/seedbank/clock")
-@RestController
+@Controller
 @SeedBankAppEndpoint
 class ClockController(private val clock: DatabaseBackedClock) {
-  @GetMapping
+  @GetMapping("/api/test/clock")
+  @Hidden
+  fun getClockTestUi(model: Model): String {
+    val now = ZonedDateTime.now(clock)
+    val currentTime = DateTimeFormatter.RFC_1123_DATE_TIME.format(now)
+    model.addAttribute("currentTime", currentTime)
+    return "test/clock"
+  }
+
+  @PostMapping("/api/test/clock")
+  @Hidden
+  fun getClockTestUi(@RequestParam days: Long, model: Model): String {
+    clock.advance(Duration.ofDays(days))
+    val daysWord = if (days == 1L) "day" else "days"
+    model.addAttribute("successMessage", "Clock advanced by $days $daysWord.")
+    return getClockTestUi(model)
+  }
+
+  @GetMapping("/api/v1/seedbank/clock")
   @Operation(
       summary = "Get the server's current date and time.",
       description =
           "In test environments, the clock can be advanced artificially, which will cause it to " +
               "differ from the real-world date and time.")
+  @ResponseBody
   fun getCurrentTime() = GetCurrentTimeResponsePayload(clock.instant())
 
   @ApiResponse(
@@ -37,7 +60,8 @@ class ClockController(private val clock: DatabaseBackedClock) {
           "Advancing the clock causes any scheduled processes to run. Subsequent GET requests to " +
               "read the current time will take the advancement into account. Only supported in " +
               "test and development environments.")
-  @PostMapping("/advance")
+  @PostMapping("/api/v1/seedbank/clock/advance")
+  @ResponseBody
   fun advanceClock(
       @RequestBody payload: AdvanceClockRequestPayload
   ): GetCurrentTimeResponsePayload {
