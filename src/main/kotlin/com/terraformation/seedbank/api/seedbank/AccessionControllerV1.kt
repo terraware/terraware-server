ebf4fc335a6676c19f32d3b45547d5d8e1b03194
Steven Grimm <steven@terraformation.com>
2021-04-08 12:02:20 -0700

    Remove simulated boolean rare/endangered fields
    Step 1 of a 3-step process for getting rid of some backward-compatibility code
    we added when we changed the `rare` and `endangered` accession fields from
    booleans to enums. We added new endpoints under `/api/v2` that exposed the
    enums, and made the existing `/api/v1` ones simulate the old boolean behavior.
    
    1. Make the v1 endpoints expose the enums once all clients have switched to
       the v2 ones. (That's this change.)
    2. Update apps to switch back to the v1 endpoints.
    3. Remove the v2 endpoints.
    
    CU-q518m0

--- src/main/kotlin/com/terraformation/seedbank/api/seedbank/AccessionControllerV1.kt
+++ src/main/kotlin/com/terraformation/seedbank/api/seedbank/AccessionControllerV1.kt
@@ -1,29 +1,10 @@
 package com.terraformation.seedbank.api.seedbank
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect
-import com.fasterxml.jackson.annotation.JsonInclude
-import com.terraformation.seedbank.api.NotFoundException
-import com.terraformation.seedbank.api.SuccessResponsePayload
 import com.terraformation.seedbank.api.annotation.ApiResponse404
 import com.terraformation.seedbank.api.annotation.SeedBankAppEndpoint
-import com.terraformation.seedbank.db.AccessionState
 import com.terraformation.seedbank.db.AccessionStore
-import com.terraformation.seedbank.db.GerminationTestType
-import com.terraformation.seedbank.db.ProcessingMethod
-import com.terraformation.seedbank.db.SpeciesEndangeredType
-import com.terraformation.seedbank.db.SpeciesRareType
-import com.terraformation.seedbank.db.StorageCondition
-import com.terraformation.seedbank.model.AccessionActive
-import com.terraformation.seedbank.model.AccessionSource
-import com.terraformation.seedbank.model.ConcreteAccession
-import com.terraformation.seedbank.model.Geolocation
-import com.terraformation.seedbank.services.perClassLogger
 import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
-import java.math.BigDecimal
-import java.time.LocalDate
-import javax.validation.Valid
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -35,7 +16,9 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/v1/seedbank/accession")
 @RestController
 @SeedBankAppEndpoint
-class AccessionControllerV1(private val accessionStore: AccessionStore) {
+class AccessionControllerV1(accessionStore: AccessionStore) {
+  private val v2Controller = AccessionController(accessionStore)
+
   @ApiResponse(
       responseCode = "200",
       description =
@@ -43,12 +26,7 @@ class AccessionControllerV1(private val accessionStore: AccessionStore) {
               "server, including the accession number.")
   @Operation(summary = "Create a new accession.")
   @PostMapping
-  fun create(
-      @RequestBody payload: CreateAccessionRequestPayloadV1
-  ): CreateAccessionResponsePayloadV1 {
-    val updatedPayload = accessionStore.create(payload.toV2Payload())
-    return CreateAccessionResponsePayloadV1(AccessionPayloadV1(updatedPayload))
-  }
+  fun create(@RequestBody payload: CreateAccessionRequestPayload) = v2Controller.create(payload)
 
   @ApiResponse(
       responseCode = "200",
@@ -59,335 +37,13 @@ class AccessionControllerV1(private val accessionStore: AccessionStore) {
   @Operation(summary = "Update an existing accession.")
   @PutMapping("/{accessionNumber}")
   fun update(
-      @RequestBody payload: UpdateAccessionRequestPayloadV1,
+      @RequestBody payload: UpdateAccessionRequestPayload,
       @PathVariable accessionNumber: String
-  ): UpdateAccessionResponsePayloadV1 {
-    perClassLogger().info("Payload $payload")
-    if (!accessionStore.update(accessionNumber, payload.toV2Payload())) {
-      throw NotFoundException()
-    } else {
-      val updatedModel = accessionStore.fetchByNumber(accessionNumber)!!
-      return UpdateAccessionResponsePayloadV1(AccessionPayloadV1(updatedModel))
-    }
-  }
+  ) = v2Controller.update(payload, accessionNumber)
 
   @ApiResponse(responseCode = "200")
   @ApiResponse404
   @GetMapping("/{accessionNumber}")
   @Operation(summary = "Retrieve an existing accession.")
-  fun read(@PathVariable accessionNumber: String): GetAccessionResponsePayloadV1 {
-    val accession =
-        accessionStore.fetchByNumber(accessionNumber)
-            ?: throw NotFoundException("The specified accession doesn't exist.")
-    return GetAccessionResponsePayloadV1(AccessionPayloadV1(accession))
-  }
+  fun read(@PathVariable accessionNumber: String) = v2Controller.read(accessionNumber)
 }
-
-fun Boolean?.toSpeciesEndangeredType() =
-    when (this) {
-      true -> SpeciesEndangeredType.Yes
-      false -> SpeciesEndangeredType.No
-      null -> null
-    }
-
-fun Boolean?.toSpeciesRareType() =
-    when (this) {
-      true -> SpeciesRareType.Yes
-      false -> SpeciesRareType.No
-      null -> null
-    }
-
-fun SpeciesEndangeredType?.toBoolean() =
-    when (this) {
-      SpeciesEndangeredType.Yes -> true
-      SpeciesEndangeredType.No -> false
-      else -> null
-    }
-
-fun SpeciesRareType?.toBoolean() =
-    when (this) {
-      SpeciesRareType.Yes -> true
-      SpeciesRareType.No -> false
-      else -> null
-    }
-
-// Ignore properties that are defined on AccessionFields but not accepted as input (CU-px8k25)
-@JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.NONE)
-data class CreateAccessionRequestPayloadV1(
-    val species: String? = null,
-    val family: String? = null,
-    val numberOfTrees: Int? = null,
-    val founderId: String? = null,
-    val endangered: Boolean? = null,
-    val rare: Boolean? = null,
-    val fieldNotes: String? = null,
-    val collectedDate: LocalDate? = null,
-    val receivedDate: LocalDate? = null,
-    val primaryCollector: String? = null,
-    val secondaryCollectors: Set<String>? = null,
-    val siteLocation: String? = null,
-    val landowner: String? = null,
-    val environmentalNotes: String? = null,
-    val bagNumbers: Set<String>? = null,
-    val geolocations: Set<Geolocation>? = null,
-    val germinationTestTypes: Set<GerminationTestType>? = null,
-    val germinationTests: List<GerminationTestPayload>? = null,
-    val deviceInfo: DeviceInfoPayload? = null,
-) {
-  fun toV2Payload(): CreateAccessionRequestPayload {
-    return CreateAccessionRequestPayload(
-        bagNumbers,
-        collectedDate,
-        deviceInfo,
-        endangered.toSpeciesEndangeredType(),
-        environmentalNotes,
-        family,
-        fieldNotes,
-        founderId,
-        geolocations,
-        germinationTestTypes,
-        germinationTests,
-        landowner,
-        numberOfTrees,
-        primaryCollector,
-        rare.toSpeciesRareType(),
-        receivedDate,
-        secondaryCollectors,
-        siteLocation,
-        null,
-        species,
-    )
-  }
-}
-
-// Ignore properties that are defined on AccessionFields but not accepted as input (CU-px8k25)
-@JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.NONE)
-data class UpdateAccessionRequestPayloadV1(
-    val species: String? = null,
-    val family: String? = null,
-    val numberOfTrees: Int? = null,
-    val founderId: String? = null,
-    val endangered: Boolean? = null,
-    val rare: Boolean? = null,
-    val fieldNotes: String? = null,
-    val collectedDate: LocalDate? = null,
-    val receivedDate: LocalDate? = null,
-    val primaryCollector: String? = null,
-    val secondaryCollectors: Set<String>? = null,
-    val siteLocation: String? = null,
-    val landowner: String? = null,
-    val environmentalNotes: String? = null,
-    val processingStartDate: LocalDate? = null,
-    val processingMethod: ProcessingMethod? = null,
-    val seedsCounted: Int? = null,
-    val subsetWeightGrams: BigDecimal? = null,
-    val totalWeightGrams: BigDecimal? = null,
-    val subsetCount: Int? = null,
-    val targetStorageCondition: StorageCondition? = null,
-    val dryingStartDate: LocalDate? = null,
-    val dryingEndDate: LocalDate? = null,
-    val dryingMoveDate: LocalDate? = null,
-    val processingNotes: String? = null,
-    val processingStaffResponsible: String? = null,
-    val bagNumbers: Set<String>? = null,
-    val storageStartDate: LocalDate? = null,
-    val storagePackets: Int? = null,
-    val storageLocation: String? = null,
-    val storageNotes: String? = null,
-    val storageStaffResponsible: String? = null,
-    val geolocations: Set<Geolocation>? = null,
-    val germinationTestTypes: Set<GerminationTestType>? = null,
-    val cutTestSeedsCompromised: Int? = null,
-    val cutTestSeedsEmpty: Int? = null,
-    val cutTestSeedsFilled: Int? = null,
-    @Valid val germinationTests: List<GerminationTestPayload>? = null,
-    @Valid val withdrawals: List<WithdrawalPayload>? = null,
-) {
-  fun toV2Payload(): UpdateAccessionRequestPayload {
-    return UpdateAccessionRequestPayload(
-        bagNumbers,
-        collectedDate,
-        cutTestSeedsCompromised,
-        cutTestSeedsEmpty,
-        cutTestSeedsFilled,
-        dryingEndDate,
-        dryingMoveDate,
-        dryingStartDate,
-        endangered.toSpeciesEndangeredType(),
-        environmentalNotes,
-        family,
-        fieldNotes,
-        founderId,
-        geolocations,
-        germinationTestTypes,
-        germinationTests,
-        landowner,
-        numberOfTrees,
-        primaryCollector,
-        processingMethod,
-        processingNotes,
-        processingStaffResponsible,
-        processingStartDate,
-        rare.toSpeciesRareType(),
-        receivedDate,
-        secondaryCollectors,
-        seedsCounted,
-        siteLocation,
-        null,
-        species,
-        storageLocation,
-        storageNotes,
-        storagePackets,
-        storageStaffResponsible,
-        storageStartDate,
-        subsetCount,
-        subsetWeightGrams,
-        targetStorageCondition,
-        totalWeightGrams,
-        withdrawals,
-    )
-  }
-}
-
-@JsonInclude(JsonInclude.Include.NON_NULL)
-data class AccessionPayloadV1(
-    @Schema(description = "Server-generated unique identifier for the accession.")
-    val accessionNumber: String,
-    @Schema(
-        description =
-            "Server-calculated accession state. Can change due to modifications to accession data " +
-                "or based on passage of time.")
-    val state: AccessionState,
-    @Schema(
-        description = "Server-calculated active indicator. This is based on the accession's state.")
-    val active: AccessionActive,
-    @Schema(
-        description =
-            "Which application this accession originally came from. This is currently based on " +
-                "the presence of the deviceInfo field.")
-    val source: AccessionSource,
-    val species: String? = null,
-    @Schema(description = "Server-generated unique ID of the species.") val speciesId: Long? = null,
-    val family: String? = null,
-    val numberOfTrees: Int? = null,
-    val founderId: String? = null,
-    val endangered: Boolean? = null,
-    val rare: Boolean? = null,
-    val fieldNotes: String? = null,
-    val collectedDate: LocalDate? = null,
-    val receivedDate: LocalDate? = null,
-    val primaryCollector: String? = null,
-    val secondaryCollectors: Set<String>? = null,
-    val siteLocation: String? = null,
-    val landowner: String? = null,
-    val environmentalNotes: String? = null,
-    val processingStartDate: LocalDate? = null,
-    val processingMethod: ProcessingMethod? = null,
-    val seedsCounted: Int? = null,
-    val subsetWeightGrams: BigDecimal? = null,
-    val totalWeightGrams: BigDecimal? = null,
-    val subsetCount: Int? = null,
-    val estimatedSeedCount: Int? = null,
-    @Schema(
-        description =
-            "Server-calculated effective seed count. This is the exact seed count if available, " +
-                "otherwise the estimated seed count, or null if neither seed count is available.")
-    val effectiveSeedCount: Int? = null,
-    val targetStorageCondition: StorageCondition? = null,
-    val dryingStartDate: LocalDate? = null,
-    val dryingEndDate: LocalDate? = null,
-    val dryingMoveDate: LocalDate? = null,
-    val processingNotes: String? = null,
-    val processingStaffResponsible: String? = null,
-    val bagNumbers: Set<String>? = null,
-    val storageStartDate: LocalDate? = null,
-    val storagePackets: Int? = null,
-    val storageLocation: String? = null,
-    val storageCondition: StorageCondition? = null,
-    val storageNotes: String? = null,
-    val storageStaffResponsible: String? = null,
-    val photoFilenames: List<String>? = null,
-    val geolocations: Set<Geolocation>? = null,
-    val germinationTestTypes: Set<GerminationTestType>? = null,
-    val germinationTests: List<GerminationTestPayload>? = null,
-    val withdrawals: List<WithdrawalPayload>? = null,
-    val cutTestSeedsFilled: Int? = null,
-    val cutTestSeedsEmpty: Int? = null,
-    val cutTestSeedsCompromised: Int? = null,
-    val latestGerminationTestDate: LocalDate? = null,
-    val latestViabilityPercent: Int? = null,
-    val totalViabilityPercent: Int? = null,
-    val deviceInfo: DeviceInfoPayload? = null,
-    @Schema(
-        description =
-            "Number of seeds remaining for withdrawal and testing. Calculated by the server when " +
-                "the accession's seed count (actual or estimated) is known.")
-    val seedsRemaining: Int? = null,
-) {
-  constructor(
-      model: ConcreteAccession
-  ) : this(
-      model.accessionNumber,
-      model.state,
-      model.active,
-      model.source,
-      model.species,
-      model.speciesId,
-      model.family,
-      model.numberOfTrees,
-      model.founderId,
-      model.endangered.toBoolean(),
-      model.rare.toBoolean(),
-      model.fieldNotes,
-      model.collectedDate,
-      model.receivedDate,
-      model.primaryCollector,
-      model.secondaryCollectors,
-      model.siteLocation,
-      model.landowner,
-      model.environmentalNotes,
-      model.processingStartDate,
-      model.processingMethod,
-      model.seedsCounted,
-      model.subsetWeightGrams,
-      model.totalWeightGrams,
-      model.subsetCount,
-      model.estimatedSeedCount,
-      model.effectiveSeedCount,
-      model.targetStorageCondition,
-      model.dryingStartDate,
-      model.dryingEndDate,
-      model.dryingMoveDate,
-      model.processingNotes,
-      model.processingStaffResponsible,
-      model.bagNumbers,
-      model.storageStartDate,
-      model.storagePackets,
-      model.storageLocation,
-      model.storageCondition,
-      model.storageNotes,
-      model.storageStaffResponsible,
-      model.photoFilenames,
-      model.geolocations,
-      model.germinationTestTypes,
-      model.germinationTests?.map { GerminationTestPayload(it) },
-      model.withdrawals?.map { WithdrawalPayload(it) },
-      model.cutTestSeedsFilled,
-      model.cutTestSeedsEmpty,
-      model.cutTestSeedsCompromised,
-      model.latestGerminationTestDate,
-      model.latestViabilityPercent,
-      model.totalViabilityPercent,
-      model.deviceInfo?.let { DeviceInfoPayload(it) },
-      model.seedsRemaining,
-  )
-}
-
-data class CreateAccessionResponsePayloadV1(val accession: AccessionPayloadV1) :
-    SuccessResponsePayload
-
-data class UpdateAccessionResponsePayloadV1(val accession: AccessionPayloadV1) :
-    SuccessResponsePayload
-
-data class GetAccessionResponsePayloadV1(val accession: AccessionPayloadV1) :
-    SuccessResponsePayload
