a018f6e6e612852ec4021da1c653efbfdabe54c7
Steven Grimm <steven@terraformation.com>
2021-07-30 15:49:54 -0700

    Make TerrawareServerConfig immutable
    The configuration doesn't change on the fly at runtime, so there is no reason
    its in-memory representation should be mutable.

--- src/main/kotlin/com/terraformation/backend/config/TerrawareServerConfig.kt
+++ src/main/kotlin/com/terraformation/backend/config/TerrawareServerConfig.kt
@@ -9,96 +9,110 @@ import java.time.ZoneOffset
 import javax.validation.constraints.Min
 import javax.validation.constraints.NotNull
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.core.io.Resource
 import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.validation.annotation.Validated
 
+/**
+ * Application-specific configuration options. These are populated by Spring's configuration
+ * properties system from a few sources, the most relevant ones being `application.yaml` files and
+ * environment variables. In the YAML files, these are all under a top-level `terraware` key. In the
+ * environment, these are all pulled from environment variables with a `TERRAWARE_` prefix in their
+ * names.
+ */
 @ConfigurationProperties("terraware")
+@ConstructorBinding
 @Validated
-class TerrawareServerConfig {
-  /** URL of site-specific configuration file. */
-  @NotNull lateinit var siteConfigUrl: Resource
+class TerrawareServerConfig(
+    /** URL of site-specific configuration file. */
+    @NotNull val siteConfigUrl: Resource,
 
-  /** How often to refresh site-specific configuration, in seconds. 0 disables periodic refresh. */
-  @Min(0) var siteConfigRefreshSecs: Long = 3600
+    /**
+     * How often to refresh site-specific configuration, in seconds. 0 disables periodic refresh.
+     */
+    @Min(0) val siteConfigRefreshSecs: Long = 3600,
 
-  var facilityId: FacilityId = FacilityId(0L)
+    /** Default facility ID (deprecated). */
+    val facilityId: FacilityId,
 
-  /**
-   * Directory to use for photo storage. The server will attempt to create this directory if it
-   * doesn't exist.
-   */
-  @NotNull lateinit var photoDir: Path
+    /**
+     * Directory to use for photo storage. The server will attempt to create this directory if it
+     * doesn't exist.
+     */
+    @NotNull val photoDir: Path,
 
-  /**
-   * Number of levels of parent directories to create for photos. Photos are stored in a tree of
-   * single-character subdirectories from the beginning of the accession number. For example, if
-   * this is 3, and `photoDir` is `/x/y`, photos for accession `ABCDEFG` will be stored in
-   * `/x/y/A/B/C/ABCDEFG`.
-   */
-  @Min(0) var photoIntermediateDepth: Int = 3
+    /**
+     * Number of levels of parent directories to create for photos. Photos are stored in a tree of
+     * single-character subdirectories from the beginning of the accession number. For example, if
+     * this is 3, and `photoDir` is `/x/y`, photos for accession `ABCDEFG` will be stored in
+     * `/x/y/A/B/C/ABCDEFG`.
+     */
+    @Min(0) val photoIntermediateDepth: Int = 3,
 
-  /**
-   * Server's time zone. This is mostly used to determine when scheduled daily jobs are run. Default
-   * is UTC. May be specified as a tz database time zone name such as `US/Hawaii` or a UTC offset
-   * such as `+04:00`.
-   */
-  var timeZone: ZoneId = ZoneOffset.UTC
+    /**
+     * Server's time zone. This is mostly used to determine when scheduled daily jobs are run.
+     * Default is UTC. May be specified as a tz database time zone name such as `US/Hawaii` or a UTC
+     * offset such as `+04:00`.
+     */
+    val timeZone: ZoneId = ZoneOffset.UTC,
 
-  /**
-   * Use a fake clock that can be advanced via API requests. This should only be enabled in test
-   * environments.
-   */
-  var useTestClock: Boolean = false
+    /**
+     * Use a fake clock that can be advanced via API requests. This should only be enabled in test
+     * environments.
+     */
+    val useTestClock: Boolean = false,
 
-  /**
-   * API key to create by default. If this is set and the key isn't already present in the `api_key`
-   * table, any other API keys are removed from the database at start time and the new key is
-   * created. If it is not set, the `api_key` table should be populated some other way.
-   *
-   * This is just a stopgap; in the future we'll introduce real API key management capability.
-   */
-  var apiKey: String? = null
+    /**
+     * API key to create by default. If this is set and the key isn't already present in the
+     * `api_key` table, any other API keys are removed from the database at start time and the new
+     * key is created. If it is not set, the `api_key` table should be populated some other way.
+     *
+     * This is just a stopgap; in the future we'll introduce real API key management capability.
+     */
+    val apiKey: String? = null,
 
-  /** Configures execution of daily tasks. */
-  var dailyTasks: DailyTasksConfig = DailyTasksConfig()
+    /** Configures execution of daily tasks. */
+    val dailyTasks: DailyTasksConfig = DailyTasksConfig(),
 
-  /** Configures the server's communication with an MQTT broker. */
-  var mqtt: MqttConfig = MqttConfig()
-
+    /** Configures the server's communication with an MQTT broker. */
+    val mqtt: MqttConfig = MqttConfig(),
+) {
+  @ConstructorBinding
   class DailyTasksConfig(
       /** Whether to run daily tasks. */
-      var enabled: Boolean = true,
+      val enabled: Boolean = true,
 
       /**
        * What time of day the daily tasks are run. This is treated as a local time in the configured
        * [timeZone]. Default is 8AM.
        */
-      @DateTimeFormat(iso = DateTimeFormat.ISO.TIME) var startTime: LocalTime = LocalTime.of(8, 0)
+      @DateTimeFormat(iso = DateTimeFormat.ISO.TIME) val startTime: LocalTime = LocalTime.of(8, 0)
   )
 
+  @ConstructorBinding
   class MqttConfig(
       /** If true, connect to an MQTT broker to publish and receive device data. */
-      var enabled: Boolean = false,
+      val enabled: Boolean = false,
 
       /** URI of MQTT broker. Supported schemes are "ws", "wss", and "tcp". */
-      var address: URI? = URI("ws://localhost:1883"),
+      val address: URI? = URI("ws://localhost:1883"),
 
       /** Client identifier to send during MQTT authentication. */
-      var clientId: String? = "terraware-server",
+      val clientId: String? = "terraware-server",
 
       /** Password to send during MQTT authentication. */
-      var password: String? = null,
+      val password: String? = null,
 
       /**
        * Prefix to use in front of topic names. The server will automatically add "/" between this
        * and the topic names it generates. Default is to directly use the generated topics with no
        * prefix.
        */
-      var topicPrefix: String? = null,
+      val topicPrefix: String? = null,
 
       /** How often to retry connecting to the MQTT broker if the connection fails. */
-      var connectRetryIntervalMillis: Long = 30000,
+      val connectRetryIntervalMillis: Long = 30000,
   )
 
   companion object {
