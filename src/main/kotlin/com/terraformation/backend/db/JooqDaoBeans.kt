ea79443dcb23ec920066aa31ed6c701fb553dcc3
Steven Grimm <sgrimm@users.noreply.github.com>
2021-08-23 14:36:11 -0700

    Declare missing DAO beans; add CI test to catch it next time (#20)
    There were some DAO classes that were declared as dependencies of some managed
    beans but weren't ever declared by the code. This caused the server to fail to
    start up, though tests continued to pass since none of our tests involves
    constructing the entire dependency graph.
    
    Add the missing DAOs, and also add a CI step to detect this kind of problem in
    the future: the Gradle task to generate the OpenAPI docs launches a temporary
    server instance, and if there's a problem with the bean graph, it will fail.

--- src/main/kotlin/com/terraformation/backend/db/JooqDaoBeans.kt
+++ src/main/kotlin/com/terraformation/backend/db/JooqDaoBeans.kt
@@ -4,12 +4,16 @@ import com.terraformation.backend.db.tables.daos.AccessionPhotosDao
 import com.terraformation.backend.db.tables.daos.AccessionsDao
 import com.terraformation.backend.db.tables.daos.DevicesDao
 import com.terraformation.backend.db.tables.daos.FacilitiesDao
+import com.terraformation.backend.db.tables.daos.FeaturesDao
+import com.terraformation.backend.db.tables.daos.LayersDao
 import com.terraformation.backend.db.tables.daos.OrganizationsDao
+import com.terraformation.backend.db.tables.daos.PhotosDao
 import com.terraformation.backend.db.tables.daos.ProjectsDao
 import com.terraformation.backend.db.tables.daos.SitesDao
 import com.terraformation.backend.db.tables.daos.SpeciesDao
 import com.terraformation.backend.db.tables.daos.StorageLocationsDao
 import com.terraformation.backend.db.tables.daos.TaskProcessedTimesDao
+import com.terraformation.backend.db.tables.daos.ThumbnailDao
 import com.terraformation.backend.db.tables.daos.TimeseriesDao
 import com.terraformation.backend.db.tables.daos.UsersDao
 import org.jooq.DSLContext
@@ -32,12 +36,16 @@ class JooqDaoBeans(dslContext: DSLContext) {
   @Bean fun accessionsDao() = AccessionsDao(configuration)
   @Bean fun deviceDao() = DevicesDao(configuration)
   @Bean fun facilitiesDao() = FacilitiesDao(configuration)
+  @Bean fun featuresDao() = FeaturesDao(configuration)
+  @Bean fun layersDao() = LayersDao(configuration)
   @Bean fun organizationDao() = OrganizationsDao(configuration)
+  @Bean fun photosDao() = PhotosDao(configuration)
   @Bean fun projectsDao() = ProjectsDao(configuration)
   @Bean fun siteDao() = SitesDao(configuration)
   @Bean fun speciesDao() = SpeciesDao(configuration)
   @Bean fun storageLocationDao() = StorageLocationsDao(configuration)
   @Bean fun taskProcessedTimeDao() = TaskProcessedTimesDao(configuration)
+  @Bean fun thumbnailDao() = ThumbnailDao(configuration)
   @Bean fun timeseriesDao() = TimeseriesDao(configuration)
   @Bean fun usersDao() = UsersDao(configuration)
 }
