5dbb7021fc33072c11e24085c0db4ae776bd5b44
Steven Grimm <steven@terraformation.com>
2021-11-30 10:53:10 -0800

    (WIP) TRACK-485 Introduce field aliases
    Need to write tests that don't depend on flattened fields

---
+++ src/main/kotlin/com/terraformation/backend/search/field/AliasField.kt
@@ -0,0 +1,39 @@
+package com.terraformation.backend.search.field
+
+import com.terraformation.backend.search.SearchFieldPath
+
+/**
+ * An alternate name for another search field. Using an alias is equivalent to using the target
+ * field name, except that search results will use the alias's name.
+ *
+ * This is primarily used to support short names for fields in flattened sublists for backward
+ * compatibility with earlier versions of the accession search API.
+ */
+class AliasField
+private constructor(
+    /** Alternate name for the field. */
+    override val fieldName: String,
+
+    /**
+     * Which underlying field this alias refers to. The path must be relative to the namespace that
+     * contains this alias field. It can refer to flattened sublists but not nested ones.
+     */
+    val targetPath: SearchFieldPath,
+
+    /**
+     * The underlying field. Other than the field name, all the properties on the alias delegate to
+     * this.
+     */
+    val original: SearchField
+) : SearchField by original {
+  constructor(
+      fieldName: String,
+      targetPath: SearchFieldPath
+  ) : this(fieldName, targetPath, targetPath.searchField)
+
+  init {
+    if (targetPath.isNested) {
+      throw IllegalArgumentException("Cannot alias nested path $targetPath")
+    }
+  }
+}
