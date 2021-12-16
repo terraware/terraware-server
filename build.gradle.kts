871e09b4a4aa3e9cf54c17eef889bc06d71dc415
Steven Grimm <steven@terraformation.com>
2021-12-16 11:19:55 -0800

    Upgrade to MockK 1.12.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -103,7 +103,7 @@ dependencies {
 
   runtimeOnly("net.logstash.logback:logstash-logback-encoder:7.0.1")
 
-  testImplementation("io.mockk:mockk:1.10.6")
+  testImplementation("io.mockk:mockk:1.12.1")
   testImplementation("org.junit.jupiter:junit-jupiter-api")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.springframework.security:spring-security-test")
