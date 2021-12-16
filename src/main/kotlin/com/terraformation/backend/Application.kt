c894debfbb089058608c9f65483ecc19c34925b8
Steven Grimm <steven@terraformation.com>
2021-12-16 09:58:12 -0800

    Upgrade to springdoc-openapi 1.6.1
    Also upgrade its Gradle plugin. Remove the list of tags from the
    `OpenAPIDefinition` annotation since it's no longer needed. Update the other
    fields on that annotation, which still referred to terraware-server as the seed
    bank server.

--- src/main/kotlin/com/terraformation/backend/Application.kt
+++ src/main/kotlin/com/terraformation/backend/Application.kt
@@ -4,7 +4,6 @@ import com.terraformation.backend.config.TerrawareServerConfig
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.info.Info
 import io.swagger.v3.oas.annotations.info.License
-import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration
@@ -18,18 +17,10 @@ import org.springframework.scheduling.annotation.EnableScheduling
 @OpenAPIDefinition(
     info =
         Info(
-            title = "Terraware Seed Bank",
+            title = "Terraware Server",
             version = "0.1-SNAPSHOT",
-            description = "Local server API for seed banks",
-            license = License(name = "MIT"),
-        ),
-    tags =
-        [
-            Tag(name = "SeedBankApp"),
-            Tag(name = "DeviceManager"),
-            Tag(name = "GISApp"),
-            Tag(name = "Search"),
-        ])
+            description = "Back end for Terraware applications",
+            license = License(name = "MIT")))
 @EnableConfigurationProperties(TerrawareServerConfig::class)
 @EnableScheduling
 @SpringBootApplication(
