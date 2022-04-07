a1e9232f44ea063639b12f0f5da8d583e9ae5db5
Steven Grimm <sgrimm@users.noreply.github.com>
2022-04-07 00:52:40 -0700

    Add superclass for numeric search fields (#271)
    The `SearchField` classes for numeric fields had a bunch of duplicate code.
    The only difference between them is how they convert string representations
    to the appropriate numeric type and back again, so introduce a superclass that
    ensures they work consistently apart from the string conversions.

--- src/main/kotlin/com/terraformation/backend/search/field/GramsField.kt
+++ src/main/kotlin/com/terraformation/backend/search/field/GramsField.kt
@@ -1,53 +1,22 @@
 package com.terraformation.backend.search.field
 
 import com.terraformation.backend.db.SeedQuantityUnits
-import com.terraformation.backend.search.FieldNode
-import com.terraformation.backend.search.SearchFilterType
 import com.terraformation.backend.search.SearchTable
 import com.terraformation.backend.seedbank.model.toGrams
 import java.math.BigDecimal
-import java.util.EnumSet
-import org.jooq.Condition
 import org.jooq.Record
 import org.jooq.TableField
-import org.jooq.impl.DSL
 
 /** Search field for columns with weights in grams. Supports unit conversions on search criteria. */
 class GramsField(
-    override val fieldName: String,
-    override val displayName: String,
-    override val databaseField: TableField<*, BigDecimal?>,
-    override val table: SearchTable,
-) : SingleColumnSearchField<BigDecimal>() {
-  override val supportedFilterTypes: Set<SearchFilterType> =
-      EnumSet.of(SearchFilterType.Exact, SearchFilterType.Range)
-
-  override fun computeValue(record: Record) = record[databaseField]?.toPlainString()
-
-  override fun getCondition(fieldNode: FieldNode): Condition {
-    val bigDecimalValues = fieldNode.values.map { parseGrams(it) }
-    val nonNullValues = bigDecimalValues.filterNotNull()
-
-    return when (fieldNode.type) {
-      SearchFilterType.Exact -> {
-        DSL.or(
-            listOfNotNull(
-                if (nonNullValues.isNotEmpty()) databaseField.`in`(nonNullValues) else null,
-                if (fieldNode.values.any { it == null }) databaseField.isNull else null))
-      }
-      SearchFilterType.Fuzzy ->
-          throw RuntimeException("Fuzzy search not supported for numeric fields")
-      SearchFilterType.Range -> rangeCondition(bigDecimalValues)
-    }
-  }
-
+    fieldName: String,
+    displayName: String,
+    databaseField: TableField<*, BigDecimal?>,
+    table: SearchTable,
+) : NumericSearchField<BigDecimal>(fieldName, displayName, databaseField, table) {
   private val formatRegex = Regex("([\\d.]+)\\s*(\\D*)")
 
-  private fun parseGrams(value: String?): BigDecimal? {
-    if (value == null) {
-      return null
-    }
-
+  override fun fromString(value: String): BigDecimal {
     val matches =
         formatRegex.matchEntire(value)
             ?: throw IllegalStateException(
@@ -62,4 +31,6 @@ class GramsField(
 
     return units.toGrams(number)
   }
+
+  override fun computeValue(record: Record) = record[databaseField]?.toPlainString()
 }
--- src/main/kotlin/com/terraformation/backend/search/field/IntegerField.kt
+++ src/main/kotlin/com/terraformation/backend/search/field/IntegerField.kt
@@ -1,37 +1,15 @@
 package com.terraformation.backend.search.field
 
-import com.terraformation.backend.search.FieldNode
-import com.terraformation.backend.search.SearchFilterType
 import com.terraformation.backend.search.SearchTable
-import java.util.*
-import org.jooq.Condition
 import org.jooq.TableField
-import org.jooq.impl.DSL
 
 /** Search field for numeric columns that don't allow fractional values. */
 class IntegerField(
-    override val fieldName: String,
-    override val displayName: String,
-    override val databaseField: TableField<*, Int?>,
-    override val table: SearchTable,
-    override val nullable: Boolean = true
-) : SingleColumnSearchField<Int>() {
-  override val supportedFilterTypes: Set<SearchFilterType>
-    get() = EnumSet.of(SearchFilterType.Exact, SearchFilterType.Range)
-
-  override fun getCondition(fieldNode: FieldNode): Condition {
-    val intValues = fieldNode.values.map { it?.toInt() }
-    val nonNullValues = intValues.filterNotNull()
-    return when (fieldNode.type) {
-      SearchFilterType.Exact ->
-          DSL.or(
-              listOfNotNull(
-                  if (nonNullValues.isNotEmpty()) databaseField.`in`(nonNullValues) else null,
-                  if (fieldNode.values.any { it == null }) databaseField.isNull else null,
-              ))
-      SearchFilterType.Fuzzy ->
-          throw RuntimeException("Fuzzy search not supported for numeric fields")
-      SearchFilterType.Range -> rangeCondition(intValues)
-    }
-  }
+    fieldName: String,
+    displayName: String,
+    databaseField: TableField<*, Int?>,
+    table: SearchTable,
+    nullable: Boolean = true,
+) : NumericSearchField<Int>(fieldName, displayName, databaseField, table, nullable) {
+  override fun fromString(value: String) = value.toInt()
 }
