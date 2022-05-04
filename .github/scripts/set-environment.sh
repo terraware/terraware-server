63d16a7fc791ef9a803589dda31accb926a1a276
Steven Grimm <steven@terraformation.com>
2022-05-04 14:48:47 -0700

    SW-944 Build Docker image for species branch

--- .github/scripts/set-environment.sh
+++ .github/scripts/set-environment.sh
@@ -2,33 +2,43 @@
 
 set -euo pipefail
 
-# Define tier based on branch ref
-if [[ "$GITHUB_REF" =~ refs/tags/v[0-9]+\.[0-9.]+ ]]; then
-  export TIER=PROD
-  export IS_CD=true
-elif [[ "$GITHUB_REF" == refs/heads/main ]]; then
-  export TIER=STAGING
-  export IS_CD=true
-elif [[ "$GITHUB_REF" == refs/heads/qa ]]; then
-  export TIER=QA
-  export IS_CD=true
-else
-  export IS_CD=false
-  exit
-fi
-
 commit_sha="${GITHUB_SHA:0:12}"
 docker_image='terraware/terraware-server'
-docker_tags="${docker_image}:$commit_sha,${docker_image}:${TIER}"
 
-# Define secret names based on the tier
-echo "TIER=$TIER
-IS_CD=$IS_CD
-SSH_HOST_SECRET_NAME=${TIER}_SSH_HOST
-SSH_KEY_SECRET_NAME=${TIER}_SSH_KEY
-SSH_USER_SECRET_NAME=${TIER}_SSH_USER
-AWS_ACCESS_KEY_ID_SECRET_NAME=${TIER}_AWS_ACCESS_KEY_ID
-AWS_SECRET_ACCESS_KEY_SECRET_NAME=${TIER}_AWS_SECRET_ACCESS_KEY
-AWS_REGION_SECRET_NAME=${TIER}_AWS_REGION
-COMMIT_SHA=$commit_sha
-DOCKER_TAGS=$docker_tags" >> $GITHUB_ENV
+# Define tier based on branch ref
+if [[ "$GITHUB_REF" =~ refs/tags/v[0-9]+\.[0-9.]+ ]]; then
+    TIER=PROD
+    IS_CD=true
+elif [[ "$GITHUB_REF" == refs/heads/main ]]; then
+    TIER=STAGING
+    IS_CD=true
+elif [[ "$GITHUB_REF" == refs/heads/qa ]]; then
+    TIER=QA
+    IS_CD=true
+else
+    IS_CD=false
+fi
+
+(
+    echo "IS_CD=$IS_CD"
+    echo "COMMIT_SHA=$commit_sha"
+
+    if [[ "$IS_CD" == true ]]; then
+        echo "TIER=$TIER"
+        echo "DOCKER_TAGS=${docker_image}:$commit_sha,${docker_image}:${TIER}"
+
+        # Define secret names based on the tier
+        echo "SSH_HOST_SECRET_NAME=${TIER}_SSH_HOST"
+        echo "SSH_KEY_SECRET_NAME=${TIER}_SSH_KEY"
+        echo "SSH_USER_SECRET_NAME=${TIER}_SSH_USER"
+        echo "AWS_ACCESS_KEY_ID_SECRET_NAME=${TIER}_AWS_ACCESS_KEY_ID"
+        echo "AWS_SECRET_ACCESS_KEY_SECRET_NAME=${TIER}_AWS_SECRET_ACCESS_KEY"
+        echo "AWS_REGION_SECRET_NAME=${TIER}_AWS_REGION"
+    fi
+
+    # Build Docker images for the species branch (TODO: Remove this before merging to main)
+    if [[ "$GITHUB_REF" == refs/heads/species ]]; then
+        echo "DOCKER_TAGS=${docker_image}:species"
+    fi
+) >> "$GITHUB_ENV"
+
