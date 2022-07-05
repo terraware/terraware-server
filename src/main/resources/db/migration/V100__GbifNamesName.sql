fbb93bc6c0a72b5f6b198d7bce9100496df8c42f
Steven Grimm <steven@terraformation.com>
2022-07-05 10:11:20 -0700

    Clarify that we're creating the B-tree index here

--- src/main/resources/db/migration/V100__GbifNamesName.sql
+++ src/main/resources/db/migration/V100__GbifNamesName.sql
@@ -1,3 +1,4 @@
 -- The trigram index on gbif_names.name can be used for exact lookups, but only on PostgreSQL 14
--- and higher, and even there, it is significantly slower than a B-tree index.
+-- and higher, and even there, it is significantly slower than a B-tree index. Create a B-tree index
+-- to cover exact-name queries.
 CREATE INDEX ON gbif_names (name, is_scientific);
