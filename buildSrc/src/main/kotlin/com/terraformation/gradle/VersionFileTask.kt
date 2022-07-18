9bb39986659204be407ea8a9200fbf6cf03b0015
Steven Grimm <sgrimm@users.noreply.github.com>
2022-07-18 07:37:35 -0700

    Move custom tasks out of build.gradle.kts (#491)
    Our Gradle build script is a little hard to navigate in part because it has a
    bunch of one-off custom tasks to implement custom build logic. Turn these into
    full-fledged classes that use Gradle's task API. This should declutter the build
    script a bit and also make it easier to improve the custom logic in the future.

---
+++ buildSrc/src/main/kotlin/com/terraformation/gradle/VersionFileTask.kt
@@ -0,0 +1,28 @@
+package com.terraformation.gradle
+
+import java.io.File
+import java.nio.file.Files
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.TaskAction
+
+/**
+ * Generates a Kotlin source file that contains the project version number, so it can be referenced
+ * at runtime.
+ */
+abstract class VersionFileTask : DefaultTask() {
+  @get:Input val version: String = "${project.version}"
+
+  @get:OutputFile
+  val outputFile =
+      File("${project.buildDir}/generated/kotlin/com/terraformation/backend/Version.kt")
+
+  @TaskAction
+  fun generate() {
+    val path = outputFile.toPath()
+    Files.createDirectories(path.parent)
+    Files.writeString(
+        path, "package com.terraformation.backend\nconst val VERSION = \"$version\"\n")
+  }
+}
