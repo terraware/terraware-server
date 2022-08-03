5c175806ff8a835601dac3fdd877199adaf462b6
Steven Grimm <sgrimm@users.noreply.github.com>
2022-08-03 10:49:14 -0700

    Clean up CI configuration (#525)
    To support changing how we access the AWS hosts, move the entire ssh
    configuration to a GitHub secret.
    
    Remove the temporary special-case handling of the `species` branch.

--- .github/scripts/set-environment.sh
+++ .github/scripts/set-environment.sh
@@ -30,14 +30,7 @@ fi
         # Define secret names based on the tier
         echo "AWS_REGION_SECRET_NAME=${TIER}_AWS_REGION"
         echo "AWS_ROLE_SECRET_NAME=${TIER}_AWS_ROLE"
-        echo "SSH_HOST_SECRET_NAME=${TIER}_SSH_HOST"
+        echo "SSH_CONFIG_SECRET_NAME=${TIER}_SSH_CONFIG"
         echo "SSH_KEY_SECRET_NAME=${TIER}_SSH_KEY"
-        echo "SSH_USER_SECRET_NAME=${TIER}_SSH_USER"
-    fi
-
-    # Build Docker images for the species branch (TODO: Remove this before merging to main)
-    if [[ "$GITHUB_REF" == refs/heads/species ]]; then
-        echo "DOCKER_TAGS=${docker_image}:species"
     fi
 ) >> "$GITHUB_ENV"
-
