60113c917f5f3f26f3f8de0a7c97ba6f087c2254
Steven Grimm <steven@terraformation.com>
2022-09-08 09:51:24 -0700

    SW-1575 Remove photo metadata from API 2/2
    Drop a bunch of metadata columns from the photos table, both the ones that were
    removed from the API in part 1 of this series and some others that were part of
    our original data model design but never ended up being used.

---
+++ src/main/resources/db/migration/V127__DropPhotosMetadata.sql
@@ -0,0 +1,6 @@
+ALTER TABLE photos DROP COLUMN captured_time;
+ALTER TABLE photos DROP COLUMN gps_horiz_accuracy;
+ALTER TABLE photos DROP COLUMN gps_vert_accuracy;
+ALTER TABLE photos DROP COLUMN heading;
+ALTER TABLE photos DROP COLUMN location;
+ALTER TABLE photos DROP COLUMN orientation;
