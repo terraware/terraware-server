421996aa37b882d06a696a996f8e9b4f6e41af8a
Steven Grimm <sgrimm@users.noreply.github.com>
2022-09-30 14:58:34 -0700

    Add smoke test for JobRunr API (#667)
    Twice now, we've run into Kotlin version upgrades that broke JobRunr's "extract
    the parameters from the body of a lambda function" logic, but since our test suite
    was always stubbing out JobRunr, we only discovered the problem at runtime.
    
    Add a simple sanity-check test to try to detect future incidents before they get
    merged.

---
+++ src/test/kotlin/com/terraformation/backend/integration/JobRunrCompatibilityTest.kt
@@ -0,0 +1,38 @@
+package com.terraformation.backend.integration
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.terraformation.backend.db.DatabaseTest
+import java.time.Duration
+import javax.sql.DataSource
+import org.jobrunr.jobs.mappers.JobMapper
+import org.jobrunr.scheduling.JobScheduler
+import org.jobrunr.storage.sql.postgres.PostgresStorageProvider
+import org.jobrunr.utils.mapper.jackson.JacksonJsonMapper
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+
+/**
+ * Sanity check to make sure JobRunr doesn't break with the other dependency versions we're using.
+ * This isn't intended to test that JobRunr's functionality is as advertised, just to make sure it
+ * doesn't blow up when we do things like upgrade to new Kotlin versions.
+ */
+internal class JobRunrCompatibilityTest : DatabaseTest() {
+  @Autowired private lateinit var dataSource: DataSource
+
+  private val scheduler by lazy {
+    val storageProvider = PostgresStorageProvider(dataSource)
+    storageProvider.setJobMapper(JobMapper(JacksonJsonMapper(jacksonObjectMapper())))
+    JobScheduler(storageProvider)
+  }
+
+  private val jobId = javaClass.simpleName
+
+  @Test
+  fun `can schedule a recurrent job using a lambda function`() {
+    scheduler.scheduleRecurrently<JobRunrCompatibilityTest>(jobId, Duration.ofDays(1)) {
+      doNothing()
+    }
+  }
+
+  fun doNothing() {}
+}
