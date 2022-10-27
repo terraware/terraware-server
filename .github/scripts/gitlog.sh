748c1618fcde4dd8668e0812355f490e299bb9f4
Alex <alex@terraformation.com>
2022-10-27 11:17:46 -0700

    Just get last created tag that matches and use that

--- .github/scripts/gitlog.sh
+++ .github/scripts/gitlog.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-LAST_TAG_DESCRIBE=`for tag in $(git tag | grep -E 'v[0-9]+\.[0-9]'); do
-    git describe --tags --long --match="$tag"
-done | sort -k2 -t"-" -n | head -n1`
-
-LAST_TAG=${LAST_TAG_DESCRIBE%%-*}  # retain the part before the first dash
-HEAD=${LAST_TAG_DESCRIBE##*g}
+LAST_TAG=`git tag --list --sort=creatordate 'v[0-9]*' | tail -n1`
 
 git log $LAST_TAG..HEAD --oneline > docs/unreleased.log
