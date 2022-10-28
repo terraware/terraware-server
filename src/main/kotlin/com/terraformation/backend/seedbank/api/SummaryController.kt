97df4496f11de7afedb3a6f9effc14fca6aba75b
Steven Grimm <steven@terraformation.com>
2022-10-28 14:11:06 -0700

    SW-1842 Exact-then-fuzzy logic for search summaries
    Commit ee97dd57 updated the search code so that if you do a fuzzy search, it first
    looks for exact matches and returns those if there are any. This had the desired
    effect of letting people type exact accession numbers into the accession search
    UI, but the summary statistics were still based on the fuzzy matches.
    
    Update the summary endpoint so it uses the same "try an exact search first, and
    fall back to fuzzy if that doesn't return results" logic so the summary stats will
    match the detailed search results.

--- src/main/kotlin/com/terraformation/backend/seedbank/api/SummaryController.kt
+++ src/main/kotlin/com/terraformation/backend/seedbank/api/SummaryController.kt
@@ -2,12 +2,12 @@ package com.terraformation.backend.seedbank.api
 
 import com.terraformation.backend.api.SeedBankAppEndpoint
 import com.terraformation.backend.api.SuccessResponsePayload
-import com.terraformation.backend.config.TerrawareServerConfig
 import com.terraformation.backend.db.default_schema.FacilityId
 import com.terraformation.backend.db.default_schema.OrganizationId
 import com.terraformation.backend.db.seedbank.AccessionId
 import com.terraformation.backend.db.seedbank.AccessionState
 import com.terraformation.backend.search.SearchFieldPrefix
+import com.terraformation.backend.search.SearchNode
 import com.terraformation.backend.search.SearchService
 import com.terraformation.backend.search.api.HasSearchNode
 import com.terraformation.backend.search.api.SearchNodePayload
@@ -16,7 +16,6 @@ import com.terraformation.backend.seedbank.db.AccessionStore
 import com.terraformation.backend.seedbank.model.AccessionSummaryStatistics
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Schema
-import java.time.Clock
 import org.jooq.Record1
 import org.jooq.Select
 import org.springframework.web.bind.annotation.GetMapping
@@ -31,8 +30,6 @@ import org.springframework.web.bind.annotation.RestController
 @SeedBankAppEndpoint
 class SummaryController(
     private val accessionStore: AccessionStore,
-    private val clock: Clock,
-    private val config: TerrawareServerConfig,
     private val searchService: SearchService,
     tables: SearchTables,
 ) {
@@ -68,17 +65,32 @@ class SummaryController(
   fun summarizeAccessionSearch(
       @RequestBody payload: SummarizeAccessionSearchRequestPayload
   ): SummarizeAccessionSearchResponsePayload {
+    val originalCriteria = payload.toSearchNode(accessionsPrefix)
+
+    // If there are fuzzy search criteria, first see if we get results by treating them as exact
+    // matches. This mirrors the behavior of SearchService.search(), such that the summary data
+    // will be consistent with the search results.
+    val exactCriteria = originalCriteria.toExactSearch()
+    val exactSummary = querySummary(exactCriteria)
+
+    val summary =
+        if (exactSummary.isNonZero() || exactCriteria == originalCriteria) {
+          exactSummary
+        } else {
+          querySummary(originalCriteria)
+        }
+
+    return SummarizeAccessionSearchResponsePayload(summary)
+  }
+
+  private fun querySummary(criteria: SearchNode): AccessionSummaryStatistics {
     @Suppress("UNCHECKED_CAST")
     val query =
         searchService
-            .buildQuery(
-                accessionsPrefix, listOf(accessionIdField), payload.toSearchNode(accessionsPrefix))
+            .buildQuery(accessionsPrefix, listOf(accessionIdField), criteria)
             .toSelectQuery() as Select<Record1<AccessionId?>>
 
-    val summary = accessionStore.getSummaryStatistics(query)
-
-    return SummarizeAccessionSearchResponsePayload(
-        summary.accessions, summary.species, SeedCountSummaryPayload(summary))
+    return accessionStore.getSummaryStatistics(query)
   }
 
   private fun getSummary(facilityId: FacilityId): SummaryResponsePayload {
@@ -154,4 +166,8 @@ data class SummarizeAccessionSearchResponsePayload(
     val accessions: Int,
     val species: Int,
     val seedsRemaining: SeedCountSummaryPayload,
-) : SuccessResponsePayload
+) : SuccessResponsePayload {
+  constructor(
+      summary: AccessionSummaryStatistics
+  ) : this(summary.accessions, summary.species, SeedCountSummaryPayload(summary))
+}
--- src/main/kotlin/com/terraformation/backend/seedbank/model/AccessionSummaryStatistics.kt
+++ src/main/kotlin/com/terraformation/backend/seedbank/model/AccessionSummaryStatistics.kt
@@ -24,4 +24,14 @@ data class AccessionSummaryStatistics(
       totalSeedsRemaining = subtotalBySeedCount.toLong() + subtotalByWeightEstimate.toLong(),
       unknownQuantityAccessions = unknownQuantityAccessions.toInt(),
   )
+
+  /** Returns true if any of the fields have nonzero values. */
+  fun isNonZero(): Boolean {
+    return accessions != 0 ||
+        species != 0 ||
+        subtotalBySeedCount != 0L ||
+        subtotalByWeightEstimate != 0L ||
+        totalSeedsRemaining != 0L ||
+        unknownQuantityAccessions != 0
+  }
 }
