ed496dd198c7eadf31ac0a5c15a3ccccdd0551e3
Steven Grimm <sgrimm@users.noreply.github.com>
2022-11-11 11:42:36 -0800

    SW-2230 Add count fields for planting zones and plots (#793)
    The UI needs to show the number of planting zones and plots in each planting
    site as searchable, sortable table fields. Expose the counts as numeric fields
    in the search API.

---
+++ src/main/resources/db/migration/V154__PlantingSiteSummaries.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE VIEW tracking.planting_site_summaries AS
+SELECT id,
+       organization_id,
+       name,
+       description,
+       boundary,
+       created_by,
+       created_time,
+       modified_by,
+       modified_time,
+       (SELECT COUNT(*)
+        FROM tracking.planting_zones pz
+        WHERE ps.id = pz.planting_site_id) AS num_planting_zones,
+       (SELECT COUNT(*)
+        FROM tracking.planting_zones pz
+                 JOIN tracking.plots p ON pz.id = p.planting_zone_id
+        WHERE ps.id = pz.planting_site_id) AS num_plots
+FROM tracking.planting_sites ps;
