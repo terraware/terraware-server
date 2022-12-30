fb2bcc96033b809767aefccc305759f62f523634
Steven Grimm <steven@terraformation.com>
2022-12-30 13:05:25 -0800

    Rework API endpoint to make it more extensible; add OpenAPI schema docs

--- src/main/kotlin/com/terraformation/backend/i18n/api/TimeZonesController.kt
+++ src/main/kotlin/com/terraformation/backend/i18n/api/TimeZonesController.kt
@@ -3,6 +3,7 @@ package com.terraformation.backend.i18n.api
 import com.terraformation.backend.api.CustomerEndpoint
 import com.terraformation.backend.api.SuccessResponsePayload
 import com.terraformation.backend.i18n.TimeZones
+import io.swagger.v3.oas.annotations.media.Schema
 import java.time.ZoneId
 import java.util.Locale
 import org.springframework.util.StringUtils
@@ -12,17 +13,34 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @CustomerEndpoint
-@RequestMapping("/api/v1/i18n/tz")
+@RequestMapping("/api/v1/i18n/timeZones")
 @RestController
 class TimeZonesController(private val timeZones: TimeZones) {
-  @GetMapping("/names")
+  @GetMapping
   fun listTimeZoneNames(
-      @RequestParam("locale") localeName: String?
+      @RequestParam("locale")
+      @Schema(
+          description = "Language code and optional country code suffix.",
+          defaultValue = "en_US",
+          example = "zh_CN")
+      localeName: String?
   ): ListTimeZoneNamesResponsePayload {
-    val locale = localeName?.let { StringUtils.parseLocaleString(it) } ?: Locale.ENGLISH
-    return ListTimeZoneNamesResponsePayload(timeZones.getTimeZoneNames(locale))
+    val locale = localeName?.let { StringUtils.parseLocaleString(it) } ?: Locale.US
+    val payloads = timeZones.getTimeZoneNames(locale).map { TimeZonePayload(it.key, it.value) }
+    return ListTimeZoneNamesResponsePayload(payloads)
   }
 }
 
-data class ListTimeZoneNamesResponsePayload(val timeZones: Map<ZoneId, String>) :
-    SuccessResponsePayload
+data class TimeZonePayload(
+    val id: ZoneId,
+    @Schema(
+        description =
+            "Long name of time zone, possibly including a city name. This name is guaranteed to " +
+                "be unique across all zones.",
+        example = "Central European Time - Berlin")
+    val longName: String
+)
+
+data class ListTimeZoneNamesResponsePayload(
+    val timeZones: List<TimeZonePayload>,
+) : SuccessResponsePayload
