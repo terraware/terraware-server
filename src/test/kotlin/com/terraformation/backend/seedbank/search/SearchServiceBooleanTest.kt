8ae6bc6e5dc1b69b667372175828321773f26c14
Steven Grimm <steven@terraformation.com>
2023-02-08 10:34:32 -0800

    SW-2925 Localize boolean search fields
    Boolean search fields were using the English words "true" and "false" rather than
    treating them the same as enum fields and using localized strings.

---
+++ src/test/kotlin/com/terraformation/backend/seedbank/search/SearchServiceBooleanTest.kt
@@ -0,0 +1,46 @@
+package com.terraformation.backend.seedbank.search
+
+import com.terraformation.backend.i18n.Locales
+import com.terraformation.backend.i18n.toGibberish
+import com.terraformation.backend.i18n.use
+import com.terraformation.backend.search.FieldNode
+import com.terraformation.backend.search.NoConditionNode
+import com.terraformation.backend.search.SearchFieldPrefix
+import com.terraformation.backend.search.SearchResults
+import com.terraformation.backend.search.SearchSortField
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class SearchServiceBooleanTest : SearchServiceTest() {
+  @Test
+  fun `returns localized boolean values`() {
+    val prefix = SearchFieldPrefix(tables.species)
+    val fields = listOf(prefix.resolve("id"), prefix.resolve("endangered"), prefix.resolve("rare"))
+    val sortOrder = fields.map { SearchSortField(it) }
+
+    val result =
+        Locales.GIBBERISH.use { searchService.search(prefix, fields, NoConditionNode(), sortOrder) }
+
+    val expected =
+        SearchResults(
+            listOf(
+                mapOf("id" to "10000", "rare" to "false".toGibberish()),
+                mapOf("id" to "10001", "endangered" to "true".toGibberish())),
+            cursor = null)
+
+    assertEquals(expected, result)
+  }
+
+  @Test
+  fun `accepts localized boolean values as search criteria`() {
+    val prefix = SearchFieldPrefix(tables.species)
+    val fields = listOf(prefix.resolve("id"))
+    val criteria = FieldNode(prefix.resolve("endangered"), listOf("true".toGibberish()))
+
+    val result = Locales.GIBBERISH.use { searchService.search(prefix, fields, criteria) }
+
+    val expected = SearchResults(listOf(mapOf("id" to "10001")), cursor = null)
+
+    assertEquals(expected, result)
+  }
+}
