9b6805e50c7e708d069f62b2e66b756321b0b3e6
Steven Grimm <steven@terraformation.com>
2023-02-09 13:20:13 -0800

    SW-2933 Use French number formatting for gibberish
    Replace the custom separator characters in gibberish number formatting with the
    French ones. This eases implementation on the client side, where it's harder to
    define custom locales with custom formatting rules.

--- src/test/kotlin/com/terraformation/backend/i18n/GibberishDecimalFormatSymbolsProviderTest.kt
+++ src/test/kotlin/com/terraformation/backend/i18n/GibberishDecimalFormatSymbolsProviderTest.kt
@@ -1,7 +1,7 @@
 package com.terraformation.backend.i18n
 
 import java.text.NumberFormat
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class GibberishDecimalFormatSymbolsProviderTest {
@@ -9,7 +9,8 @@ class GibberishDecimalFormatSymbolsProviderTest {
   fun `uses correct punctuation in number formatting`() {
     val formatter = NumberFormat.getNumberInstance(Locales.GIBBERISH)
 
-    val expected = "123&456,789"
+    val narrowNonBreakingSpace = '\u202f'
+    val expected = "123${narrowNonBreakingSpace}456,789"
     val actual = formatter.format(123456.789)
 
     assertEquals(expected, actual)
