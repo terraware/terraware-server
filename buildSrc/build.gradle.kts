0373e09fde6d4d9fd00b2cc67d836f312e7056bb
Steven Grimm <steven@terraformation.com>
2023-02-15 10:00:05 -0800

    Upgrade to Gradle 8.1.1
    Upgrade to the latest version of Gradle. Among other things, it uses a newer
    version of Kotlin to execute the build scripts; as a result, our custom Gradle
    plugins need to be updated to stop calling Kotlin standard library functions
    that are now deprecated.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,12 +1,4 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins { `kotlin-dsl` }
-
-// Needed to avoid noisy warning message from Gradle. Won't be needed in Gradle 7.5.
-// https://github.com/gradle/gradle/issues/18935 https://github.com/gradle/gradle/issues/19308
-java.targetCompatibility = JavaVersion.VERSION_1_8
-
-tasks.withType<KotlinCompile> { kotlinOptions.jvmTarget = java.targetCompatibility.majorVersion }
 
 repositories {
   gradlePluginPortal()
