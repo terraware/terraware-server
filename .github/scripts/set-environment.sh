85abf9947e6f94cb3bbc63357d00921f16e585d8
Steven Grimm <sgrimm@users.noreply.github.com>
2023-05-25 08:58:54 -0700

    SW-1031 Run tests that depend on external services (#1176)
    By default, we skip tests like `S3FileStoreTest` in CI because we don't want our
    CI builds to fail if the AWS API is having a glitch that's unrelated to the
    changes we're making to our code. But that means we don't detect problems in
    the code under test, or, more commonly, problems caused by upgrades of the client
    libraries we use to talk to those services.
    
    Add a build step that runs those tests (which are now required to have class names
    ending in `ExternalTest` but doesn't cause the whole workflow to fail if they
    don't pass. The failures will still be visible in the build log, so we can decide
    on a case-by-case basis whether we care about them.

--- .github/scripts/set-environment.sh
+++ .github/scripts/set-environment.sh
@@ -12,24 +12,22 @@ if [[ "$GITHUB_REF" =~ refs/tags/v[0-9]+\.[0-9.]+ ]]; then
 elif [[ "$GITHUB_REF" == refs/heads/main ]]; then
     TIER=STAGING
     IS_CD=true
-elif [[ "$GITHUB_REF" == refs/heads/qa ]]; then
-    TIER=QA
-    IS_CD=true
 else
+    TIER=CI
     IS_CD=false
 fi
 
 (
     echo "IS_CD=$IS_CD"
     echo "COMMIT_SHA=$commit_sha"
+    echo "AWS_REGION_SECRET_NAME=${TIER}_AWS_REGION"
+    echo "AWS_ROLE_SECRET_NAME=${TIER}_AWS_ROLE"
 
     if [[ "$IS_CD" == true ]]; then
-        echo "TIER=$TIER"
         echo "DOCKER_TAGS=${docker_image}:$commit_sha,${docker_image}:${TIER}"
+        echo "TIER=$TIER"
 
         # Define secret names based on the tier
-        echo "AWS_REGION_SECRET_NAME=${TIER}_AWS_REGION"
-        echo "AWS_ROLE_SECRET_NAME=${TIER}_AWS_ROLE"
         echo "SSH_CONFIG_SECRET_NAME=${TIER}_SSH_CONFIG"
         echo "SSH_KEY_SECRET_NAME=${TIER}_SSH_KEY"
     fi
