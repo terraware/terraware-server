4271187a0f29bb38348d2768913e45750574bc9e
Steven Grimm <steven@terraformation.com>
2023-05-25 06:47:29 -0700

    Use staging AWS credentials for non-CD runs

--- .github/scripts/set-environment.sh
+++ .github/scripts/set-environment.sh
@@ -5,15 +5,13 @@ set -euo pipefail
 commit_sha="${GITHUB_SHA:0:12}"
 docker_image='terraware/terraware-server'
 
+TIER=STAGING
+
 # Define tier based on branch ref
 if [[ "$GITHUB_REF" =~ refs/tags/v[0-9]+\.[0-9.]+ ]]; then
     TIER=PROD
     IS_CD=true
 elif [[ "$GITHUB_REF" == refs/heads/main ]]; then
-    TIER=STAGING
-    IS_CD=true
-elif [[ "$GITHUB_REF" == refs/heads/qa ]]; then
-    TIER=QA
     IS_CD=true
 else
     IS_CD=false
@@ -22,14 +20,14 @@ fi
 (
     echo "IS_CD=$IS_CD"
     echo "COMMIT_SHA=$commit_sha"
+    echo "AWS_REGION_SECRET_NAME=${TIER}_AWS_REGION"
+    echo "AWS_ROLE_SECRET_NAME=${TIER}_AWS_ROLE"
 
     if [[ "$IS_CD" == true ]]; then
-        echo "TIER=$TIER"
         echo "DOCKER_TAGS=${docker_image}:$commit_sha,${docker_image}:${TIER}"
+        echo "TIER=$TIER"
 
         # Define secret names based on the tier
-        echo "AWS_REGION_SECRET_NAME=${TIER}_AWS_REGION"
-        echo "AWS_ROLE_SECRET_NAME=${TIER}_AWS_ROLE"
         echo "SSH_CONFIG_SECRET_NAME=${TIER}_SSH_CONFIG"
         echo "SSH_KEY_SECRET_NAME=${TIER}_SSH_KEY"
     fi
