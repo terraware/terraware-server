5b070af4b47e1daf2a190e5fee12ce93cc861f49
Steven Grimm <sgrimm@users.noreply.github.com>
2023-06-15 09:19:11 -0700

    SW-3348 Automatically log out incompatible sessions (#1214)
    When we make a non-backward-compatible change to one of the classes that is
    stored as part of the user's login session data, treat the session as
    nonexistent instead of bombing out with an internal error.
    
    In practice, this will often be invisible to the user: they will be treated
    as unauthenticated, so they'll be redirected to Keycloak. If Keycloak still
    considers them logged in, they'll be issued a new access token and redirected
    straight back to the web app which will then create a fresh session for them.
    
    This is in preparation for moving away from the Keycloak adapter library; when
    we switch away from it, sessions that include serialized objects from that
    library will no longer be deserializable.

---
+++ src/main/kotlin/com/terraformation/backend/auth/SessionConfig.kt
@@ -0,0 +1,59 @@
+package com.terraformation.backend.auth
+
+import com.terraformation.backend.log.perClassLogger
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.convert.converter.Converter
+import org.springframework.core.convert.support.GenericConversionService
+import org.springframework.core.serializer.DefaultDeserializer
+import org.springframework.core.serializer.support.SerializingConverter
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository
+import org.springframework.session.config.SessionRepositoryCustomizer
+import org.springframework.session.jdbc.JdbcIndexedSessionRepository
+import org.springframework.session.jdbc.PostgreSqlJdbcIndexedSessionRepositoryCustomizer
+
+@Configuration
+class SessionConfig {
+  /**
+   * Make the default security context repository available as an injectable dependency so our
+   * application code can manipulate session data.
+   */
+  @Bean fun securityContextRepository() = HttpSessionSecurityContextRepository()
+
+  /**
+   * Uses PostgreSQL-specific SQL when inserting session attributes to avoid primary key collisions
+   * when two requests arrive for a brand-new session at the same time.
+   */
+  @Bean fun jdbcSessionRepositoryCustomizer() = PostgreSqlJdbcIndexedSessionRepositoryCustomizer()
+
+  @Bean
+  fun sessionRepositoryCustomizer(): SessionRepositoryCustomizer<JdbcIndexedSessionRepository> {
+    val service = GenericConversionService()
+    service.addConverter(Any::class.java, ByteArray::class.java, SerializingConverter())
+    service.addConverter(ByteArray::class.java, Any::class.java, SessionInvalidatingConverter())
+
+    return SessionRepositoryCustomizer { repository -> repository.setConversionService(service) }
+  }
+
+  /**
+   * Deserializer for session data that treats deserialization failures as missing sessions rather
+   * than as internal errors. With this class, we don't need to explicitly delete active login
+   * sessions when we upgrade Spring and there's a backward-incompatible change to one of the
+   * authentication objects that gets serialized as part of the session data. Without this class,
+   * existing sessions that aren't serialization-compatible with the current version of the code
+   * cause HTTP 500 responses.
+   */
+  class SessionInvalidatingConverter : Converter<ByteArray, Any> {
+    private val deserializer = DefaultDeserializer(javaClass.classLoader)
+    private val log = perClassLogger()
+
+    override fun convert(source: ByteArray): Any? {
+      return try {
+        deserializer.deserialize(source.inputStream())
+      } catch (e: Exception) {
+        log.warn("Failed to deserialize session data; will start a new session")
+        null
+      }
+    }
+  }
+}
