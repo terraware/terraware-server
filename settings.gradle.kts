972b0877de8f7e69b4eac6b3a68ac4bec418a5f6
Steven Grimm <sgrimm@users.noreply.github.com>
2023-07-06 11:19:09 -0700

    Update build scripts for Gradle 8.2 (#1282)
    - Use property assignment rather than `.set()` (technically still an incubating
      feature but it's enabled by default in 8.2)
    - Upgrade a couple plugins to newer versions that don't call newly-deprecated
      Gradle APIs
    - Enable "all warnings as errors" compilation mode on build scripts
    - Remove a null check that is no longer necessary

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ pluginManagement {
 
 plugins {
   // Automatically downloads the correct JDK version if it's not installed locally.
-  id("org.gradle.toolchains.foojay-resolver-convention") version ("0.4.0")
+  id("org.gradle.toolchains.foojay-resolver-convention") version ("0.5.0")
 }
 
 rootProject.name = "terraware-server"
