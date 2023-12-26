4d716ac3b0c46460592e12bc2f7dd7dc2dbd6173
Steven Grimm <sgrimm@users.noreply.github.com>
2023-12-26 13:32:33 -0800

    SW-4595 Fix incorrect apostrophes (#1589)
    ASCII apostrophe characters are special in properties files and have to be doubled
    to escape them. But some text editors "helpfully" detect pairs of apostrophes and
    turn the second one into a closing single quote mark. Then Phrase, or other
    software, notices the remaining apostrophe and doubles it again, such that we end
    up with strings that have an ASCII apostophe and a single quote together.
    
    We also had a couple sentences that should have used the word "its" but instead
    used "it’s."
    
    Remove the unnecessary single quote characters and add a test to catch the problem
    in the future.

---
+++ src/test/kotlin/com/terraformation/backend/i18n/PropertiesFilesTest.kt
@@ -0,0 +1,40 @@
+package com.terraformation.backend.i18n
+
+import java.util.Locale
+import java.util.ResourceBundle
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+
+class PropertiesFilesTest {
+  @MethodSource("getMessageBundles")
+  @ParameterizedTest
+  fun `mixed single quote styles`(bundleName: String) {
+    val bundle = ResourceBundle.getBundle(bundleName, Locale.ENGLISH)
+    val invalidKeys =
+        bundle.keys
+            .asSequence()
+            .filter { key ->
+              val value = bundle.getString(key)
+              value.contains('\'') && value.contains('’')
+            }
+            .toList()
+
+    assertEquals(
+        emptyList<String>(),
+        invalidKeys,
+        "String contains both an ASCII apostrophe and a closing single quote")
+  }
+
+  companion object {
+    @JvmStatic
+    fun getMessageBundles() =
+        listOf(
+            "i18n.Countries",
+            "i18n.CountrySubdivisions",
+            "i18n.Enums",
+            "i18n.Messages",
+            "i18n.TimeZoneWithoutCity",
+        )
+  }
+}
