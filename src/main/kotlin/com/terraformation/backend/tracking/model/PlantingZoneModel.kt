c007c830821fa6d36c9272f69eb03ed67ecc9c7a
Steven Grimm <steven@terraformation.com>
2024-01-23 16:42:50 -0800

    Use more precise recursive drill-down base case

--- src/main/kotlin/com/terraformation/backend/tracking/model/PlantingZoneModel.kt
+++ src/main/kotlin/com/terraformation/backend/tracking/model/PlantingZoneModel.kt
@@ -185,6 +185,19 @@ data class PlantingZoneModel(
                 Coordinate(west, north),
                 Coordinate(west, south)))
 
+    fun roundToGrid(value: Double): Double = Math.round(value / gridInterval) * gridInterval
+
+    /**
+     * Returns a square [sizeMeters] on a side with the coordinates snapped to multiples of
+     * [gridInterval].
+     */
+    fun gridAlignedSquare(west: Double, south: Double): Polygon {
+      val roundedWest = roundToGrid(west)
+      val roundedSouth = roundToGrid(south)
+      return rectangle(
+          roundedWest, roundedSouth, roundedWest + sizeMeters, roundedSouth + sizeMeters)
+    }
+
     /**
      * Attempts to find an available square within a rectangular region of the zone. First tries to
      * pick a few random points, and if none of them works, divides the region into four quadrants
@@ -195,26 +208,42 @@ data class PlantingZoneModel(
      * an exhaustive search for matches.
      */
     fun findInRegion(southwest: Coordinate, northeast: Coordinate): Polygon? {
-      val maxAttempts = 5
+      val regionPolygon = rectangle(southwest.x, southwest.y, northeast.x, northeast.y)
 
-      // Try a few times to find a random spot in the region. This will succeed most of the time.
-      for (attemptNumber in 1..maxAttempts) {
-        val squareWest =
-            Math.round(Random.nextDouble(southwest.x, northeast.x) / gridInterval) * gridInterval
-        val squareSouth =
-            Math.round(Random.nextDouble(southwest.y, northeast.y) / gridInterval) * gridInterval
-        val squareEast = squareWest + sizeMeters
-        val squareNorth = squareSouth + sizeMeters
+      // If all possible points in the region will round to the same point on the grid, or if the
+      // region is less than a quarter the size of a grid square, check it and return; trying random
+      // points within the region wouldn't be useful, nor would dividing the region in quarters and
+      // drilling down.
+      val minimumArea = (gridInterval * gridInterval) / 4.0
+      if (roundToGrid(southwest.x) == roundToGrid(northeast.x) &&
+          roundToGrid(southwest.y) == roundToGrid(northeast.y) ||
+          regionPolygon.area < minimumArea) {
+        val polygon = gridAlignedSquare(southwest.x, southwest.y)
 
-        val square = rectangle(squareWest, squareSouth, squareEast, squareNorth)
-
-        if (square.coveredBy(zoneGeometry)) {
-          return JTS.transform(square, toOriginalCrs) as Polygon
+        return if (polygon.coveredBy(zoneGeometry)) {
+          JTS.transform(polygon, toOriginalCrs) as Polygon
+        } else {
+          null
         }
       }
 
-      // No luck. This might be a sparse site map. Split the region into quartiles and search them
-      // in random order but with the ones that cover the most zone area more likely to come first.
+      // Try a few times to find a random spot in the region. This will succeed most of the time.
+      val maxAttempts = 5
+
+      for (attemptNumber in 1..maxAttempts) {
+        val polygon =
+            gridAlignedSquare(
+                Random.nextDouble(southwest.x, northeast.x),
+                Random.nextDouble(southwest.y, northeast.y))
+
+        if (polygon.coveredBy(zoneGeometry)) {
+          return JTS.transform(polygon, toOriginalCrs) as Polygon
+        }
+      }
+
+      // No luck. This might be a sparse site map. Split the region into equal-sized quadrants and
+      // search them in random order but with the ones that cover the most zone area more likely to
+      // come first.
       val middleX = (southwest.x + northeast.x) / 2.0
       val middleY = (southwest.y + northeast.y) / 2.0
 
@@ -226,9 +255,8 @@ data class PlantingZoneModel(
                   rectangle(middleX, middleY, northeast.x, northeast.y),
                   rectangle(southwest.x, middleY, middleX, northeast.y))
               .map { quadrant -> quadrant to zoneGeometry.intersection(quadrant).area }
-              // Stop drilling down when we get to quadrants with 4 square meters of usable area
-              // since they're unlikely to round to different grid points than their parents.
-              .filter { it.second > 16.0 }
+              // Exclude quadrants that don't cover any zone area at all.
+              .filter { it.second > 0 }
               .toMutableList()
 
       while (quadrants.isNotEmpty()) {
