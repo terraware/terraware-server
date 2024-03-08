2bb2541ca931175572403e03c01678db46653a64
Steven Grimm <1248649+sgrimm@users.noreply.github.com>
2024-03-08 12:51:43 -0500

    SW-5000 Don't notify about "Needs Translation" status (#1729)
    When a submission goes from "In Review" to "Needs Translation" or back, the change
    isn't visible to end users. Suppress the notifications in that case.
    
    Include the project ID in the application event since there can be multiple
    projects in the same organization that submit the same deliverable.

--- src/main/kotlin/com/terraformation/backend/accelerator/event/DeliverableStatusUpdatedEvent.kt
+++ src/main/kotlin/com/terraformation/backend/accelerator/event/DeliverableStatusUpdatedEvent.kt
@@ -1,10 +1,33 @@
 package com.terraformation.backend.accelerator.event
 
 import com.terraformation.backend.db.accelerator.DeliverableId
-import com.terraformation.backend.db.default_schema.OrganizationId
+import com.terraformation.backend.db.accelerator.SubmissionStatus
+import com.terraformation.backend.db.default_schema.ProjectId
 
 /** Published when a deliverable status is updated */
 data class DeliverableStatusUpdatedEvent(
     val deliverableId: DeliverableId,
-    val organizationId: OrganizationId,
-)
+    val projectId: ProjectId,
+    val oldStatus: SubmissionStatus,
+    val newStatus: SubmissionStatus,
+) {
+  /**
+   * Returns true if the transition is visible to end users, or false if it is a transition between
+   * internally-visible statuses that are presented as the same status to users.
+   */
+  fun isUserVisible(): Boolean {
+    return when (oldStatus) {
+      SubmissionStatus.InReview ->
+          when (newStatus) {
+            SubmissionStatus.NeedsTranslation -> false
+            else -> true
+          }
+      SubmissionStatus.NeedsTranslation ->
+          when (newStatus) {
+            SubmissionStatus.InReview -> false
+            else -> true
+          }
+      else -> true
+    }
+  }
+}
