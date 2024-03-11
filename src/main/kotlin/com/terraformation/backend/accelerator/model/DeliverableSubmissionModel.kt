57732921e4abf99c8f1ffb875ba70fdcd8719102
Steven Grimm <1248649+sgrimm@users.noreply.github.com>
2024-03-11 13:04:27 -0400

    SW-4952 Add DeliverableStore to query deliverables (#1738)
    Add a new store class with a method that queries the list of deliverables and
    their submissions with optional filter criteria.

---
+++ src/main/kotlin/com/terraformation/backend/accelerator/model/DeliverableSubmissionModel.kt
@@ -0,0 +1,34 @@
+package com.terraformation.backend.accelerator.model
+
+import com.terraformation.backend.db.accelerator.DeliverableCategory
+import com.terraformation.backend.db.accelerator.DeliverableId
+import com.terraformation.backend.db.accelerator.DeliverableType
+import com.terraformation.backend.db.accelerator.ParticipantId
+import com.terraformation.backend.db.accelerator.SubmissionId
+import com.terraformation.backend.db.accelerator.SubmissionStatus
+import com.terraformation.backend.db.default_schema.OrganizationId
+import com.terraformation.backend.db.default_schema.ProjectId
+import java.net.URI
+
+/**
+ * The combination of a deliverable for a particular project and possibly one of its submissions.
+ */
+data class DeliverableSubmissionModel(
+    val deliverableId: DeliverableId,
+    val submissionId: SubmissionId?,
+    val category: DeliverableCategory,
+    val descriptionHtml: String?,
+    val documents: List<SubmissionDocumentModel>,
+    val feedback: String?,
+    val internalComment: String?,
+    val name: String,
+    val organizationId: OrganizationId,
+    val organizationName: String,
+    val participantId: ParticipantId,
+    val participantName: String,
+    val projectId: ProjectId,
+    val projectName: String,
+    val status: SubmissionStatus,
+    val templateUrl: URI?,
+    val type: DeliverableType,
+)
