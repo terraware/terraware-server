41736f90a85796496e617fca86f0ee4fb4e80c73
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-03-13 22:47:34 -0700

    Removed enums artifacts

--- src/test/kotlin/com/terraformation/backend/db/SchemaDocsGenerator.kt
+++ src/test/kotlin/com/terraformation/backend/db/SchemaDocsGenerator.kt
@@ -145,7 +145,6 @@ class SchemaDocsGenerator : DatabaseTest() {
                   "submission_documents" to setOf(ALL, ACCELERATOR),
                   "submission_statuses" to setOf(ALL, ACCELERATOR),
                   "submissions" to setOf(ALL, ACCELERATOR),
-                  "vote_decisions" to setOf(ALL, ACCELERATOR),
                   "vote_options" to setOf(ALL, ACCELERATOR),
               ),
           "nursery" to
