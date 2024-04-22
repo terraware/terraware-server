f6f771de86b03ff3c73fadfccd59e2267f45cf57
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-04-22 15:18:39 -0700

    Updated module importer

--- src/main/kotlin/com/terraformation/backend/accelerator/db/ModulesImporter.kt
+++ src/main/kotlin/com/terraformation/backend/accelerator/db/ModulesImporter.kt
@@ -2,6 +2,7 @@ package com.terraformation.backend.accelerator.db
 
 import com.terraformation.backend.auth.currentUser
 import com.terraformation.backend.customer.model.requirePermissions
+import com.terraformation.backend.db.accelerator.CohortPhase
 import com.terraformation.backend.db.accelerator.ModuleId
 import com.terraformation.backend.db.accelerator.tables.references.MODULES
 import com.terraformation.backend.importer.processCsvFile
@@ -13,13 +14,19 @@ import org.jooq.DSLContext
 /** Imports the list of modules from a CSV file. */
 @Named
 class ModulesImporter(
-    private val clock: InstantSource,
-    private val dslContext: DSLContext,
+  private val clock: InstantSource,
+  private val dslContext: DSLContext,
 ) {
   companion object {
     private const val COLUMN_NAME = 0
     private const val COLUMN_ID = COLUMN_NAME + 1
-    private const val NUM_COLUMNS = COLUMN_ID + 1
+    private const val COLUMN_OVERVIEW = COLUMN_ID + 1
+    private const val COLUMN_PREPARATION_MATERIALS = COLUMN_OVERVIEW + 1
+    private const val COLUMN_ADDITIONAL_RESOURCES = COLUMN_PREPARATION_MATERIALS + 1
+    private const val COLUMN_LIVE_SESSION_INFO = COLUMN_ADDITIONAL_RESOURCES + 1
+    private const val COLUMN_ONE_ON_ONE_INFO = COLUMN_LIVE_SESSION_INFO + 1
+    private const val COLUMN_WORKSHOP_INFO = COLUMN_ONE_ON_ONE_INFO + 1
+    private const val NUM_COLUMNS = COLUMN_WORKSHOP_INFO + 1
   }
 
   fun importModules(inputStream: InputStream) {
@@ -37,6 +44,12 @@ class ModulesImporter(
 
         val moduleId = values[COLUMN_ID]?.toLongOrNull()?.let { ModuleId(it) }
         val name = values[COLUMN_NAME]
+        val overview = values[COLUMN_OVERVIEW]
+        val preparationMaterials = values[COLUMN_PREPARATION_MATERIALS]
+        val additionalResources = values[COLUMN_ADDITIONAL_RESOURCES]
+        val liveSessionInfo = values[COLUMN_LIVE_SESSION_INFO]
+        val oneOnOneInfo = values[COLUMN_ONE_ON_ONE_INFO]
+        val workshopInfo = values[COLUMN_WORKSHOP_INFO]
 
         if (moduleId == null) {
           addError("Missing or invalid module ID")
@@ -51,6 +64,13 @@ class ModulesImporter(
                 .insertInto(MODULES)
                 .set(ID, moduleId)
                 .set(NAME, name)
+                .set(OVERVIEW, overview)
+                .set(PREPARATION_MATERIALS, preparationMaterials)
+                .set(ADDITIONAL_RESOURCES, additionalResources)
+                .set(LIVE_SESSION_DESCRIPTION, liveSessionInfo)
+                .set(ONE_ON_ONE_SESSION_DESCRIPTION, oneOnOneInfo)
+                .set(WORKSHOP_DESCRIPTION, workshopInfo)
+                .set(PHASE_ID, CohortPhase.Phase1FeasibilityStudy)
                 .set(CREATED_BY, userId)
                 .set(CREATED_TIME, now)
                 .set(MODIFIED_BY, userId)
@@ -58,6 +78,13 @@ class ModulesImporter(
                 .onConflict()
                 .doUpdate()
                 .set(NAME, name)
+                .set(OVERVIEW, overview)
+                .set(PREPARATION_MATERIALS, preparationMaterials)
+                .set(ADDITIONAL_RESOURCES, additionalResources)
+                .set(LIVE_SESSION_DESCRIPTION, liveSessionInfo)
+                .set(ONE_ON_ONE_SESSION_DESCRIPTION, oneOnOneInfo)
+                .set(WORKSHOP_DESCRIPTION, workshopInfo)
+                .set(PHASE_ID, CohortPhase.Phase1FeasibilityStudy)
                 .set(MODIFIED_BY, userId)
                 .set(MODIFIED_TIME, now)
                 .execute()
