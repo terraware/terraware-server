0e0b7f234a8e89ee24b17f34b985bf5bcb6c5a12
Steven Grimm <steven@terraformation.com>
2024-04-24 16:54:37 -0700

    Add test to validate search field names
    Currently there's nothing stopping people from adding fields with invalid names to
    the search API. Add a test to check for some typical naming problems.

---
+++ src/test/kotlin/com/terraformation/backend/i18n/SearchFieldMetadataTest.kt
@@ -0,0 +1,72 @@
+package com.terraformation.backend.i18n
+
+import com.terraformation.backend.search.SearchTable
+import com.terraformation.backend.search.field.SearchField
+import com.terraformation.backend.search.table.SearchTables
+import java.time.Clock
+import kotlin.reflect.full.memberProperties
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SearchFieldMetadataTest {
+  private val messages = Messages()
+
+  @Test
+  fun `all search fields have English descriptions`() {
+    val missingFields =
+        mapAllSearchFields { table, field ->
+              try {
+                // This will throw an exception if there is no entry in the strings table
+                // for the field.
+                messages.searchFieldDisplayName(table.name, field.fieldName)
+                null
+              } catch (e: Exception) {
+                "search.${table.name}.${field.fieldName}="
+              }
+            }
+            .sorted()
+            .joinToString("\n")
+
+    assertEquals("", missingFields, "Missing search field descriptions")
+  }
+
+  @Test
+  fun `search field names are valid`() {
+    val invalidFields = mapAllSearchFields { table, field ->
+      val fieldName = field.fieldName
+      val tableName = table.name
+      if ('.' in fieldName || '_' in fieldName) {
+        "$fieldName in $tableName will be interpreted as a sublist reference; use camelCase instead of . or _"
+      } else if (fieldName[0].isUpperCase()) {
+        "$fieldName in $tableName should not be capitalized; use camelCase"
+      } else if (fieldName.isBlank()) {
+        "$tableName has a field with a blank name"
+      } else {
+        null
+      }
+    }
+
+    assertEquals(emptyList<String>(), invalidFields, "Found invalid field names")
+  }
+
+  @Test
+  fun `search field names are unique`() {
+    val duplicateFields =
+        mapAllSearchFields { table, field -> "${field.fieldName} in ${table.name}" }
+            .groupBy { it }
+            .mapValues { it.value.size }
+            .filterValues { it > 1 }
+            .keys
+
+    assertEquals(emptySet<String>(), duplicateFields, "Found duplicate field names")
+  }
+
+  private fun <T> mapAllSearchFields(func: (SearchTable, SearchField) -> T?): List<T> {
+    val tables = SearchTables(Clock.systemUTC())
+
+    return SearchTables::class
+        .memberProperties
+        .mapNotNull { it.get(tables) as? SearchTable }
+        .flatMap { table -> table.fields.mapNotNull { func(table, it) } }
+  }
+}
