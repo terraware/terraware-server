9a2b16407cf883b2703209e03dd80e87a3b83611
Nick Graziano <nick@terraformation.com>
2024-04-29 10:45:48 -0600

    Combine species_growth_form creation table creation and species.growth_form migration into single migration file

--- src/main/resources/db/migration/0250/V265__SpeciesGrowthForm.sql
+++ src/main/resources/db/migration/0250/V265__SpeciesGrowthForm.sql
@@ -6,3 +6,11 @@ CREATE TABLE public.species_growth_forms (
 );
 
 CREATE INDEX ON public.species_growth_forms (species_id);
+
+INSERT INTO public.species_growth_forms (species_id, growth_form_id)
+SELECT id, growth_form_id
+FROM public.species
+WHERE growth_form_id IS NOT null
+ON CONFLICT DO NOTHING;
+
+ALTER TABLE public.species DROP COLUMN growth_form_id;
