430c55f1699d86f0f5073ea2f8b6d7dd19a46cea
Nicholas Graziano <nick.graziano@gmail.com>
2024-05-02 09:32:01 -0600

    SW-5318 ParticipantProjectSpecies store and model (#1918)
    - Adds store for `ParticipantProjectSpecies` with "find one" and "find all for project" functions
    - Delete/Update/Create will be in future PRs

---
+++ src/test/kotlin/com/terraformation/backend/accelerator/db/ParticipantProjectSpeciesStoreTest.kt
@@ -0,0 +1,105 @@
+package com.terraformation.backend.accelerator.db
+
+import com.terraformation.backend.RunsAsUser
+import com.terraformation.backend.accelerator.model.ExistingParticipantProjectSpeciesModel
+import com.terraformation.backend.db.DatabaseTest
+import com.terraformation.backend.db.accelerator.ParticipantProjectSpeciesId
+import com.terraformation.backend.db.accelerator.SubmissionStatus
+import com.terraformation.backend.mockUser
+import io.mockk.every
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class ParticipantProjectSpeciesStoreTest : DatabaseTest(), RunsAsUser {
+  override val user = mockUser()
+
+  private val store: ParticipantProjectSpeciesStore by lazy {
+    ParticipantProjectSpeciesStore(dslContext)
+  }
+
+  @BeforeEach
+  fun setUp() {
+    insertUser()
+    insertOrganization()
+
+    every { user.canReadParticipantProjectSpecies(any()) } returns true
+  }
+
+  @Nested
+  inner class FetchOneById {
+    @Test
+    fun `populates all fields and includes associated entities where applicable`() {
+      val participantId = insertParticipant()
+      val projectId = insertProject(participantId = participantId)
+      val speciesId = insertSpecies()
+      val participantProjectSpeciesId =
+          insertParticipantProjectSpecies(
+              feedback = "feedback",
+              projectId = projectId,
+              rationale = "rationale",
+              speciesId = speciesId)
+
+      assertEquals(
+          ExistingParticipantProjectSpeciesModel(
+              feedback = "feedback",
+              id = participantProjectSpeciesId,
+              projectId = projectId,
+              rationale = "rationale",
+              speciesId = speciesId,
+              submissionStatus = SubmissionStatus.NotSubmitted,
+          ),
+          store.fetchOneById(participantProjectSpeciesId))
+    }
+
+    @Test
+    fun `throws exception if no permission to read participant project species`() {
+      val participantId = insertParticipant()
+      val projectId = insertProject(participantId = participantId)
+      val speciesId = insertSpecies()
+      val participantProjectSpeciesId =
+          insertParticipantProjectSpecies(
+              feedback = "feedback",
+              projectId = projectId,
+              rationale = "rationale",
+              speciesId = speciesId)
+
+      every { user.canReadParticipantProjectSpecies(participantProjectSpeciesId) } returns false
+
+      assertThrows<ParticipantProjectSpeciesNotFoundException> {
+        store.fetchOneById(participantProjectSpeciesId)
+      }
+    }
+  }
+
+  @Nested
+  inner class FindAll {
+    @Test
+    fun `only includes participant project species the user has permission to read`() {
+      val participantId1 = insertParticipant()
+      val projectId1 = insertProject(participantId = participantId1)
+      val speciesId1 = insertSpecies()
+      val participantProjectSpeciesId1 =
+          insertParticipantProjectSpecies(projectId = projectId1, speciesId = speciesId1)
+
+      val participantId2 = insertParticipant()
+      val projectId2 = insertProject(participantId = participantId2)
+      val speciesId2 = insertSpecies()
+      val participantProjectSpeciesId2 =
+          insertParticipantProjectSpecies(projectId = projectId2, speciesId = speciesId2)
+
+      every { user.canReadParticipantProjectSpecies(participantProjectSpeciesId2) } returns false
+
+      assertEquals(
+          listOf(participantProjectSpeciesId1),
+          store.findAllForProject(projectId1).map { it.id },
+          "Participant Project Species IDs")
+      assertEquals(
+          emptyList<ParticipantProjectSpeciesId>(),
+          store.findAllForProject(projectId2).map { it.id },
+          "Participant Project Species IDs")
+    }
+  }
+}
