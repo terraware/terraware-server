1ba21337fa40f4889895f9ba17f008576f5dcea9
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-05-03 10:32:52 -0700

    Added modules options to cohort API (#1927)

--- src/main/kotlin/com/terraformation/backend/accelerator/api/CohortsController.kt
+++ src/main/kotlin/com/terraformation/backend/accelerator/api/CohortsController.kt
@@ -2,6 +2,8 @@ package com.terraformation.backend.accelerator.api
 
 import com.terraformation.backend.accelerator.db.CohortStore
 import com.terraformation.backend.accelerator.model.CohortDepth
+import com.terraformation.backend.accelerator.model.CohortModuleDepth
+import com.terraformation.backend.accelerator.model.CohortModuleModel
 import com.terraformation.backend.accelerator.model.ExistingCohortModel
 import com.terraformation.backend.accelerator.model.NewCohortModel
 import com.terraformation.backend.api.AcceleratorEndpoint
@@ -11,11 +13,15 @@ import com.terraformation.backend.api.SimpleSuccessResponsePayload
 import com.terraformation.backend.api.SuccessResponsePayload
 import com.terraformation.backend.db.accelerator.CohortId
 import com.terraformation.backend.db.accelerator.CohortPhase
+import com.terraformation.backend.db.accelerator.ModuleId
 import com.terraformation.backend.db.accelerator.ParticipantId
+import com.terraformation.backend.i18n.TimeZones
 import com.terraformation.backend.util.orNull
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.responses.ApiResponse
+import java.time.InstantSource
+import java.time.LocalDate
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -29,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController
 @AcceleratorEndpoint
 @RequestMapping("/api/v1/accelerator/cohorts")
 @RestController
-class CohortsController(private val cohortStore: CohortStore) {
+class CohortsController(private val clock: InstantSource, private val cohortStore: CohortStore) {
   @ApiResponse200
   @ApiResponse404
   @GetMapping("/{cohortId}")
@@ -41,10 +47,12 @@ class CohortsController(private val cohortStore: CohortStore) {
               "If specified, retrieve associated entities to the supplied depth. For example, " +
                   "'participant' depth will return the participants associated to the cohort.")
       @RequestParam
-      depth: CohortDepth = CohortDepth.Cohort
+      cohortDepth: CohortDepth = CohortDepth.Cohort,
+      cohortModuleDepth: CohortModuleDepth = CohortModuleDepth.Cohort,
   ): CohortResponsePayload {
-    val cohort = cohortStore.fetchOneById(cohortId, depth)
-    return CohortResponsePayload(CohortPayload(cohort))
+    val cohort = cohortStore.fetchOneById(cohortId, cohortDepth, cohortModuleDepth)
+    val today = LocalDate.ofInstant(clock.instant(), TimeZones.UTC)
+    return CohortResponsePayload(CohortPayload(cohort, today))
   }
 
   @ApiResponse200
@@ -56,10 +64,12 @@ class CohortsController(private val cohortStore: CohortStore) {
               "If specified, retrieve associated entities to the supplied depth. For example, " +
                   "'participant' depth will return the participants associated to the cohort.")
       @RequestParam
-      depth: CohortDepth = CohortDepth.Cohort
+      cohortDepth: CohortDepth = CohortDepth.Cohort,
+      cohortModuleDepth: CohortModuleDepth = CohortModuleDepth.Cohort,
   ): CohortListResponsePayload {
-    val cohortList = cohortStore.findAll(depth)
-    return CohortListResponsePayload(cohortList.map { CohortPayload(it) })
+    val cohortList = cohortStore.findAll(cohortDepth, cohortModuleDepth)
+    val today = LocalDate.ofInstant(clock.instant(), TimeZones.UTC)
+    return CohortListResponsePayload(cohortList.map { CohortPayload(it, today) })
   }
 
   @ApiResponse(responseCode = "200", description = "The cohort was created successfully.")
@@ -67,7 +77,8 @@ class CohortsController(private val cohortStore: CohortStore) {
   @Operation(summary = "Creates a new cohort.")
   fun createCohort(@RequestBody payload: CreateCohortRequestPayload): CohortResponsePayload {
     val insertedModel = cohortStore.create(payload.toModel())
-    return CohortResponsePayload(CohortPayload(insertedModel))
+    val today = LocalDate.ofInstant(clock.instant(), TimeZones.UTC)
+    return CohortResponsePayload(CohortPayload(insertedModel, today))
   }
 
   @ApiResponse200
@@ -92,16 +103,37 @@ class CohortsController(private val cohortStore: CohortStore) {
   }
 }
 
+data class CohortModule(
+    val id: ModuleId,
+    val title: String,
+    val startDate: LocalDate,
+    val endDate: LocalDate,
+    val isActive: Boolean,
+) {
+  constructor(
+      model: CohortModuleModel,
+      today: LocalDate,
+  ) : this(
+      id = model.moduleId,
+      title = model.title,
+      startDate = model.startDate,
+      endDate = model.endDate,
+      isActive = model.isActive(today))
+}
+
 data class CohortPayload(
     val id: CohortId,
+    val modules: List<CohortModule>,
     val name: String,
     val participantIds: Set<ParticipantId>?,
     val phase: CohortPhase
 ) {
   constructor(
-      cohort: ExistingCohortModel
+      cohort: ExistingCohortModel,
+      today: LocalDate,
   ) : this(
       id = cohort.id,
+      modules = cohort.modules.map { CohortModule(it, today) },
       name = cohort.name,
       participantIds = cohort.participantIds.orNull(),
       phase = cohort.phase,
