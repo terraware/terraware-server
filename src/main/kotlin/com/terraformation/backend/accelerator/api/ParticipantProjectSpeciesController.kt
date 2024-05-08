9643950adbaeb7f6d2776cd131c1e5960b40f9a9
Nicholas Graziano <nick.graziano@gmail.com>
2024-05-08 10:21:42 -0600

    SW-5318 Add endpoints for create and assign (create many) for ParticipantProjectSpecies (#1933)
    - POST `/api/v1/accelerator/projects/species` endpoint for creating a single entry
    - POST `/api/v1/accelerator/projects/species/assign` endpoint for bulk assigning species to projects

--- src/main/kotlin/com/terraformation/backend/accelerator/api/ParticipantProjectSpeciesController.kt
+++ src/main/kotlin/com/terraformation/backend/accelerator/api/ParticipantProjectSpeciesController.kt
@@ -1,9 +1,12 @@
 package com.terraformation.backend.accelerator.api
 
 import com.terraformation.backend.accelerator.db.ParticipantProjectSpeciesStore
+import com.terraformation.backend.accelerator.model.ExistingParticipantProjectSpeciesModel
+import com.terraformation.backend.accelerator.model.NewParticipantProjectSpeciesModel
 import com.terraformation.backend.api.AcceleratorEndpoint
 import com.terraformation.backend.api.ApiResponse200
 import com.terraformation.backend.api.ApiResponse404
+import com.terraformation.backend.api.SimpleSuccessResponsePayload
 import com.terraformation.backend.api.SuccessResponsePayload
 import com.terraformation.backend.db.accelerator.ParticipantProjectSpeciesId
 import com.terraformation.backend.db.accelerator.SubmissionStatus
@@ -12,6 +15,8 @@ import com.terraformation.backend.db.default_schema.SpeciesId
 import io.swagger.v3.oas.annotations.Operation
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -22,6 +27,36 @@ class ParticipantProjectSpeciesController(
     private val participantProjectSpeciesStore: ParticipantProjectSpeciesStore,
 ) {
   @ApiResponse200
+  @PostMapping("/assign")
+  @Operation(
+      summary =
+          "Creates a new participant project species entry for every project ID and species ID pairing.")
+  fun assignParticipantProjectSpecies(
+      @RequestBody payload: AssignParticipantProjectSpeciesPayload
+  ): SimpleSuccessResponsePayload {
+    participantProjectSpeciesStore.createMany(payload.projectIds, payload.speciesIds)
+    return SimpleSuccessResponsePayload()
+  }
+
+  @ApiResponse200
+  @PostMapping
+  @Operation(summary = "Creates a new participant project species entry.")
+  fun createParticipantProjectSpecies(
+      @RequestBody payload: CreateParticipantProjectSpeciesPayload
+  ): GetParticipantProjectSpeciesResponsePayload {
+    val model =
+        participantProjectSpeciesStore.create(
+            NewParticipantProjectSpeciesModel(
+                id = null,
+                projectId = payload.projectId,
+                rationale = payload.rationale,
+                speciesId = payload.speciesId,
+            ))
+
+    return makeGetResponse(model)
+  }
+
+  @ApiResponse200
   @ApiResponse404
   @GetMapping("/{participantProjectSpeciesId}")
   @Operation(summary = "Gets information about a participant project species.")
@@ -30,17 +65,33 @@ class ParticipantProjectSpeciesController(
   ): GetParticipantProjectSpeciesResponsePayload {
     val model = participantProjectSpeciesStore.fetchOneById(participantProjectSpeciesId)
 
-    return GetParticipantProjectSpeciesResponsePayload(
-        ParticipantProjectSpeciesPayload(
-            feedback = model.feedback,
-            id = model.id,
-            projectId = model.projectId,
-            rationale = model.rationale,
-            speciesId = model.speciesId,
-            submissionStatus = model.submissionStatus,
-        ))
+    return makeGetResponse(model)
   }
+
+  private fun makeGetResponse(
+      model: ExistingParticipantProjectSpeciesModel
+  ): GetParticipantProjectSpeciesResponsePayload =
+      GetParticipantProjectSpeciesResponsePayload(
+          ParticipantProjectSpeciesPayload(
+              feedback = model.feedback,
+              id = model.id,
+              projectId = model.projectId,
+              rationale = model.rationale,
+              speciesId = model.speciesId,
+              submissionStatus = model.submissionStatus,
+          ))
 }
+
+data class AssignParticipantProjectSpeciesPayload(
+    val projectIds: Set<ProjectId>,
+    val speciesIds: Set<SpeciesId>
+)
+
+data class CreateParticipantProjectSpeciesPayload(
+    val projectId: ProjectId,
+    val rationale: String?,
+    val speciesId: SpeciesId,
+)
 
 data class ParticipantProjectSpeciesPayload(
     val feedback: String?,
