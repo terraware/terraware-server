ce62dab7b7aa75b7fcb7b5ffe94729fff70e7b7d
Nick Graziano <nick@terraformation.com>
2024-05-14 13:06:23 -0600

    Add the new columns correctly so that previous entries are given correct values for the new columns

--- src/main/resources/db/migration/0250/V276__NewParticipantProjectSpeciesFields.sql
+++ src/main/resources/db/migration/0250/V276__NewParticipantProjectSpeciesFields.sql
@@ -1,4 +1,17 @@
 ALTER TABLE accelerator.participant_project_species ADD COLUMN created_by BIGINT REFERENCES users (id);
-ALTER TABLE accelerator.participant_project_species ADD COLUMN created_time TIMESTAMP WITH TIME ZONE NOT NULL;
 ALTER TABLE accelerator.participant_project_species ADD COLUMN modified_by BIGINT REFERENCES users (id);
-ALTER TABLE accelerator.participant_project_species ADD COLUMN modified_time TIMESTAMP WITH TIME ZONE NOT NULL;
+UPDATE accelerator.participant_project_species SET (created_by, modified_by) = (
+    SELECT organizations.created_by, organizations.modified_by
+    FROM organizations
+    LEFT JOIN species on species.organization_id = organizations.id
+    WHERE species.id = accelerator.participant_project_species.species_id
+);
+
+ALTER TABLE accelerator.participant_project_species ADD COLUMN created_time TIMESTAMP WITH TIME ZONE;
+ALTER TABLE accelerator.participant_project_species ADD COLUMN modified_time TIMESTAMP WITH TIME ZONE;
+
+UPDATE accelerator.participant_project_species SET created_time = NOW();
+UPDATE accelerator.participant_project_species SET modified_time = NOW();
+ALTER TABLE accelerator.participant_project_species ALTER COLUMN created_time SET NOT NULL;
+ALTER TABLE accelerator.participant_project_species ALTER COLUMN modified_time SET NOT NULL;
+
