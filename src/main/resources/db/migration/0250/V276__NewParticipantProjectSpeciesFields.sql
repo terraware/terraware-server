b651bfd7fd2636907eb5af60f1bfb852a1fb46b3
Nick Graziano <nick@terraformation.com>
2024-05-15 12:40:14 -0600

    Fix migration

--- src/main/resources/db/migration/0250/V276__NewParticipantProjectSpeciesFields.sql
+++ src/main/resources/db/migration/0250/V276__NewParticipantProjectSpeciesFields.sql
@@ -1,8 +1,8 @@
 ALTER TABLE accelerator.participant_project_species ADD COLUMN created_by BIGINT REFERENCES users (id);
 ALTER TABLE accelerator.participant_project_species ADD COLUMN modified_by BIGINT REFERENCES users (id);
-UPDATE accelerator.participant_project_species SET (created_by, modified_by) = (
-    SELECT id FROM users WHERE user_type_id = 4
-);
+
+UPDATE accelerator.participant_project_species SET created_by = (SELECT id FROM users WHERE user_type_id = 4);
+UPDATE accelerator.participant_project_species SET modified_by = (SELECT id FROM users WHERE user_type_id = 4);
 
 ALTER TABLE accelerator.participant_project_species ADD COLUMN created_time TIMESTAMP WITH TIME ZONE;
 ALTER TABLE accelerator.participant_project_species ADD COLUMN modified_time TIMESTAMP WITH TIME ZONE;
