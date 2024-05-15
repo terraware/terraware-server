6349fdad6134d084d1d5e585068c30716da047f6
Nick Graziano <nick@terraformation.com>
2024-05-15 12:34:04 -0600

    Update the new user ID fields in participant project species rows with the system user ID when running the migration

--- src/main/resources/db/migration/0250/V276__NewParticipantProjectSpeciesFields.sql
+++ src/main/resources/db/migration/0250/V276__NewParticipantProjectSpeciesFields.sql
@@ -1,10 +1,7 @@
 ALTER TABLE accelerator.participant_project_species ADD COLUMN created_by BIGINT REFERENCES users (id);
 ALTER TABLE accelerator.participant_project_species ADD COLUMN modified_by BIGINT REFERENCES users (id);
 UPDATE accelerator.participant_project_species SET (created_by, modified_by) = (
-    SELECT organizations.created_by, organizations.modified_by
-    FROM organizations
-    LEFT JOIN species on species.organization_id = organizations.id
-    WHERE species.id = accelerator.participant_project_species.species_id
+    SELECT id FROM users WHERE user_type_id = 4
 );
 
 ALTER TABLE accelerator.participant_project_species ADD COLUMN created_time TIMESTAMP WITH TIME ZONE;
