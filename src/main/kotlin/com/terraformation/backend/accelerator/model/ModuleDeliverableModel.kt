b199f19b1e64b28aec11af7659cac2b73dc68266
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-05-17 15:08:08 -0700

    Return deliverables not including submissions as part of module store

---
+++ src/main/kotlin/com/terraformation/backend/accelerator/model/ModuleDeliverableModel.kt
@@ -0,0 +1,37 @@
+package com.terraformation.backend.accelerator.model
+
+import com.terraformation.backend.db.accelerator.DeliverableCategory
+import com.terraformation.backend.db.accelerator.DeliverableId
+import com.terraformation.backend.db.accelerator.DeliverableType
+import com.terraformation.backend.db.accelerator.ModuleId
+import com.terraformation.backend.db.accelerator.tables.records.DeliverablesRecord
+import com.terraformation.backend.db.accelerator.tables.references.DELIVERABLES
+
+/** Deliverable data class without submission information. */
+data class ModuleDeliverableModel(
+    val id: DeliverableId,
+    val category: DeliverableCategory,
+    val descriptionHtml: String?,
+    val moduleId: ModuleId,
+    val name: String,
+    val position: Int,
+    val required: Boolean,
+    val sensitive: Boolean,
+    val type: DeliverableType,
+) {
+  companion object {
+    fun of(record: DeliverablesRecord): ModuleDeliverableModel =
+        with(DELIVERABLES) {
+          ModuleDeliverableModel(
+              id = record[ID]!!,
+              category = record[DELIVERABLE_CATEGORY_ID]!!,
+              descriptionHtml = record[DESCRIPTION_HTML],
+              moduleId = record[MODULE_ID]!!,
+              name = record[NAME]!!,
+              position = record[POSITION]!!,
+              required = record[IS_REQUIRED]!!,
+              sensitive = record[IS_SENSITIVE]!!,
+              type = record[DELIVERABLE_TYPE_ID]!!)
+        }
+  }
+}
--- src/test/kotlin/com/terraformation/backend/accelerator/db/ModuleStoreTest.kt
+++ src/test/kotlin/com/terraformation/backend/accelerator/db/ModuleStoreTest.kt
@@ -3,11 +3,14 @@ package com.terraformation.backend.accelerator.db
 import com.terraformation.backend.RunsAsUser
 import com.terraformation.backend.accelerator.model.CohortModuleModel
 import com.terraformation.backend.accelerator.model.EventModel
+import com.terraformation.backend.accelerator.model.ModuleDeliverableModel
 import com.terraformation.backend.accelerator.model.ModuleModel
 import com.terraformation.backend.db.DatabaseTest
 import com.terraformation.backend.db.ProjectNotFoundException
 import com.terraformation.backend.db.accelerator.CohortId
 import com.terraformation.backend.db.accelerator.CohortPhase
+import com.terraformation.backend.db.accelerator.DeliverableCategory
+import com.terraformation.backend.db.accelerator.DeliverableType
 import com.terraformation.backend.db.accelerator.EventStatus
 import com.terraformation.backend.db.accelerator.EventType
 import com.terraformation.backend.db.accelerator.ModuleId
@@ -599,6 +602,73 @@ class ModuleStoreTest : DatabaseTest(), RunsAsUser {
               .find { it.id == moduleId }!!
               .eventSessions[EventType.OneOnOneSession]!!
               .map { it.id })
+    }
+  }
+
+  @Nested
+  inner class FetchModulesWithDeliverables {
+    @Test
+    fun `returns associated deliverables with details ordered by position`() {
+      val moduleId = insertModule()
+      val deliverable1 =
+          insertDeliverable(
+              deliverableCategoryId = DeliverableCategory.Compliance,
+              deliverableTypeId = DeliverableType.Document,
+              descriptionHtml = "Description",
+              isSensitive = true,
+              isRequired = true,
+              moduleId = moduleId,
+              name = "Deliverable name",
+              position = 2)
+
+      val deliverable2 =
+          insertDeliverable(
+              deliverableCategoryId = DeliverableCategory.CarbonEligibility,
+              deliverableTypeId = DeliverableType.Species,
+              descriptionHtml = "Species description",
+              isSensitive = false,
+              isRequired = false,
+              moduleId = moduleId,
+              name = "Species name",
+              position = 1)
+
+      val model1 =
+          ModuleDeliverableModel(
+              id = deliverable1,
+              category = DeliverableCategory.Compliance,
+              descriptionHtml = "Description",
+              moduleId = moduleId,
+              name = "Deliverable name",
+              position = 2,
+              required = true,
+              sensitive = true,
+              type = DeliverableType.Document,
+          )
+
+      val model2 =
+          ModuleDeliverableModel(
+              id = deliverable2,
+              category = DeliverableCategory.CarbonEligibility,
+              descriptionHtml = "Species description",
+              moduleId = moduleId,
+              name = "Species name",
+              position = 1,
+              required = false,
+              sensitive = false,
+              type = DeliverableType.Species,
+          )
+
+      assertEquals(
+          listOf(model2, model1),
+          store.fetchAllModules().firstOrNull()?.deliverables,
+          "Fetch all with deliverables")
+
+      insertCohortModule(cohortId, moduleId)
+
+      assertEquals(
+          listOf(model2, model1),
+          store.fetchModulesForProject(projectId).firstOrNull()?.deliverables,
+          "Fetch by project with deliverables")
     }
   }
 }
