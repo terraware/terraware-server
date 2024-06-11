9c5da1b462e23c4061a0038bb394ea42bbb096ed
Nick Graziano <nick@terraformation.com>
2024-06-11 13:36:09 -0600

    Change URL for document tempaltes

--- src/main/kotlin/com/terraformation/backend/documentproducer/api/DocumentTemplatesController.kt
+++ src/main/kotlin/com/terraformation/backend/documentproducer/api/DocumentTemplatesController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @InternalEndpoint
-@RequestMapping("/api/v1/document-producer/document-templates")
+@RequestMapping("/api/v1/document-producer/templates")
 @RestController
 class DocumentTemplatesController(
     val documentTemplatesDao: DocumentTemplatesDao,
