be9104144470faed50521967dc4752dd602508e3
Steven Grimm <1248649+sgrimm@users.noreply.github.com>
2024-06-21 08:23:06 -0700

    SW-5582 Project-based variable value endpoints (#2084)
    To support variable values that aren't associated with documents, add versions of
    the variable value endpoints that take a project ID rather than a document ID in
    the URL path. The existing document-based endpoints continue to work as before;
    once the web app has been updated to move to the project-based ones, we can remove
    the document-based versions.

--- src/main/kotlin/com/terraformation/backend/documentproducer/api/ImagesController.kt
+++ src/main/kotlin/com/terraformation/backend/documentproducer/api/ImagesController.kt
@@ -5,6 +5,7 @@ import com.terraformation.backend.api.SuccessResponsePayload
 import com.terraformation.backend.api.getFilename
 import com.terraformation.backend.api.getPlainContentType
 import com.terraformation.backend.api.toResponseEntity
+import com.terraformation.backend.db.default_schema.ProjectId
 import com.terraformation.backend.db.docprod.DocumentId
 import com.terraformation.backend.db.docprod.VariableId
 import com.terraformation.backend.db.docprod.VariableValueId
@@ -29,14 +30,106 @@ import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
 @InternalEndpoint
-@RequestMapping("/api/v1/document-producer/documents/{documentId}/images")
+@RequestMapping("/api/v1/document-producer")
 @RestController
 class ImagesController(
     private val documentStore: DocumentStore,
     private val variableFileService: VariableFileService,
 ) {
   @GetMapping(
-      "/{valueId}",
+      "/projects/{projectId}/images/{valueId}",
+      produces =
+          [MediaType.APPLICATION_JSON_VALUE, MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE])
+  @Operation(
+      summary = "Gets the contents of an image variable value.",
+      description =
+          "Optional maxWidth and maxHeight parameters may be included to control the dimensions " +
+              "of the image; the server will scale the original down as needed. If neither " +
+              "parameter is specified, the original full-size image will be returned. The aspect " +
+              "ratio of the original image is maintained, so the returned image may be smaller " +
+              "than the requested width and height. If only maxWidth or only maxHeight is " +
+              "supplied, the other dimension will be computed based on the original image's " +
+              "aspect ratio.")
+  fun getProjectImageValue(
+      @PathVariable projectId: ProjectId,
+      @PathVariable valueId: VariableValueId,
+      @RequestParam
+      @Schema(
+          description =
+              "Maximum desired width in pixels. If neither this nor maxHeight is specified, the " +
+                  "full-sized original image will be returned. If this is specified, an image no " +
+                  "wider than this will be returned. The image may be narrower than this value " +
+                  "if needed to preserve the aspect ratio of the original.")
+      maxWidth: Int? = null,
+      @RequestParam
+      @Schema(
+          description =
+              "Maximum desired height in pixels. If neither this nor maxWidth is specified, the " +
+                  "full-sized original image will be returned. If this is specified, an image no " +
+                  "taller than this will be returned. The image may be shorter than this value " +
+                  "if needed to preserve the aspect ratio of the original.")
+      maxHeight: Int? = null,
+  ): ResponseEntity<InputStreamResource> {
+    return variableFileService
+        .readImageValue(projectId, valueId, maxWidth, maxHeight)
+        .toResponseEntity()
+  }
+
+  @Operation(summary = "Save an image to a new variable value.")
+  @PostMapping("/projects/{projectId}/images")
+  fun uploadProjectImageValue(
+      @PathVariable projectId: ProjectId,
+      @RequestPart file: MultipartFile,
+      @RequestPart(required = false) caption: String?,
+      @RequestPart(required = false) citation: String?,
+      @RequestPart @Schema(format = "int64", type = "integer") variableId: String,
+      @RequestPart(required = false)
+      @Schema(
+          description =
+              "If the variable is a list, which list position to use for the value. If not " +
+                  "specified, the server will use the next available list position if the " +
+                  "variable is a list, or will replace any existing image if the variable is " +
+                  "not a list.",
+          format = "int32",
+          type = "integer")
+      listPosition: String? = null,
+      @RequestPart(required = false)
+      @Schema(
+          description =
+              "If the variable is a table column, value ID of the row the value should belong to.",
+          format = "int64",
+          type = "integer")
+      rowValueId: String? = null,
+  ): UploadImageFileResponsePayload {
+    val newMetadata =
+        FileMetadata.of(
+            contentType = file.getPlainContentType(SUPPORTED_PHOTO_TYPES),
+            filename = file.getFilename("file"),
+            size = file.size,
+        )
+
+    val isAppend = listPosition == null
+
+    val base =
+        BaseVariableValueProperties(
+            citation = citation,
+            id = null,
+            // If list position isn't specified, the value here will be ignored because isAppend
+            // will be true.
+            listPosition = listPosition?.toIntOrNull() ?: 0,
+            projectId = projectId,
+            rowValueId = rowValueId?.ifEmpty { null }?.let { VariableValueId(it.toLong()) },
+            variableId = VariableId(variableId.toLong()),
+        )
+
+    val valueId =
+        variableFileService.storeImageValue(file.inputStream, newMetadata, base, caption, isAppend)
+
+    return UploadImageFileResponsePayload(valueId)
+  }
+
+  @GetMapping(
+      "/documents/{documentId}/images/{valueId}",
       produces =
           [MediaType.APPLICATION_JSON_VALUE, MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE])
   @Operation(
@@ -73,13 +166,11 @@ class ImagesController(
         documentStore.fetchDocumentById(documentId).projectId
             ?: throw DocumentNotFoundException(documentId)
 
-    return variableFileService
-        .readImageValue(projectId, valueId, maxWidth, maxHeight)
-        .toResponseEntity()
+    return getProjectImageValue(projectId, valueId, maxWidth, maxHeight)
   }
 
   @Operation(summary = "Save an image to a new variable value.")
-  @PostMapping
+  @PostMapping("/documents/{documentId}/images")
   fun uploadImageValue(
       @PathVariable documentId: DocumentId,
       @RequestPart file: MultipartFile,
@@ -104,32 +195,10 @@ class ImagesController(
           type = "integer")
       rowValueId: String? = null,
   ): UploadImageFileResponsePayload {
-    val newMetadata =
-        FileMetadata.of(
-            contentType = file.getPlainContentType(SUPPORTED_PHOTO_TYPES),
-            filename = file.getFilename("file"),
-            size = file.size,
-        )
-
     val projectId = documentStore.fetchProjectId(documentId)
-    val isAppend = listPosition == null
 
-    val base =
-        BaseVariableValueProperties(
-            citation = citation,
-            id = null,
-            // If list position isn't specified, the value here will be ignored because isAppend
-            // will be true.
-            listPosition = listPosition?.toIntOrNull() ?: 0,
-            projectId = projectId,
-            rowValueId = rowValueId?.ifEmpty { null }?.let { VariableValueId(it.toLong()) },
-            variableId = VariableId(variableId.toLong()),
-        )
-
-    val valueId =
-        variableFileService.storeImageValue(file.inputStream, newMetadata, base, caption, isAppend)
-
-    return UploadImageFileResponsePayload(valueId)
+    return uploadProjectImageValue(
+        projectId, file, caption, citation, variableId, listPosition, rowValueId)
   }
 }
 
