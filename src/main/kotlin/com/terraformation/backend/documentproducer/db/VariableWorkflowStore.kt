ea4e1913d735af952704c8917283f965d52153de
Steven Grimm <steven@terraformation.com>
2024-06-24 08:54:52 -0700

    SW-5499 Endpoint to update variable workflow details
    Add an internal-user-only endpoint that updates the workflow details for a
    variable in a project.

--- src/main/kotlin/com/terraformation/backend/documentproducer/db/VariableWorkflowStore.kt
+++ src/main/kotlin/com/terraformation/backend/documentproducer/db/VariableWorkflowStore.kt
@@ -2,16 +2,24 @@ package com.terraformation.backend.documentproducer.db
 
 import com.terraformation.backend.auth.currentUser
 import com.terraformation.backend.customer.model.requirePermissions
+import com.terraformation.backend.db.ProjectNotFoundException
 import com.terraformation.backend.db.default_schema.ProjectId
+import com.terraformation.backend.db.default_schema.tables.references.PROJECTS
 import com.terraformation.backend.db.docprod.VariableId
+import com.terraformation.backend.db.docprod.VariableWorkflowStatus
+import com.terraformation.backend.db.docprod.tables.references.VARIABLES
+import com.terraformation.backend.db.docprod.tables.references.VARIABLE_VALUES
 import com.terraformation.backend.db.docprod.tables.references.VARIABLE_WORKFLOW_HISTORY
 import com.terraformation.backend.documentproducer.model.ExistingVariableWorkflowHistoryModel
 import jakarta.inject.Named
+import java.time.InstantSource
 import org.jooq.Condition
 import org.jooq.DSLContext
+import org.jooq.impl.DSL
 
 @Named
 class VariableWorkflowStore(
+    private val clock: InstantSource,
     private val dslContext: DSLContext,
 ) {
   /**
@@ -25,6 +33,40 @@ class VariableWorkflowStore(
 
     return fetchCurrentByCondition(VARIABLE_WORKFLOW_HISTORY.PROJECT_ID.eq(projectId)).associateBy {
       it.variableId
+    }
+  }
+
+  fun update(
+      projectId: ProjectId,
+      variableId: VariableId,
+      status: VariableWorkflowStatus,
+      feedback: String?,
+      internalComment: String?,
+  ) {
+    requirePermissions { updateInternalVariableWorkflowDetails(projectId) }
+
+    if (!dslContext.fetchExists(PROJECTS, PROJECTS.ID.eq(projectId))) {
+      throw ProjectNotFoundException(projectId)
+    }
+
+    if (!dslContext.fetchExists(VARIABLES, VARIABLES.ID.eq(variableId))) {
+      throw VariableNotFoundException(variableId)
+    }
+
+    with(VARIABLE_WORKFLOW_HISTORY) {
+      dslContext
+          .insertInto(VARIABLE_WORKFLOW_HISTORY)
+          .set(CREATED_BY, currentUser().userId)
+          .set(CREATED_TIME, clock.instant())
+          .set(FEEDBACK, feedback)
+          .set(INTERNAL_COMMENT, internalComment)
+          .set(
+              MAX_VARIABLE_VALUE_ID,
+              DSL.field(DSL.select(DSL.max(VARIABLE_VALUES.ID)).from(VARIABLE_VALUES)))
+          .set(PROJECT_ID, projectId)
+          .set(VARIABLE_ID, variableId)
+          .set(VARIABLE_WORKFLOW_STATUS_ID, status)
+          .execute()
     }
   }
 
