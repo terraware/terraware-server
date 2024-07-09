52f4f65c9307b53724d3abb2a64b340fc3656063
Nick Graziano <nick@terraformation.com>
2024-07-09 12:01:24 -0600

    Add missing pdd-backend commit for https://github.com/terraware/pdd-backend/pull/135/files

--- src/main/kotlin/com/terraformation/backend/api/SerializerConfiguration.kt
+++ src/main/kotlin/com/terraformation/backend/api/SerializerConfiguration.kt
@@ -7,12 +7,6 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class SerializerConfiguration {
   @Bean
-  fun blankStringDeserializerModule(): SimpleModule {
-    return SimpleModule("BlankStringDeserializer")
-        .addDeserializer(String::class.java, BlankStringDeserializer())
-  }
-
-  @Bean
   fun arbitraryJsonObjectSerializerModule(): SimpleModule {
     return SimpleModule("JsonbSerializer")
         .addDeserializer(ArbitraryJsonObject::class.java, ArbitraryJsonObjectDeserializer())
