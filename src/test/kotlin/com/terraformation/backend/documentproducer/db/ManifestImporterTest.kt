2828b3ffaa8b54df5482224049afa98c98ffcfba
Nick Graziano <nick@terraformation.com>
2024-07-10 13:00:29 -0600

    Fix intermittent test failure for manifest importer by explicitly sorting the variables in the order needed

--- src/test/kotlin/com/terraformation/backend/documentproducer/db/ManifestImporterTest.kt
+++ src/test/kotlin/com/terraformation/backend/documentproducer/db/ManifestImporterTest.kt
@@ -1375,7 +1375,7 @@ class ManifestImporterTest : DatabaseTest(), RunsAsUser {
     // does not care about hierarchy and will grab the first one that matches by name in reversed
     // order
     private fun getVariableByName(name: String): VariablesRow =
-        variablesDao.fetchByName(name).reversed().first()
+        variablesDao.fetchByName(name).sortedBy { it.id!!.value }.reversed().first()
   }
 }
 
