900a518be9a036678b26d61934d5b91f1040c894
Nick Graziano <nick@terraformation.com>
2024-07-15 16:38:29 -0600

    Fix a methodology -> document template change that I missed

--- src/main/kotlin/com/terraformation/backend/documentproducer/api/DocumentTemplatesController.kt
+++ src/main/kotlin/com/terraformation/backend/documentproducer/api/DocumentTemplatesController.kt
@@ -40,7 +40,7 @@ class DocumentTemplatesController(
 data class DocumentTemplatePayload(
     val id: DocumentTemplateId,
     val name: String,
-    @Schema(description = "ID of the most recent variable manifest for the methodology, if any.")
+    @Schema(description = "ID of the most recent variable manifest for the document template, if any.")
     val variableManifestId: VariableManifestId?,
 )
 
