ee2876c13dbf9ae8c90c08ebd65231e2002138a1
Steven Grimm <1248649+sgrimm@users.noreply.github.com>
2024-07-24 08:41:42 -0700

    SW-5627 Handle uploaded application boundaries (#2195)
    Implement the endpoint that accepts uploaded files for application boundaries.
    It accepts GeoJSON, KML, and KMZ (zipped KML) formats.

---
+++ src/test/kotlin/com/terraformation/backend/gis/GeometryFileParserTest.kt
@@ -0,0 +1,88 @@
+package com.terraformation.backend.gis
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.terraformation.backend.db.GeometryModule
+import com.terraformation.backend.db.SRID
+import com.terraformation.backend.util.equalsOrBothNull
+import com.terraformation.backend.util.toMultiPolygon
+import org.geotools.util.ContentFormatException
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.locationtech.jts.geom.Coordinate
+import org.locationtech.jts.geom.GeometryFactory
+import org.locationtech.jts.geom.PrecisionModel
+
+class GeometryFileParserTest {
+  private val objectMapper = jacksonObjectMapper().registerModule(GeometryModule())
+  private val parser = GeometryFileParser(objectMapper)
+
+  private val geometryFactory = GeometryFactory(PrecisionModel(), SRID.LONG_LAT)
+  private val triangle =
+      geometryFactory
+          .createMultiPolygon(
+              arrayOf(
+                  geometryFactory.createPolygon(
+                      arrayOf(
+                          Coordinate(-155.63200453, 19.16360697),
+                          Coordinate(-155.63297767, 19.16570482),
+                          Coordinate(-155.63433804, 19.16382232),
+                          Coordinate(-155.63200453, 19.16360697)))))
+          .norm()
+
+  @Test
+  fun `can parse GeoJSON file`() {
+    runTriangleScenario("/gis/triangle.geojson")
+  }
+
+  @Test
+  fun `can parse KML file`() {
+    runTriangleScenario("/gis/triangle.kml")
+  }
+
+  @Test
+  fun `can parse KMZ file`() {
+    runTriangleScenario("/gis/triangle.kmz")
+  }
+
+  @Test
+  fun `throws exception if file type is unrecognized`() {
+    assertThrows<ContentFormatException> { parser.parse(byteArrayOf(1, 2, 3, 4), "dummy.bin") }
+  }
+
+  @Test
+  fun `throws exception if GeoJSON file is malformed`() {
+    assertThrows<ContentFormatException> {
+      parser.parse("""{"foo":"bar"}""".encodeToByteArray(), "malformed.json")
+    }
+  }
+
+  @Test
+  fun `throws exception if KML file is malformed`() {
+    assertThrows<ContentFormatException> {
+      parser.parse("""<not-kml />""".encodeToByteArray(), "malformed.kml")
+    }
+  }
+
+  @Test
+  fun `throws exception if KMZ file is malformed`() {
+    assertThrows<ContentFormatException> { parser.parse(byteArrayOf(1, 2, 3, 4), "malformed.kmz") }
+  }
+
+  @Test
+  fun `throws exception if KMZ file has no KML file`() {
+    assertThrows<ContentFormatException> { runTriangleScenario("/gis/no-kml.kmz") }
+  }
+
+  private fun runTriangleScenario(resourcePath: String) {
+    javaClass.getResourceAsStream(resourcePath).use { stream ->
+      val bytes = stream.readAllBytes()
+
+      val geometry = parser.parse(bytes, resourcePath)
+
+      if (!triangle.equalsOrBothNull(geometry.toMultiPolygon().norm())) {
+        assertEquals(triangle, geometry.toMultiPolygon())
+      }
+    }
+  }
+}
