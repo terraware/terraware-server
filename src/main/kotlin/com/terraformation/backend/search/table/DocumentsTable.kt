8eb2877d278cef74be36641e545416986e7034a3
Nick Graziano <nick@terraformation.com>
2024-08-07 10:26:24 -0600

    Formatting

--- src/main/kotlin/com/terraformation/backend/search/table/DocumentsTable.kt
+++ src/main/kotlin/com/terraformation/backend/search/table/DocumentsTable.kt
@@ -23,16 +23,16 @@ class DocumentsTable(tables: SearchTables) : SearchTable() {
 
   override val sublists: List<SublistField> by lazy {
     with(tables) {
-      listOf(
-          projects.asMultiValueSublist(
-              "projects", PROJECTS.ID.eq(DOCUMENTS.PROJECT_ID)))
+      listOf(projects.asMultiValueSublist("projects", PROJECTS.ID.eq(DOCUMENTS.PROJECT_ID)))
     }
   }
 
   override val fields: List<SearchField> =
       listOf(
           timestampField("createdTime", DOCUMENTS.CREATED_TIME, nullable = false),
-          idWrapperField("documentTemplateId", DOCUMENTS.DOCUMENT_TEMPLATE_ID) { DocumentTemplateId(it) },
+          idWrapperField("documentTemplateId", DOCUMENTS.DOCUMENT_TEMPLATE_ID) {
+            DocumentTemplateId(it)
+          },
           idWrapperField("id", DOCUMENTS.ID) { DocumentId(it) },
           integerField(
               "lastSavedVersionId",
@@ -43,15 +43,14 @@ class DocumentsTable(tables: SearchTables) : SearchTable() {
           timestampField("modifiedTime", DOCUMENTS.MODIFIED_TIME, nullable = false),
           textField("name", DOCUMENTS.NAME, nullable = false),
           idWrapperField("projectId", DOCUMENTS.PROJECT_ID) { ProjectId(it) },
-          enumField("status", DOCUMENTS.STATUS_ID, nullable = false)
-      )
+          enumField("status", DOCUMENTS.STATUS_ID, nullable = false))
 
   override fun conditionForVisibility(): Condition =
-    if (currentUser().canManageDocumentProducer()) {
-      DSL.trueCondition()
-    } else {
-      DSL.falseCondition()
-    }
+      if (currentUser().canManageDocumentProducer()) {
+        DSL.trueCondition()
+      } else {
+        DSL.falseCondition()
+      }
 
   override val defaultOrderFields: List<OrderField<*>> = listOf(DOCUMENTS.NAME)
 }
