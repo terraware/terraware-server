bc88b72a5e34961f06b3f9c3197ee1c2d65cfa47
Nick Graziano <nick@terraformation.com>
2024-08-19 10:46:22 -0600

    Put the automatic variable status update back to use the system user ID

--- src/main/kotlin/com/terraformation/backend/accelerator/VariableValueStatusUpdater.kt
+++ src/main/kotlin/com/terraformation/backend/accelerator/VariableValueStatusUpdater.kt
@@ -1,7 +1,6 @@
 package com.terraformation.backend.accelerator
 
 import com.terraformation.backend.accelerator.event.VariableValueUpdatedEvent
-import com.terraformation.backend.auth.currentUser
 import com.terraformation.backend.customer.model.SystemUser
 import com.terraformation.backend.db.docprod.VariableWorkflowStatus
 import com.terraformation.backend.documentproducer.db.VariableWorkflowStore
@@ -16,11 +15,9 @@ class VariableValueStatusUpdater(
   /** Update variable status to "in review" when value is updated */
   @EventListener
   fun on(event: VariableValueUpdatedEvent) {
-    val createdBy = currentUser().userId
-
     systemUser.run {
       variableWorkflowStore.update(
-          event.projectId, event.variableId, VariableWorkflowStatus.InReview, null, null, createdBy)
+          event.projectId, event.variableId, VariableWorkflowStatus.InReview, null, null)
     }
   }
 }
