77ad7ebbd263a17148f1c1185bbfb311181efee2
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-08-28 14:30:51 -0700

    Removed events from module store, added events store

--- src/main/kotlin/com/terraformation/backend/accelerator/db/ModuleStore.kt
+++ src/main/kotlin/com/terraformation/backend/accelerator/db/ModuleStore.kt
@@ -1,52 +1,22 @@
 package com.terraformation.backend.accelerator.db
 
-import com.terraformation.backend.accelerator.model.CohortModuleModel
-import com.terraformation.backend.accelerator.model.EventModel
-import com.terraformation.backend.accelerator.model.ModuleDeliverableModel
 import com.terraformation.backend.accelerator.model.ModuleModel
 import com.terraformation.backend.customer.model.requirePermissions
 import com.terraformation.backend.db.accelerator.CohortPhase
-import com.terraformation.backend.db.accelerator.EventType
 import com.terraformation.backend.db.accelerator.ModuleId
-import com.terraformation.backend.db.accelerator.tables.references.COHORTS
-import com.terraformation.backend.db.accelerator.tables.references.COHORT_MODULES
-import com.terraformation.backend.db.accelerator.tables.references.DELIVERABLES
-import com.terraformation.backend.db.accelerator.tables.references.EVENTS
-import com.terraformation.backend.db.accelerator.tables.references.EVENT_PROJECTS
 import com.terraformation.backend.db.accelerator.tables.references.MODULES
-import com.terraformation.backend.db.accelerator.tables.references.PARTICIPANTS
-import com.terraformation.backend.db.default_schema.ProjectId
-import com.terraformation.backend.db.default_schema.tables.references.PROJECTS
 import jakarta.inject.Named
 import org.jooq.Condition
 import org.jooq.DSLContext
-import org.jooq.Field
-import org.jooq.impl.DSL
 
 @Named
 class ModuleStore(
     private val dslContext: DSLContext,
 ) {
   fun fetchOneById(moduleId: ModuleId): ModuleModel {
-    requirePermissions { readModuleDetails(moduleId) }
+    requirePermissions { readModule(moduleId) }
 
     return fetch(MODULES.ID.eq(moduleId)).firstOrNull() ?: throw ModuleNotFoundException(moduleId)
-  }
-
-  fun fetchOneByIdForProject(moduleId: ModuleId, projectId: ProjectId): ModuleModel {
-    requirePermissions {
-      readModule(moduleId)
-      readProject(projectId)
-    }
-
-    return fetchForProject(projectId, MODULES.ID.eq(moduleId)).firstOrNull()
-        ?: throw ModuleNotFoundException(moduleId)
-  }
-
-  fun fetchModulesForProject(projectId: ProjectId): List<ModuleModel> {
-    requirePermissions { readProjectModules(projectId) }
-
-    return fetchForProject(projectId)
   }
 
   fun fetchAllModules(): List<ModuleModel> {
@@ -55,6 +25,8 @@ class ModuleStore(
   }
 
   fun fetchCohortPhase(moduleId: ModuleId): CohortPhase {
+    requirePermissions { readModule(moduleId) }
+
     return dslContext
         .select(MODULES.PHASE_ID)
         .from(MODULES)
@@ -62,107 +34,15 @@ class ModuleStore(
         .fetchOne(MODULES.PHASE_ID) ?: throw ModuleNotFoundException(moduleId)
   }
 
-  private fun cohortsMultiset(): Field<List<CohortModuleModel>> {
-    val projectsField =
-        DSL.multiset(
-                DSL.select(PROJECTS.ID)
-                    .from(COHORTS)
-                    .join(PARTICIPANTS)
-                    .on(PARTICIPANTS.COHORT_ID.eq(COHORTS.ID))
-                    .join(PROJECTS)
-                    .on(PROJECTS.PARTICIPANT_ID.eq(PARTICIPANTS.ID))
-                    .where(COHORTS.ID.eq(COHORT_MODULES.COHORT_ID)))
-            .convertFrom { result -> result.map { it.value1() }.toSet() }
-    return with(COHORT_MODULES) {
-      DSL.multiset(
-              DSL.select(asterisk(), projectsField)
-                  .from(this)
-                  .where(MODULE_ID.eq(MODULES.ID))
-                  .orderBy(START_DATE))
-          .convertFrom { result -> result.map { CohortModuleModel.of(it, projectsField) } }
-    }
-  }
-
-  private fun deliverablesMultiset(): Field<List<ModuleDeliverableModel>> {
-    return with(DELIVERABLES) {
-      DSL.multiset(DSL.selectFrom(this).where(MODULE_ID.eq(MODULES.ID)).orderBy(POSITION))
-          .convertFrom { result -> result.map { ModuleDeliverableModel.of(it) } }
-    }
-  }
-
-  private fun eventsMultiset(
-      projectId: ProjectId? = null
-  ): Field<Map<EventType, List<EventModel>>> {
-    val projectsField =
-        if (projectId == null) {
-          eventProjectsMultiset()
-        } else {
-          null
-        }
-
-    val projectEventCondition =
-        projectId?.let {
-          EVENTS.ID.`in`(
-              DSL.select(EVENT_PROJECTS.EVENT_ID)
-                  .from(EVENT_PROJECTS)
-                  .where(EVENT_PROJECTS.PROJECT_ID.eq(it)))
-        }
-
-    return with(EVENTS) {
-      DSL.multiset(
-              DSL.select(asterisk(), projectsField)
-                  .from(this)
-                  .where(projectEventCondition)
-                  .and(MODULE_ID.eq(MODULES.ID))
-                  .orderBy(START_TIME))
-          .convertFrom { result ->
-            result.groupBy({ it[EVENTS.EVENT_TYPE_ID]!! }, { EventModel.of(it, projectsField) })
-          }
-    }
-  }
-
   private fun fetch(condition: Condition? = null): List<ModuleModel> {
-    val cohortsField = cohortsMultiset()
     val deliverablesField = deliverablesMultiset()
-    val eventsField = eventsMultiset()
     return with(MODULES) {
       dslContext
-          .select(asterisk(), cohortsField, deliverablesField, eventsField)
+          .select(asterisk(), deliverablesField)
           .from(this)
           .apply { condition?.let { where(it) } }
-          .fetch { ModuleModel.of(it, deliverablesField, eventsField, cohortsField) }
-    }
-  }
-
-  private fun fetchForProject(
-      projectId: ProjectId,
-      condition: Condition? = null
-  ): List<ModuleModel> {
-    val deliverablesField = deliverablesMultiset()
-    val eventsField = eventsMultiset(projectId)
-
-    return with(MODULES) {
-      dslContext
-          .select(
-              asterisk(),
-              deliverablesField,
-              eventsField,
-              COHORT_MODULES.COHORT_ID,
-              COHORT_MODULES.MODULE_ID,
-              COHORT_MODULES.TITLE,
-              COHORT_MODULES.START_DATE,
-              COHORT_MODULES.END_DATE)
-          .from(this)
-          .join(COHORT_MODULES)
-          .on(COHORT_MODULES.MODULE_ID.eq(ID))
-          .join(PARTICIPANTS)
-          .on(PARTICIPANTS.COHORT_ID.eq(COHORT_MODULES.COHORT_ID))
-          .join(PROJECTS)
-          .on(PROJECTS.PARTICIPANT_ID.eq(PARTICIPANTS.ID))
-          .where(PROJECTS.ID.eq(projectId))
-          .apply { condition?.let { and(condition) } }
-          .orderBy(COHORT_MODULES.START_DATE)
-          .fetch { ModuleModel.of(it, deliverablesField, eventsField) }
+          .orderBy(ID)
+          .fetch { ModuleModel.of(it, deliverablesField) }
     }
   }
 }
