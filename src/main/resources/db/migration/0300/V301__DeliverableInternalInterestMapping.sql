b0df4bb9dc78dfbb1e8256503f6f2955598778aa
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-09-03 12:39:01 -0700

    Updated column name

--- src/main/resources/db/migration/0300/V301__DeliverableInternalInterestMapping.sql
+++ src/main/resources/db/migration/0300/V301__DeliverableInternalInterestMapping.sql
@@ -1,5 +1,5 @@
 ALTER TABLE accelerator.deliverable_categories
-    ADD COLUMN internal_interest_id INTEGER REFERENCES internal_interests ON DELETE SET NULL;
+    ADD COLUMN internal_interest_id INTEGER REFERENCES accelerator.internal_interests ON DELETE SET NULL;
 
 UPDATE accelerator.deliverable_categories
 SET internal_interest_id = id;
