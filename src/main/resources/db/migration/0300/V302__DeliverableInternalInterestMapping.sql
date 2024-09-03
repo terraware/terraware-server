9373f0d20a3ea70c66b9614e10088fb97acfed6c
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-09-03 15:49:23 -0700

    Make internal_comment_id non-null

--- src/main/resources/db/migration/0300/V302__DeliverableInternalInterestMapping.sql
+++ src/main/resources/db/migration/0300/V302__DeliverableInternalInterestMapping.sql
@@ -1,5 +1,7 @@
 ALTER TABLE accelerator.deliverable_categories
-    ADD COLUMN internal_interest_id INTEGER REFERENCES accelerator.internal_interests ON DELETE SET NULL;
+    ADD COLUMN internal_interest_id INTEGER REFERENCES accelerator.internal_interests ON DELETE CASCADE;
 
 UPDATE accelerator.deliverable_categories
 SET internal_interest_id = id;
+
+ALTER TABLE accelerator.deliverable_categories ALTER COLUMN internal_interest_id SET NOT NULL;
