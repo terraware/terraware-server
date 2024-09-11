3b0e0c781dc1ce4f5623a40c6a6065297f129536
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-09-11 10:32:18 -0700

    Added separate incomplete endpoint

--- src/main/kotlin/com/terraformation/backend/accelerator/api/DeliverablesController.kt
+++ src/main/kotlin/com/terraformation/backend/accelerator/api/DeliverablesController.kt
@@ -196,19 +196,25 @@ class DeliverablesController(
   }
 
   @ApiResponseSimpleSuccess
-  @Operation(summary = "Marks a submission from a project as complete or incomplete.")
+  @Operation(summary = "Marks a submission from a project as complete.")
   @PostMapping("/{deliverableId}/submissions/{projectId}/complete")
   fun completeSubmission(
       @PathVariable deliverableId: DeliverableId,
       @PathVariable projectId: ProjectId,
-      @Parameter(
-          description =
-              "True if the submission should be marked as Completed, or Incomplete otherwise. Defaults to true. ")
-      @RequestParam
-      isComplete: Boolean?,
   ): SimpleSuccessResponsePayload {
-    // "Create" operation updates status of existing submission if there is one.
-    deliverableService.completeDeliverable(deliverableId, projectId, isComplete ?: true)
+    deliverableService.completeDeliverable(deliverableId, projectId, true)
+
+    return SimpleSuccessResponsePayload()
+  }
+
+  @ApiResponseSimpleSuccess
+  @Operation(summary = "Marks a submission from a project as incomplete.")
+  @PostMapping("/{deliverableId}/submissions/{projectId}/incomplete")
+  fun incompleteSubmission(
+      @PathVariable deliverableId: DeliverableId,
+      @PathVariable projectId: ProjectId,
+  ): SimpleSuccessResponsePayload {
+    deliverableService.completeDeliverable(deliverableId, projectId, false)
 
     return SimpleSuccessResponsePayload()
   }
