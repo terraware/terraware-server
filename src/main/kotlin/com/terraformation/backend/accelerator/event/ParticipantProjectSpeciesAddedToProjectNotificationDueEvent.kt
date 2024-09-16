9a5d08348feb89e7391c5fff8976e11298d6d1e6
Steven Grimm <1248649+sgrimm@users.noreply.github.com>
2024-09-16 13:27:26 -0700

    SW-5607 Use rate-limited events for species notifications (#2416)
    Use `RateLimitedEventPublisher` to limit the rate of notifications about
    participant project species list additions and changes, replacing the code that
    scans for recent updates.
    
    In addition to requiring less code, this improves the notification behavior:
    previously, the code would defer notifying until at least 10 minutes had passed
    since the last action on the species list, no matter how long that took. Now it
    will send a notification the first time an action happens, followed by
    notifications at most every 10 minutes if additional actions happen.

--- src/main/kotlin/com/terraformation/backend/accelerator/event/ParticipantProjectSpeciesAddedToProjectNotificationDueEvent.kt
+++ src/main/kotlin/com/terraformation/backend/accelerator/event/ParticipantProjectSpeciesAddedToProjectNotificationDueEvent.kt
@@ -3,11 +3,11 @@ package com.terraformation.backend.accelerator.event
 import com.terraformation.backend.db.accelerator.DeliverableId
 import com.terraformation.backend.db.default_schema.ProjectId
 import com.terraformation.backend.db.default_schema.SpeciesId
+import com.terraformation.backend.ratelimit.RateLimitedEvent
+import java.time.Duration
 
 /**
- * Published when a participant project species is added to a project This event is a deferred,
- * throttled event and will only be sent out if a subsequent new species has not been added within
- * the defer period
+ * Published when a participant project species is added to a project.
  *
  * @see com.terraformation.backend.accelerator.SpeciesNotifier
  */
@@ -15,4 +15,8 @@ data class ParticipantProjectSpeciesAddedToProjectNotificationDueEvent(
     val deliverableId: DeliverableId,
     val projectId: ProjectId,
     val speciesId: SpeciesId,
-)
+) : RateLimitedEvent<ParticipantProjectSpeciesAddedToProjectNotificationDueEvent> {
+  override fun getRateLimitKey() = projectId
+
+  override fun getMinimumInterval(): Duration = Duration.ofMinutes(10)
+}
