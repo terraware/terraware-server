a895b1fa10d536cfc67191d63bba3a42e7c7415d
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-10-01 12:43:17 -0700

    SW-6065 Added feature to create Google Drive folder for applications when internal name is set (#2462)

--- src/main/kotlin/com/terraformation/backend/accelerator/GoogleFolderUpdater.kt
+++ src/main/kotlin/com/terraformation/backend/accelerator/GoogleFolderUpdater.kt
@@ -3,17 +3,23 @@ package com.terraformation.backend.accelerator
 import com.terraformation.backend.accelerator.db.ApplicationStore
 import com.terraformation.backend.accelerator.db.ProjectAcceleratorDetailsStore
 import com.terraformation.backend.accelerator.event.ApplicationInternalNameUpdatedEvent
+import com.terraformation.backend.config.TerrawareServerConfig
 import com.terraformation.backend.customer.model.SystemUser
 import com.terraformation.backend.db.accelerator.ApplicationStatus
+import com.terraformation.backend.db.accelerator.tables.references.PROJECT_ACCELERATOR_DETAILS
+import com.terraformation.backend.db.default_schema.ProjectId
 import com.terraformation.backend.file.GoogleDriveWriter
 import java.net.URI
 import javax.inject.Named
+import org.jooq.DSLContext
 import org.springframework.context.event.EventListener
 
 /** Event listeners for updating Google Drive folder when the names are changed */
 @Named
 class GoogleFolderUpdater(
     private val applicationStore: ApplicationStore,
+    private val config: TerrawareServerConfig,
+    private val dslContext: DSLContext,
     private val googleDriveWriter: GoogleDriveWriter,
     private val projectAcceleratorDetailsStore: ProjectAcceleratorDetailsStore,
     private val systemUser: SystemUser,
@@ -25,11 +31,13 @@ class GoogleFolderUpdater(
       val application = applicationStore.fetchOneById(event.applicationId)
       val projectDetails = projectAcceleratorDetailsStore.fetchOneById(application.projectId)
 
-      // If a project has a Google folder, and is still in pre-screen
-      if (projectDetails.googleFolderUrl != null &&
-          setOf(ApplicationStatus.NotSubmitted, ApplicationStatus.FailedPreScreen)
-              .contains(application.status)) {
-        renameFolder(projectDetails.googleFolderUrl, application.internalName)
+      if (setOf(ApplicationStatus.NotSubmitted, ApplicationStatus.FailedPreScreen)
+          .contains(application.status)) {
+        if (projectDetails.googleFolderUrl != null) {
+          renameFolder(projectDetails.googleFolderUrl, application.internalName)
+        } else {
+          createGoogleDriveFolder(application.projectId, application.internalName)
+        }
       }
     }
   }
@@ -38,5 +46,34 @@ class GoogleFolderUpdater(
     val folderName = fileNaming + INTERNAL_FOLDER_SUFFIX
     val fileId = googleDriveWriter.getFileIdForFolderUrl(folderUrl)
     googleDriveWriter.renameFile(fileId, folderName)
+  }
+
+  /**
+   * Creates a new folder on Google Drive for a project that doesn't have one yet. This will be the
+   * case for new applicants. Records the folder's URL in the project accelerator details.
+   *
+   * @return The URL of the new folder, or null if it couldn't be created.
+   */
+  private fun createGoogleDriveFolder(projectId: ProjectId, fileNaming: String): URI? {
+    val folderName = fileNaming + INTERNAL_FOLDER_SUFFIX
+    val parentFolderId = config.accelerator.applicationGoogleFolderId ?: return null
+    val driveId = googleDriveWriter.getDriveIdForFile(parentFolderId)
+
+    val newFolderId =
+        googleDriveWriter.findOrCreateFolders(driveId, parentFolderId, listOf(folderName))
+    val newFolderUrl = googleDriveWriter.shareFile(newFolderId)
+
+    with(PROJECT_ACCELERATOR_DETAILS) {
+      dslContext
+          .insertInto(PROJECT_ACCELERATOR_DETAILS)
+          .set(PROJECT_ID, projectId)
+          .set(GOOGLE_FOLDER_URL, newFolderUrl)
+          .onConflict(PROJECT_ID)
+          .doUpdate()
+          .set(GOOGLE_FOLDER_URL, newFolderUrl)
+          .execute()
+    }
+
+    return newFolderUrl
   }
 }
