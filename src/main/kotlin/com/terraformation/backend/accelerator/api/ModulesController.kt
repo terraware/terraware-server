aaa8072c763207439bfe0ae5d27f3ee0997d6379
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-10-02 09:19:53 -0700

    Added endpoints for importing modules and deliverables (#2471)

--- src/main/kotlin/com/terraformation/backend/accelerator/api/ModulesController.kt
+++ src/main/kotlin/com/terraformation/backend/accelerator/api/ModulesController.kt
@@ -3,11 +3,13 @@ package com.terraformation.backend.accelerator.api
 import com.terraformation.backend.accelerator.db.CohortModuleStore
 import com.terraformation.backend.accelerator.db.DeliverableStore
 import com.terraformation.backend.accelerator.db.ModuleNotFoundException
+import com.terraformation.backend.accelerator.db.ModulesImporter
 import com.terraformation.backend.accelerator.model.ModuleDeliverableModel
 import com.terraformation.backend.accelerator.model.ModuleModel
 import com.terraformation.backend.api.AcceleratorEndpoint
 import com.terraformation.backend.api.ApiResponse200
 import com.terraformation.backend.api.ApiResponse404
+import com.terraformation.backend.api.SimpleSuccessResponsePayload
 import com.terraformation.backend.api.SuccessResponsePayload
 import com.terraformation.backend.db.accelerator.CohortId
 import com.terraformation.backend.db.accelerator.DeliverableCategory
@@ -19,14 +21,20 @@ import com.terraformation.backend.db.accelerator.ParticipantId
 import com.terraformation.backend.db.default_schema.ProjectId
 import com.terraformation.backend.i18n.TimeZones
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Encoding
 import io.swagger.v3.oas.annotations.media.Schema
 import java.time.InstantSource
 import java.time.LocalDate
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
 
 @AcceleratorEndpoint
 @RequestMapping("/api/v1/accelerator/modules")
@@ -35,6 +43,7 @@ class ModulesController(
     private val clock: InstantSource,
     private val cohortModuleStore: CohortModuleStore,
     private val deliverableStore: DeliverableStore,
+    private val modulesImporter: ModulesImporter,
 ) {
   @ApiResponse200
   @ApiResponse404
@@ -83,6 +92,18 @@ class ModulesController(
   ): ListModuleDeliverablesResponsePayload {
     val deliverables = deliverableStore.fetchDeliverables(moduleId = moduleId)
     return ListModuleDeliverablesResponsePayload(deliverables.map { ModuleDeliverablePayload(it) })
+  }
+
+  @ApiResponse200
+  @Operation(summary = "Import a list of modules. ")
+  @PostMapping("/import", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+  @io.swagger.v3.oas.annotations.parameters.RequestBody(
+      content = [Content(encoding = [Encoding(name = "file", contentType = MediaType.ALL_VALUE)])])
+  fun importModules(
+      @RequestPart(required = true) file: MultipartFile
+  ): SimpleSuccessResponsePayload {
+    file.inputStream.use { inputStream -> modulesImporter.importModules(inputStream) }
+    return SimpleSuccessResponsePayload()
   }
 }
 
