0acff4a0cd94244f0ba7b99e5abe50ac90a7bbd4
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-10-03 12:40:00 -0700

    Launch cleanup jobs using coroutine async

--- src/main/kotlin/com/terraformation/backend/admin/AdminApplicationsController.kt
+++ src/main/kotlin/com/terraformation/backend/admin/AdminApplicationsController.kt
@@ -1,10 +1,12 @@
 package com.terraformation.backend.admin
 
+import com.terraformation.backend.accelerator.DeliverableFilesRenamer
 import com.terraformation.backend.accelerator.db.ApplicationStore
-import com.terraformation.backend.accelerator.event.ProjectGoogleDriveCleanupEvent
 import com.terraformation.backend.api.RequireGlobalRole
 import com.terraformation.backend.db.default_schema.GlobalRole
-import org.springframework.context.ApplicationEventPublisher
+import com.terraformation.backend.log.perClassLogger
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 import org.springframework.stereotype.Controller
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
@@ -17,8 +19,10 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes
 @Validated
 class AdminApplicationsController(
     private val applicationStore: ApplicationStore,
-    private val eventPublisher: ApplicationEventPublisher,
+    private val deliverableFilesRenamer: DeliverableFilesRenamer,
 ) {
+  private val log = perClassLogger()
+
   @PostMapping("/applications/cleanUpDrive")
   fun cleanUpApplicationDrive(
       redirectAttributes: RedirectAttributes,
@@ -31,16 +35,24 @@ class AdminApplicationsController(
           return redirectToHome()
         }
 
-    applications.forEach {
-      eventPublisher.publishEvent(ProjectGoogleDriveCleanupEvent(it.projectId, it.internalName))
+    runBlocking {
+      applications.forEach {
+        async {
+          try {
+            deliverableFilesRenamer.createOrUpdateGoogleDriveFolder(it.projectId, it.internalName)
+          } catch (e: Exception) {
+            log.warn("Failed to cleanup Google Drive folder for project ${it.projectId}", e)
+          }
+        }
+      }
     }
 
     redirectAttributes.successMessage =
-        "Dispatched ${applications.size} clean up jobs. " +
+        "Launched ${applications.size} clean up jobs. " +
             "Please wait a few minutes for all the jobs to complete. Check logs for results."
 
     return redirectToHome()
   }
 
-  private fun redirectToHome() = "redirect:/admin"
+  private fun redirectToHome() = "redirect:/admin/"
 }
