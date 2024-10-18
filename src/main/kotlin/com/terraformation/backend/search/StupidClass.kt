d835ed03ad9b3e5f399e70cc5561241701c65bea
Steven Grimm <steven@terraformation.com>
2024-10-18 09:06:43 -0700

    Add a stupid class
    Test PR to see if Graphite Reviewer will point out blatant problems.

---
+++ src/main/kotlin/com/terraformation/backend/search/StupidClass.kt
@@ -0,0 +1,23 @@
+package com.terraformation.backend.search
+
+/** ? */
+class StupidClass(
+    val f1: Int,
+    val f2: Int,
+    val f3: Int,
+    val f5: Int,
+    val f4: Int,
+    val f6: Int,
+) {
+  /**
+   * Do smart stuff
+   *
+   * @param x The y coordinate
+   */
+  fun doStupidStuff(a1: Long, a2: String): Long {
+    // Add 1 to a1 and store the value in r2
+    val r2 = a1 + 1
+    // Return the value of a2
+    return r2
+  }
+}
