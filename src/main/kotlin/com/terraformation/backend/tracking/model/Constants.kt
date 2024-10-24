ef13d533a299111c890b6cf2ea78597a0d468df9
Steven Grimm <steven@terraformation.com>
2024-10-24 09:53:50 -0700

    SW-6186 Use plot size in planting density calculation
    Remove the hardwired plot size from the plot-level planting density calculation in
    `ObservationResultsStore` so that the density number is calculated correctly as
    the plot size varies.
    
    Add a copy of one of the observation calculation test scenarios with the expected
    planting density and estimated total plants adjusted for 30m plots instead of 25m.

--- src/main/kotlin/com/terraformation/backend/tracking/model/Constants.kt
+++ src/main/kotlin/com/terraformation/backend/tracking/model/Constants.kt
@@ -1,6 +1,5 @@
 package com.terraformation.backend.tracking.model
 
-import com.terraformation.backend.util.SQUARE_METERS_PER_HECTARE
 import java.math.BigDecimal
 
 /** Number of digits after the decimal point to retain in area (hectares) calculations. */
@@ -14,10 +13,3 @@ const val MONITORING_PLOT_SIZE: Double = 25.0
 
 /** Monitoring plot width and height in meters. */
 const val MONITORING_PLOT_SIZE_INT = MONITORING_PLOT_SIZE.toInt()
-
-/** Number of square meters in a monitoring plot. */
-const val SQUARE_METERS_PER_MONITORING_PLOT: Double = MONITORING_PLOT_SIZE * MONITORING_PLOT_SIZE
-
-/** Number of monitoring plots in a hectare. */
-const val MONITORING_PLOTS_PER_HECTARE: Double =
-    SQUARE_METERS_PER_HECTARE / SQUARE_METERS_PER_MONITORING_PLOT
