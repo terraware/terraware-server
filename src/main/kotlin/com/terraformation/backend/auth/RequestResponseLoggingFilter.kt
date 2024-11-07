terraware/terraware-server

commit e8c4e86eb4920f4746f5cc2576852ef6d8b5aafe

Add more request context to log messages (#2580)
Currently it can be a bit of a hassle to search for log messages for a particular
request or logs from all requests for a particular user, which we sometimes need
to do when looking into a bug report.

Start including the following information in the mapped diagnostic context (MDC)
for all messages that are logged in the course of handling an HTTP request:

* The user's auth (Keycloak) ID. This can be used to match a user's log messages
  with the access log entries for their requests.
* A unique request ID. This can be used to quickly find all the log messages for
  a particular request.
* The user's email address. Previously we only logged this for internal users.

For requests from internal users, the log message that records their request and
response bodies now includes the method, URI, and email address as part of the
text of the log message. Previously the log message was just "Request" which
isn't too useful when scanning logs in Datadog. The query string, if any, is
now included along with the request and response bodies.

The default log format for dev environments now renders the MDC in cyan rather
than in the default text color so it's easy to visually distinguish from the
text of the log messages.

src/main/kotlin/com/terraformation/backend/auth/RequestResponseLoggingFilter.kt
@@ -13,8 +13,11 @@ import jakarta.servlet.ServletResponse
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import jakarta.ws.rs.core.MediaType
+import java.nio.ByteBuffer
 import java.nio.charset.StandardCharsets
+import java.util.UUID
 import kotlin.math.min
+import org.apache.commons.codec.binary.Base32
 import org.slf4j.MDC
 import org.springframework.web.util.ContentCachingRequestWrapper
 import org.springframework.web.util.ContentCachingResponseWrapper
@@ -41,40 +44,66 @@ class RequestResponseLoggingFilter(
   private val loggableContentTypes =
       listOf(MediaType.APPLICATION_JSON, MediaType.APPLICATION_FORM_URLENCODED)
 
+  /**
+   * Prefix to include in request IDs. [ServletRequest.getRequestId] doesn't return globally unique
+   * request IDs, and it's useful to be able to search for logs from a specific request.
+   */
+  private val requestIdPrefix: String by lazy {
+    val uuid = UUID.randomUUID()
+    val buffer = ByteBuffer.allocate(16)
+
+    buffer.putLong(uuid.mostSignificantBits)
+    buffer.putLong(uuid.leastSignificantBits)
+
+    Base32().encodeToString(buffer.array()).trimEnd('=')
+  }
+
   override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
     val user = CurrentUserHolder.getCurrentUser()
-    if (log.isDebugEnabled &&
-        requestLogConfig.emailRegex != null &&
-        user is IndividualUser &&
-        user.email.lowercase().matches(requestLogConfig.emailRegex) &&
-        request is HttpServletRequest &&
-        response is HttpServletResponse &&
-        request.dispatcherType != DispatcherType.ASYNC &&
-        (requestLogConfig.excludeRegex == null ||
-            !request.requestURI.matches(requestLogConfig.excludeRegex))) {
-      val wrappedRequest = ContentCachingRequestWrapper(request, maxPayloadSize)
-      val wrappedResponse = ContentCachingResponseWrapper(response)
+    val oldMdc = MDC.getCopyOfContextMap()
 
-      try {
-        chain.doFilter(wrappedRequest, wrappedResponse)
-      } finally {
-        val oldMdc = MDC.getCopyOfContextMap()
-        try {
-          mdcPut("request", payload(wrappedRequest.contentType, wrappedRequest.contentAsByteArray))
-          mdcPut(
-              "response", payload(wrappedResponse.contentType, wrappedResponse.contentAsByteArray))
-          mdcPut("email", user.email)
-          mdcPut("method", wrappedRequest.method)
-          mdcPut("uri", wrappedRequest.requestURI)
+    try {
+      mdcPut("authId", user?.authId)
+      mdcPut("requestId", "${requestIdPrefix}_${request.requestId}")
 
-          log.debug("Request")
-        } finally {
-          oldMdc?.let { MDC.setContextMap(it) } ?: MDC.clear()
-          wrappedResponse.copyBodyToResponse()
+      if (user is IndividualUser) {
+        mdcPut("email", user.email)
+
+        if (log.isDebugEnabled &&
+            requestLogConfig.emailRegex != null &&
+            user.email.lowercase().matches(requestLogConfig.emailRegex) &&
+            request is HttpServletRequest &&
+            response is HttpServletResponse &&
+            request.dispatcherType != DispatcherType.ASYNC &&
+            (requestLogConfig.excludeRegex == null ||
+                !request.requestURI.matches(requestLogConfig.excludeRegex))) {
+          val wrappedRequest = ContentCachingRequestWrapper(request, maxPayloadSize)
+          val wrappedResponse = ContentCachingResponseWrapper(response)
+
+          try {
+            chain.doFilter(wrappedRequest, wrappedResponse)
+          } finally {
+            try {
+              mdcPut(
+                  "request", payload(wrappedRequest.contentType, wrappedRequest.contentAsByteArray))
+              mdcPut(
+                  "response",
+                  payload(wrappedResponse.contentType, wrappedResponse.contentAsByteArray))
+              mdcPut("method", wrappedRequest.method)
+              mdcPut("queryString", wrappedRequest.queryString)
+              mdcPut("uri", wrappedRequest.requestURI)
+
+              log.debug("Request ${request.method} ${request.requestURI} ${user.email}")
+            } finally {
+              wrappedResponse.copyBodyToResponse()
+            }
+          }
+        } else {
+          chain.doFilter(request, response)
         }
       }
-    } else {
-      chain.doFilter(request, response)
+    } finally {
+      oldMdc?.let { MDC.setContextMap(it) } ?: MDC.clear()
     }
   }
 

build.gradle.kts
@@ -97,6 +97,7 @@ dependencies {
   implementation("com.google.apis:google-api-services-drive:v3-rev20241014-2.0.0")
   implementation("com.opencsv:opencsv:5.9")
   implementation("com.squarespace.cldr-engine:cldr-engine:1.8.2")
+  implementation("commons-codec:commons-codec:1.17.1")
   implementation("commons-validator:commons-validator:1.9.0")
   implementation("dev.akkinoc.spring.boot:logback-access-spring-boot-starter:4.3.2") {
     exclude("org.apache.tomcat")

src/main/resources/application-default.yaml
@@ -52,4 +52,4 @@ logging:
       %clr([%15.15t]){faint}
       %clr(%-40.40logger{39}){cyan}
       %clr(:){faint}
-      %m%replace( %X){'^ $',''}%n%wEx
+      %m%clr(%replace( %X){'^ $',''}){cyan}%n%wEx

commit f45ddf9e816a883509a41f802d93214b2ba1da1f

Fix request handling for unauthenticated users
Commit e8c4e86e broke the redirect to Keycloak for users without login sessions.

src/main/kotlin/com/terraformation/backend/auth/RequestResponseLoggingFilter.kt
@@ -101,6 +101,8 @@ class RequestResponseLoggingFilter(
         } else {
           chain.doFilter(request, response)
         }
+      } else {
+        chain.doFilter(request, response)
       }
     } finally {
       oldMdc?.let { MDC.setContextMap(it) } ?: MDC.clear()