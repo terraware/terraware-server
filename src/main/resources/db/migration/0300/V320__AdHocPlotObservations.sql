68cb324bba91b0ffbcdce01759457c694f969058
Nick Graziano <nick@terraformation.com>
2024-11-26 11:58:29 -0700

    Move enum population and old value migration to the migration script

--- src/main/resources/db/migration/0300/V320__AdHocPlotObservations.sql
+++ src/main/resources/db/migration/0300/V320__AdHocPlotObservations.sql
@@ -3,9 +3,26 @@ CREATE TABLE tracking.observation_types (
     name TEXT NOT NULL UNIQUE
 );
 
-ALTER TABLE tracking.observations ADD COLUMN is_ad_hoc BOOLEAN DEFAULT FALSE NOT NULL;
+INSERT INTO tracking.observation_types (id, name)
+VALUES (1, 'Monitoring'),
+       (2, 'Biomass Measurements')
+ON CONFLICT (id) DO UPDATE SET name = excluded.name;
+
+ALTER TABLE tracking.observations ADD COLUMN is_ad_hoc BOOLEAN;
+
+UPDATE tracking.observations
+SET is_ad_hoc = false
+WHERE is_ad_hoc IS NULL;
+
+ALTER TABLE tracking.observations ALTER COLUMN is_ad_hoc SET NOT NULL;
+
 ALTER TABLE tracking.observations ADD COLUMN observation_type_id INTEGER REFERENCES tracking.observation_types;
-ALTER TABLE tracking.observations ADD CONSTRAINT ad_hoc_observation_type CHECK (
-    (is_ad_hoc = FALSE AND observation_type_id IS NULL)
-    OR (is_ad_hoc = TRUE AND observation_type_id IS NOT NULL)
-);
+
+UPDATE tracking.observations
+SET observation_type_id = 1
+WHERE observation_type_id IS NULL;
+
+ALTER TABLE tracking.observations ALTER COLUMN observation_type_id SET NOT NULL;
+
+ALTER TABLE tracking.monitoring_plots ALTER COLUMN planting_subzone_id DROP NOT NULL;
+ALTER TABLE tracking.observation_plots ALTER COLUMN monitoring_plot_history_id DROP NOT NULL;
