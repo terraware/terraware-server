4467d085e40109a104f79bf38d11c16804b384bb
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2025-02-11 10:28:04 -0800

    Implemented ReportMetricStore for CRU operations on Standard Metrics

---
+++ src/main/kotlin/com/terraformation/backend/accelerator/model/StandardMetricModel.kt
@@ -0,0 +1,35 @@
+package com.terraformation.backend.accelerator.model
+
+import com.terraformation.backend.db.accelerator.MetricComponent
+import com.terraformation.backend.db.accelerator.MetricType
+import com.terraformation.backend.db.accelerator.StandardMetricId
+import com.terraformation.backend.db.accelerator.tables.references.STANDARD_METRICS
+import org.jooq.Record
+
+data class StandardMetricModel<ID : StandardMetricId?>(
+    val id: ID,
+    val name: String,
+    val description: String?,
+    val component: MetricComponent,
+    val type: MetricType,
+    val reference: String,
+) {
+  companion object {
+    fun of(record: Record): ExistingStandardMetricModel {
+      return with(STANDARD_METRICS) {
+        ExistingStandardMetricModel(
+            id = record[ID]!!,
+            name = record[NAME]!!,
+            description = record[DESCRIPTION],
+            component = record[COMPONENT_ID]!!,
+            type = record[TYPE_ID]!!,
+            reference = record[REFERENCE]!!,
+        )
+      }
+    }
+  }
+}
+
+typealias ExistingStandardMetricModel = StandardMetricModel<StandardMetricId>
+
+typealias NewStandardMetricModel = StandardMetricModel<Nothing?>
