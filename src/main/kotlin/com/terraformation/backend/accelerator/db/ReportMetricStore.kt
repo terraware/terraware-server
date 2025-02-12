e4eb5b0c160186d9f3771a4ebe024f7b2e578f26
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2025-02-12 11:36:25 -0800

    Implemented ReportMetricStore for CRU operations on Standard Metrics (#2833)

---
+++ src/main/kotlin/com/terraformation/backend/accelerator/db/ReportMetricStore.kt
@@ -0,0 +1,77 @@
+package com.terraformation.backend.accelerator.db
+
+import com.terraformation.backend.accelerator.model.ExistingStandardMetricModel
+import com.terraformation.backend.accelerator.model.NewStandardMetricModel
+import com.terraformation.backend.accelerator.model.StandardMetricModel
+import com.terraformation.backend.customer.model.requirePermissions
+import com.terraformation.backend.db.StandardMetricNotFoundException
+import com.terraformation.backend.db.accelerator.StandardMetricId
+import com.terraformation.backend.db.accelerator.tables.references.STANDARD_METRICS
+import jakarta.inject.Named
+import org.jooq.Condition
+import org.jooq.DSLContext
+import org.jooq.impl.DSL
+
+@Named
+class ReportMetricStore(
+    private val dslContext: DSLContext,
+) {
+  fun fetchOneStandardMetric(metricId: StandardMetricId): ExistingStandardMetricModel {
+    requirePermissions { manageProjectReportConfigs() }
+
+    return fetchStandardMetrics(STANDARD_METRICS.ID.eq(metricId)).firstOrNull()
+        ?: throw StandardMetricNotFoundException(metricId)
+  }
+
+  fun fetchAllStandardMetrics(): List<ExistingStandardMetricModel> {
+    requirePermissions { manageProjectReportConfigs() }
+
+    return fetchStandardMetrics(DSL.trueCondition())
+  }
+
+  fun createStandardMetric(model: NewStandardMetricModel): StandardMetricId {
+    requirePermissions { manageProjectReportConfigs() }
+
+    return with(STANDARD_METRICS) {
+      dslContext
+          .insertInto(this)
+          .set(NAME, model.name)
+          .set(DESCRIPTION, model.description)
+          .set(COMPONENT_ID, model.component)
+          .set(TYPE_ID, model.type)
+          .set(REFERENCE, model.reference)
+          .returning(ID)
+          .fetchOne(ID)!!
+    }
+  }
+
+  fun updateStandardMetric(
+      metricId: StandardMetricId,
+      updateFunc: (ExistingStandardMetricModel) -> ExistingStandardMetricModel
+  ) {
+    requirePermissions { manageProjectReportConfigs() }
+
+    val existing = fetchOneStandardMetric(metricId)
+    val new = updateFunc(existing)
+
+    with(STANDARD_METRICS) {
+      dslContext
+          .update(this)
+          .set(NAME, new.name)
+          .set(DESCRIPTION, new.description)
+          .set(COMPONENT_ID, new.component)
+          .set(TYPE_ID, new.type)
+          .set(REFERENCE, new.reference)
+          .where(ID.eq(metricId))
+          .execute()
+    }
+  }
+
+  private fun fetchStandardMetrics(condition: Condition): List<ExistingStandardMetricModel> {
+    return dslContext
+        .selectFrom(STANDARD_METRICS)
+        .where(condition)
+        .orderBy(STANDARD_METRICS.REFERENCE, STANDARD_METRICS.ID)
+        .fetch { StandardMetricModel.of(it) }
+  }
+}
---
+++ src/test/kotlin/com/terraformation/backend/accelerator/db/ReportMetricStoreTest.kt
@@ -0,0 +1,284 @@
+package com.terraformation.backend.accelerator.db
+
+import com.terraformation.backend.RunsAsDatabaseUser
+import com.terraformation.backend.accelerator.model.ExistingStandardMetricModel
+import com.terraformation.backend.accelerator.model.NewStandardMetricModel
+import com.terraformation.backend.customer.model.TerrawareUser
+import com.terraformation.backend.db.DatabaseTest
+import com.terraformation.backend.db.StandardMetricNotFoundException
+import com.terraformation.backend.db.accelerator.MetricComponent
+import com.terraformation.backend.db.accelerator.MetricType
+import com.terraformation.backend.db.accelerator.StandardMetricId
+import com.terraformation.backend.db.accelerator.tables.records.StandardMetricsRecord
+import com.terraformation.backend.db.default_schema.GlobalRole
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.springframework.security.access.AccessDeniedException
+
+class ReportMetricStoreTest : DatabaseTest(), RunsAsDatabaseUser {
+  override lateinit var user: TerrawareUser
+
+  private val store: ReportMetricStore by lazy { ReportMetricStore(dslContext) }
+
+  @BeforeEach
+  fun setup() {
+    insertUserGlobalRole(role = GlobalRole.AcceleratorAdmin)
+  }
+
+  @Nested
+  inner class Fetch {
+
+    @Nested
+    inner class FetchOneStandardMetric {
+      @Test
+      fun `returns one standard metric`() {
+        val metricId =
+            insertStandardMetric(
+                component = MetricComponent.Climate,
+                description = "Climate standard metric description",
+                name = "Climate Standard Metric",
+                reference = "3.0",
+                type = MetricType.Activity,
+            )
+
+        assertEquals(
+            ExistingStandardMetricModel(
+                id = metricId,
+                component = MetricComponent.Climate,
+                description = "Climate standard metric description",
+                name = "Climate Standard Metric",
+                reference = "3.0",
+                type = MetricType.Activity,
+            ),
+            store.fetchOneStandardMetric(metricId))
+      }
+
+      @Test
+      fun `throws not found exception if no metric found`() {
+        assertThrows<StandardMetricNotFoundException> {
+          store.fetchOneStandardMetric(StandardMetricId(-1))
+        }
+      }
+
+      @Test
+      fun `throws access denied exception for non-accelerator admin`() {
+        val metricId =
+            insertStandardMetric(
+                component = MetricComponent.Climate,
+                description = "Climate standard metric description",
+                name = "Climate Standard Metric",
+                reference = "3.0",
+                type = MetricType.Activity,
+            )
+
+        deleteUserGlobalRole(role = GlobalRole.AcceleratorAdmin)
+        assertThrows<AccessDeniedException> { store.fetchOneStandardMetric(metricId) }
+      }
+    }
+  }
+
+  @Nested
+  inner class FetchAllStandardMetrics {
+    @Test
+    fun `returns all standard metrics`() {
+      val standardMetricId1 =
+          insertStandardMetric(
+              component = MetricComponent.Climate,
+              description = "Climate standard metric description",
+              name = "Climate Standard Metric",
+              reference = "3.0",
+              type = MetricType.Activity,
+          )
+
+      val standardMetricId2 =
+          insertStandardMetric(
+              component = MetricComponent.Community,
+              description = "Community metric description",
+              name = "Community Metric",
+              reference = "5.0",
+              type = MetricType.Outcome,
+          )
+
+      val standardMetricId3 =
+          insertStandardMetric(
+              component = MetricComponent.ProjectObjectives,
+              description = "Project objectives metric description",
+              name = "Project Objectives Metric",
+              reference = "3.0",
+              type = MetricType.Impact,
+          )
+
+      assertEquals(
+          listOf(
+              // Ordered by reference then ID
+              ExistingStandardMetricModel(
+                  id = standardMetricId1,
+                  component = MetricComponent.Climate,
+                  description = "Climate standard metric description",
+                  name = "Climate Standard Metric",
+                  reference = "3.0",
+                  type = MetricType.Activity,
+              ),
+              ExistingStandardMetricModel(
+                  id = standardMetricId3,
+                  component = MetricComponent.ProjectObjectives,
+                  description = "Project objectives metric description",
+                  name = "Project Objectives Metric",
+                  reference = "3.0",
+                  type = MetricType.Impact,
+              ),
+              ExistingStandardMetricModel(
+                  id = standardMetricId2,
+                  component = MetricComponent.Community,
+                  description = "Community metric description",
+                  name = "Community Metric",
+                  reference = "5.0",
+                  type = MetricType.Outcome,
+              ),
+          ),
+          store.fetchAllStandardMetrics())
+    }
+
+    @Test
+    fun `throws access denied exception for non-accelerator admin`() {
+      deleteUserGlobalRole(role = GlobalRole.AcceleratorAdmin)
+      assertThrows<AccessDeniedException> { store.fetchAllStandardMetrics() }
+    }
+  }
+
+  @Nested
+  inner class Create {
+    @Nested
+    inner class StandardMetrics {
+      @Test
+      fun `inserts new record`() {
+        val existingMetricId =
+            insertStandardMetric(
+                component = MetricComponent.Climate,
+                description = "Climate standard metric description",
+                name = "Climate Standard Metric",
+                reference = "3.0",
+                type = MetricType.Activity,
+            )
+
+        val model =
+            NewStandardMetricModel(
+                id = null,
+                component = MetricComponent.ProjectObjectives,
+                description = "Project objectives metric description",
+                name = "Project Objectives Metric",
+                reference = "1.0",
+                type = MetricType.Impact,
+            )
+
+        val newMetricId = store.createStandardMetric(model)
+
+        assertTableEquals(
+            listOf(
+                StandardMetricsRecord(
+                    id = existingMetricId,
+                    componentId = MetricComponent.Climate,
+                    description = "Climate standard metric description",
+                    name = "Climate Standard Metric",
+                    reference = "3.0",
+                    typeId = MetricType.Activity,
+                ),
+                StandardMetricsRecord(
+                    id = newMetricId,
+                    componentId = MetricComponent.ProjectObjectives,
+                    description = "Project objectives metric description",
+                    name = "Project Objectives Metric",
+                    reference = "1.0",
+                    typeId = MetricType.Impact,
+                )))
+      }
+
+      @Test
+      fun `throws access denied exception for non-accelerator admin`() {
+        val model =
+            NewStandardMetricModel(
+                id = null,
+                component = MetricComponent.ProjectObjectives,
+                description = "Project objectives metric description",
+                name = "Project Objectives Metric",
+                reference = "1.0",
+                type = MetricType.Impact,
+            )
+
+        deleteUserGlobalRole(role = GlobalRole.AcceleratorAdmin)
+        assertThrows<AccessDeniedException> { store.createStandardMetric(model) }
+      }
+    }
+  }
+
+  @Nested
+  inner class Update {
+    @Nested
+    inner class StandardMetrics {
+      @Test
+      fun `updates existing record`() {
+        val existingMetricId =
+            insertStandardMetric(
+                component = MetricComponent.Climate,
+                description = "Climate standard metric description",
+                name = "Climate Standard Metric",
+                reference = "3.0",
+                type = MetricType.Activity,
+            )
+
+        val updated =
+            ExistingStandardMetricModel(
+                id = StandardMetricId(99), // this field is ignored
+                component = MetricComponent.ProjectObjectives,
+                description = "Project objectives metric description",
+                name = "Project Objectives Metric",
+                reference = "1.0",
+                type = MetricType.Impact,
+            )
+
+        store.updateStandardMetric(existingMetricId) { updated }
+
+        assertTableEquals(
+            listOf(
+                StandardMetricsRecord(
+                    id = existingMetricId,
+                    componentId = MetricComponent.ProjectObjectives,
+                    description = "Project objectives metric description",
+                    name = "Project Objectives Metric",
+                    reference = "1.0",
+                    typeId = MetricType.Impact,
+                )))
+      }
+
+      @Test
+      fun `throws access denied exception for non-accelerator admin`() {
+        val existingMetricId =
+            insertStandardMetric(
+                component = MetricComponent.Climate,
+                description = "Climate standard metric description",
+                name = "Climate Standard Metric",
+                reference = "3.0",
+                type = MetricType.Activity,
+            )
+
+        val updated =
+            ExistingStandardMetricModel(
+                id = StandardMetricId(99), // this field is ignored
+                component = MetricComponent.ProjectObjectives,
+                description = "Project objectives metric description",
+                name = "Project Objectives Metric",
+                reference = "1.0",
+                type = MetricType.Impact,
+            )
+
+        deleteUserGlobalRole(role = GlobalRole.AcceleratorAdmin)
+        assertThrows<AccessDeniedException> {
+          store.updateStandardMetric(existingMetricId) { updated }
+        }
+      }
+    }
+  }
+}
