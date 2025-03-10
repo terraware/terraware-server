539de39a8d79bb97c0510c1d7869bfb68dd9c54c
Isaac Hudson <isaac.hudson@terraformation.com>
2025-03-10 16:29:19 -0700

    SW-6621 Refactor common logic

--- src/main/kotlin/com/terraformation/backend/funder/db/FundingEntityUserStore.kt
+++ src/main/kotlin/com/terraformation/backend/funder/db/FundingEntityUserStore.kt
@@ -8,28 +8,27 @@ import com.terraformation.backend.db.funder.tables.references.FUNDING_ENTITY_USE
 import com.terraformation.backend.funder.model.FundingEntityModel
 import jakarta.inject.Named
 import org.jooq.DSLContext
+import org.jooq.Record
 
 @Named
 class FundingEntityUserStore(private val dslContext: DSLContext) {
   fun getFundingEntityId(userId: UserId): FundingEntityId? {
     requirePermissions { readUser(userId) }
-
-    return dslContext
-        .select(FUNDING_ENTITY_USERS.FUNDING_ENTITY_ID)
-        .from(FUNDING_ENTITY_USERS)
-        .where(FUNDING_ENTITY_USERS.USER_ID.eq(userId))
-        .fetchOne(FUNDING_ENTITY_USERS.FUNDING_ENTITY_ID)
+    return findFundingEntityByUserId(userId) { record -> record.get(FUNDING_ENTITIES.ID) }
   }
 
   fun fetchEntityByUserId(userId: UserId): FundingEntityModel? {
     requirePermissions { readUser(userId) }
+    return findFundingEntityByUserId(userId) { record -> FundingEntityModel.of(record) }
+  }
 
+  private fun <T> findFundingEntityByUserId(userId: UserId, mapper: (Record) -> T): T? {
     return dslContext
         .select(FUNDING_ENTITIES.asterisk())
         .from(FUNDING_ENTITIES)
         .join(FUNDING_ENTITY_USERS)
         .on(FUNDING_ENTITY_USERS.FUNDING_ENTITY_ID.eq(FUNDING_ENTITIES.ID))
         .where(FUNDING_ENTITY_USERS.USER_ID.eq(userId))
-        .fetchOne { FundingEntityModel.of(it) }
+        .fetchOne(mapper)
   }
 }
