fc6a6d9ea9227aa0d5747a35c944f298342fa1ff
Steven Grimm <steven@terraformation.com>
2025-03-18 15:59:51 -0700

    SW-6587 Add Spring AI libraries
    The experimental Ask Terraware feature will use Spring AI to talk to OpenAI's API.
    Add the dependencies we'll need.
    
    By default, Spring AI's autoconfiguration requires an OpenAI API key to be
    present if the OpenAI client library is available. Override that behavior so that
    developers who aren't working on Ask Terraware don't have to configure API keys.

---
+++ src/main/kotlin/com/terraformation/backend/ask/SpringAiAutoConfig.kt
@@ -0,0 +1,25 @@
+package com.terraformation.backend.ask
+
+import org.springframework.ai.autoconfigure.openai.OpenAiAutoConfiguration
+import org.springframework.ai.autoconfigure.vectorstore.pgvector.PgVectorStoreAutoConfiguration
+import org.springframework.context.annotation.Configuration
+
+/**
+ * Enables configuration of OpenAI client classes if there is an OpenAI API key set in the
+ * environment or in the application properties. By default, Spring Boot will try to create all the
+ * beans and will bomb out if the creation fails, which it will if there's no API key configured.
+ *
+ * We don't want to require dev environments to set OpenAI API keys, so we configure Spring to
+ * ignore the auto-configuration classes (this is done via an exclude rule in `application.yaml`)
+ * and subclass them here, with an additional rule to make them conditional on the presence of the
+ * API key. This gives us the easy configuration of the Spring Boot starter packages without the
+ * "bomb out if there's no API key" behavior.
+ *
+ * TODO: If/when we decide to make OpenAI integration a core part of Terraware rather than just an
+ *   experiment, we'll want to get rid of this setup and make the API key required everywhere.
+ */
+@ConditionalOnSpringAi @Configuration class OpenAiConditionalAutoConfig : OpenAiAutoConfiguration()
+
+@ConditionalOnSpringAi
+@Configuration
+class PgVectorStoreConditionalAutoConfig : PgVectorStoreAutoConfiguration()
