731ce4905770b56d3237276811a608e00f99f5a9
Steven Grimm <steven@terraformation.com>
2025-04-01 16:10:33 -0700

    Remove bogus comment

--- src/main/kotlin/com/terraformation/backend/documentproducer/db/StableIdConverter.kt
+++ src/main/kotlin/com/terraformation/backend/documentproducer/db/StableIdConverter.kt
@@ -1,7 +1,6 @@
 package com.terraformation.backend.documentproducer.db
 
 import com.terraformation.backend.documentproducer.model.StableId
-import java.time.DateTimeException
 import org.jooq.impl.AbstractConverter
 
 /**
@@ -11,12 +10,6 @@ import org.jooq.impl.AbstractConverter
  */
 class StableIdConverter :
     AbstractConverter<String, StableId>(String::class.java, StableId::class.java) {
-  /**
-   * Converts a t.
-   *
-   * @throws DateTimeException The zone name wasn't found in the list of zones recognized by the
-   *   java.time package.
-   */
   override fun from(databaseObject: String?): StableId? = databaseObject?.let { StableId(it) }
 
   override fun to(stableId: StableId?): String? = stableId?.value
