9487026eb4a2bb9d79c8e67cf52a378bc89789e3
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2025-04-07 11:29:59 -0700

    Added Mapbox service to get elevation on one point (#2994)
    This implements a feature in the service class to get elevation data from a point via Mapbox Terrain tilesets. 
    
    There are a few specifications at play and the appropriate reference links have been added into the functions wherever needed. 
    
    The test data was from a second source than the Mapbox terrain data itself, since it is quite difficult to find actual data from the tile itself, but they all proved to be within 0.5% error margin.

--- src/main/kotlin/com/terraformation/backend/tracking/mapbox/MapboxService.kt
+++ src/main/kotlin/com/terraformation/backend/tracking/mapbox/MapboxService.kt
@@ -10,11 +10,18 @@ import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.client.statement.bodyAsText
 import jakarta.inject.Named
+import java.awt.image.BufferedImage
+import java.io.ByteArrayInputStream
+import java.math.BigDecimal
+import java.math.RoundingMode
 import java.net.URI
 import java.net.URL
 import java.time.Instant
 import java.time.temporal.ChronoUnit
+import javax.imageio.ImageIO
+import kotlin.math.*
 import kotlinx.coroutines.runBlocking
+import org.locationtech.jts.geom.Point
 
 @Named
 class MapboxService(
@@ -39,7 +46,7 @@ class MapboxService(
         response.token.user
       } catch (e: ClientRequestException) {
         log.error("HTTP ${e.response.status} when fetching Mapbox token information")
-        log.info("Mapbox error response payload: ${e.response.bodyAsText()}")
+        log.info("Mapbox token error response payload: ${e.response.bodyAsText()}")
         throw MapboxRequestFailedException(e.response.status)
       }
     }
@@ -67,14 +74,101 @@ class MapboxService(
         responsePayload.token
       } catch (e: ClientRequestException) {
         log.error("Mapbox token request failed with HTTP ${e.response.status}")
+        log.info("Mapbox token error response payload: ${e.response.bodyAsText()}")
+        throw MapboxRequestFailedException(e.response.status)
+      }
+    }
+  }
+
+  /** Return elevation in meter given a point */
+  fun getElevation(point: Point): Double {
+    // Zoom level for maximum precision
+    val zoom = 14
+
+    val slippyCoordinate = convertToSlippy(point, zoom)
+    val tile = retrieveMapboxTile(slippyCoordinate)
+
+    // If a tile does not exist, default to sea level.
+    return tile?.let { readHeightFromTile(it, slippyCoordinate.pixelX, slippyCoordinate.pixelY) }
+        ?: 0.0
+  }
+
+  private fun retrieveMapboxTile(slippyCoordinate: SlippyCoordinate): BufferedImage? {
+    // Tile with height data.
+    // Reference: https://docs.mapbox.com/data/tilesets/reference/mapbox-terrain-dem-v1/
+    val tilesetName = "mapbox-terrain-dem-v1"
+    val url = mapboxUrl(slippyCoordinate.mapboxEndpoint(tilesetName))
+
+    return runBlocking {
+      try {
+        val byteArray = httpClient.get(url).body<ByteArray>()
+        ImageIO.read(ByteArrayInputStream(byteArray))
+      } catch (e: ClientRequestException) {
+        if (e.response.body<MapboxClientErrorResponsePayload>().message == "Tile not found") {
+          // This is a special case for this Mapbox tile set when the requested tile is entirely
+          // surrounded by water. Return null for no tile, instead of raising an error.
+          return@runBlocking null
+        }
+
+        // All other errors will be thrown.
+        log.error("HTTP ${e.response.status} when fetching Mapbox tile at $url")
         log.info("Mapbox error response payload: ${e.response.bodyAsText()}")
         throw MapboxRequestFailedException(e.response.status)
       }
     }
   }
 
+  /**
+   * Read pixel value and decode height using the tile specifications. Reference:
+   * https://docs.mapbox.com/data/tilesets/guides/access-elevation-data/#decode-data
+   */
+  private fun readHeightFromTile(tileImage: BufferedImage, pixelX: Int, pixelY: Int): Double {
+    val rgb = tileImage.getRGB(pixelX, pixelY)
+    val offset = BigDecimal((rgb and 0xFFFFFF) * 0.1)
+
+    val elevation = BigDecimal(-10000) + offset
+    return elevation.setScale(1, RoundingMode.HALF_UP).toDouble()
+  }
+
+  /**
+   * Convert a point to the slippy tile, and the pixel location on the tile Reference:
+   * https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
+   */
+  private fun convertToSlippy(point: Point, zoom: Int): SlippyCoordinate {
+    val longitude = point.x
+    val latitude = point.y
+
+    val latRad = Math.toRadians(latitude)
+    val n = 2.0.pow(zoom)
+    val x = n * ((longitude + 180) / 360)
+    val y = n * (1.0 - (ln(tan(latRad) + 1.0 / cos(latRad)) / PI)) / 2.0
+
+    val tileX = floor(x).toInt()
+    val tileY = floor(y).toInt()
+
+    val pixelX = ((x - tileX) * 256).toInt()
+    val pixelY = ((y - tileY) * 256).toInt()
+
+    return SlippyCoordinate(tileX, tileY, zoom, pixelX, pixelY)
+  }
+
   private fun mapboxUrl(endpoint: String): URL =
       URI("https://api.mapbox.com/$endpoint?access_token=${config.mapbox.apiToken}").toURL()
+
+  data class SlippyCoordinate(
+      val x: Int,
+      val y: Int,
+      val zoom: Int,
+      val pixelX: Int,
+      val pixelY: Int,
+  ) {
+    /** Returns the mapbox endpoint to retrieve the tile */
+    fun mapboxEndpoint(tileName: String): String = "v4/mapbox.$tileName/${zoom}/${x}/${y}.pngraw"
+
+    override fun toString(): String {
+      return "z=$zoom x=$x y=$y pixel=($pixelX, $pixelY)"
+    }
+  }
 
   data class TemporaryTokenRequestPayload(
       val expires: Instant,
@@ -86,4 +180,6 @@ class MapboxService(
   data class RetrieveTokenResponsePayload(val code: String, val token: Token) {
     data class Token(val user: String)
   }
+
+  data class MapboxClientErrorResponsePayload(val message: String)
 }
---
+++ src/test/kotlin/com/terraformation/backend/tracking/mapbox/MapboxServiceExternalTest.kt
@@ -0,0 +1,115 @@
+package com.terraformation.backend.tracking.mapbox
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.terraformation.backend.RunsAsUser
+import com.terraformation.backend.config.TerrawareServerConfig
+import com.terraformation.backend.customer.model.TerrawareUser
+import com.terraformation.backend.db.SRID
+import com.terraformation.backend.mockUser
+import com.terraformation.backend.util.HttpClientConfig
+import io.ktor.client.engine.java.Java
+import io.mockk.every
+import java.net.URI
+import kotlin.math.abs
+import org.junit.Assume.assumeNotNull
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+import org.locationtech.jts.geom.Coordinate
+import org.locationtech.jts.geom.GeometryFactory
+import org.locationtech.jts.geom.Point
+import org.locationtech.jts.geom.PrecisionModel
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+
+@EnableConfigurationProperties(TerrawareServerConfig::class)
+class MapboxServiceExternalTest : RunsAsUser {
+  override val user: TerrawareUser = mockUser()
+  private lateinit var service: MapboxService
+
+  @BeforeEach
+  fun setUp() {
+    every { user.canDeleteSupportIssue() } returns true
+    val apiToken = getEnvOrSkipTest("TERRAWARE_MAPBOX_APITOKEN")
+
+    val config =
+        TerrawareServerConfig(
+            webAppUrl = URI("https://terraware.io"),
+            keycloak =
+                TerrawareServerConfig.KeycloakConfig(
+                    apiClientId = "test",
+                    apiClientGroupName = "test",
+                    apiClientUsernamePrefix = "test"),
+            mapbox =
+                TerrawareServerConfig.MapboxConfig(
+                    apiToken = apiToken,
+                    temporaryTokenExpirationMinutes = 30L,
+                ))
+
+    val httpClient = HttpClientConfig().httpClient(Java.create(), jacksonObjectMapper())
+    service = MapboxService(config, httpClient)
+  }
+
+  @MethodSource("elevationTestData")
+  @ParameterizedTest
+  fun `can fetch elevation data`(elevationDataPoint: ElevationDataPoint) {
+    val actual = service.getElevation(elevationDataPoint.point)
+    assertApproximatelyEqual(
+        elevationDataPoint.elevation, actual, 0.5, "Fetching elevation for $elevationDataPoint")
+  }
+
+  private fun getEnvOrSkipTest(name: String): String {
+    val value = System.getenv(name)
+    assumeNotNull(value, "$name not set; skipping test")
+    return value
+  }
+
+  data class ElevationDataPoint(val name: String, val point: Point, val elevation: Double) {
+    override fun toString(): String {
+      return "$name, Latitude: ${point.y}, Longitude: ${point.x}"
+    }
+  }
+
+  private fun assertApproximatelyEqual(
+      expected: Double,
+      actual: Double,
+      percentThreshold: Double = 1.0,
+      message: String = ""
+  ) {
+    val difference = abs(actual - expected)
+    val relativeDifference = difference / Math.abs(expected) * 100
+    if (relativeDifference > percentThreshold) {
+      assertEquals(expected, actual, message)
+    }
+  }
+
+  companion object {
+
+    // https://latlongdata.com/elevation/
+    @JvmStatic
+    fun elevationTestData(): List<ElevationDataPoint> {
+      val geometryFactory = GeometryFactory(PrecisionModel(), SRID.LONG_LAT)
+      return listOf(
+          ElevationDataPoint(
+              "Mount Everest", geometryFactory.createPoint(Coordinate(86.9249, 27.9881)), 8730.0),
+          ElevationDataPoint(
+              "Death Valley", geometryFactory.createPoint(Coordinate(-116.9325, 36.5323)), -80.8),
+          ElevationDataPoint(
+              "Grand Canyon", geometryFactory.createPoint(Coordinate(-112.1124, 36.0998)), 733.0),
+          ElevationDataPoint(
+              "Kilimanjaro", geometryFactory.createPoint(Coordinate(37.3556, -3.0674)), 5826.3),
+          ElevationDataPoint(
+              "Mount Whitney", geometryFactory.createPoint(Coordinate(-118.2922, 36.5785)), 4412.4),
+          ElevationDataPoint(
+              "Sahara Desert", geometryFactory.createPoint(Coordinate(25.6628, 23.4162)), 982.6),
+          // Some ocean locations have tiles but the tile set will return 0.0 elevation instead of
+          // the actual depth.
+          ElevationDataPoint(
+              "Mariana Trench", geometryFactory.createPoint(Coordinate(142.1995, 11.3493)), 0.0),
+          // This is a random location in the Pacific Ocean, which has no tile data. The service
+          // will return 0.0.
+          ElevationDataPoint(
+              "Pacific Ocean", geometryFactory.createPoint(Coordinate(-155.45, 24.79)), 0.0))
+    }
+  }
+}
