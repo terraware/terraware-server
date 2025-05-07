42a2f8e791668b5f03ed8c3e8c31ce8d7a726574
Steven Grimm <steven@terraformation.com>
2025-05-06 18:08:10 -0700

    SW-6895 Prune old timeseries values
    The `timeseries_values` table is by far the largest one in the production
    database, and most of its contents are of no interest. We only need recent history
    for most of the timeseries. However, for certain timeseries, we want to retain
    historical data forever, so we can't just apply a fixed retention period across
    the board.
    
    Add a column to the timeseries table to configure how many days' worth of values
    to retain, and add a recurring job that deletes old values based on each
    timeseries' retention setting. To avoid accidentally deleting values that we need
    to retain indefinitely, this uses an opt-in model: the default is to not prune any
    old values, and we'll set the retention days on the specific timeseries we want to
    trim. The opt-in approach also lets us roll this out gradually, starting with just
    a few timeseries to get a better understanding of the real-world performance
    impact.

---
+++ src/main/kotlin/com/terraformation/backend/device/TimeseriesPruner.kt
@@ -0,0 +1,80 @@
+package com.terraformation.backend.device
+
+import com.terraformation.backend.db.asNonNullable
+import com.terraformation.backend.db.default_schema.TimeseriesId
+import com.terraformation.backend.db.default_schema.tables.references.TIMESERIES
+import com.terraformation.backend.db.default_schema.tables.references.TIMESERIES_VALUES
+import com.terraformation.backend.log.perClassLogger
+import jakarta.inject.Named
+import java.time.InstantSource
+import java.time.temporal.ChronoUnit
+import org.jobrunr.jobs.annotations.Recurring
+import org.jooq.DSLContext
+import org.jooq.impl.DSL
+
+@Named
+class TimeseriesPruner(
+    private val clock: InstantSource,
+    private val dslContext: DSLContext,
+) {
+  /** Only delete this many old values at a time from each timeseries. */
+  var maxRowsToDelete: Int = 10000
+
+  private val log = perClassLogger()
+
+  /**
+   * Prunes data from the `timeseries_values` table based on per-timeseries retention settings. By
+   * default, a timeseries isn't pruned at all; an admin needs to set its `retention_days` column to
+   * the number of days of data to retain.
+   *
+   * To avoid slamming the database with a gigantic delete operation when a retention limit is first
+   * configured on a timeseries with a lot of existing values, this will only delete up to
+   * [maxRowsToDelete] rows per timeseries each time it runs. Since this job runs repeatedly, the
+   * deletion will gradually catch up to the desired number of retention days.
+   */
+  @Recurring(id = "pruneTimeseriesValues", cron = "8,23,38,53 * * * *")
+  fun pruneTimeseriesValues() {
+    var totalRowsDeleted = 0
+    val timeseriesIdsDeleted = mutableListOf<TimeseriesId>()
+
+    log.debug("Scanning for timeseries to prune")
+
+    try {
+      dslContext
+          .select(TIMESERIES.ID.asNonNullable(), TIMESERIES.RETENTION_DAYS.asNonNullable())
+          .from(TIMESERIES)
+          .where(TIMESERIES.RETENTION_DAYS.isNotNull)
+          .orderBy(TIMESERIES.ID)
+          .fetch()
+          .forEach { (timeseriesId, retentionDays) ->
+            val minimumCreatedTime = clock.instant().minus(retentionDays.toLong(), ChronoUnit.DAYS)
+            val rowsDeleted =
+                with(TIMESERIES_VALUES) {
+                  dslContext
+                      .deleteFrom(TIMESERIES_VALUES)
+                      .where(TIMESERIES_ID.eq(timeseriesId))
+                      .and(
+                          CREATED_TIME.`in`(
+                              DSL.select(CREATED_TIME)
+                                  .from(TIMESERIES_VALUES)
+                                  .where(TIMESERIES_ID.eq(timeseriesId))
+                                  .and(CREATED_TIME.lessThan(minimumCreatedTime))
+                                  .orderBy(CREATED_TIME)
+                                  .limit(maxRowsToDelete)))
+                      .execute()
+                }
+
+            if (rowsDeleted > 0) {
+              totalRowsDeleted += rowsDeleted
+              timeseriesIdsDeleted.add(timeseriesId)
+            }
+          }
+    } catch (e: Exception) {
+      log.error("Error while pruning timeseries values", e)
+    }
+
+    if (totalRowsDeleted > 0) {
+      log.info("Deleted $totalRowsDeleted values from timeseries $timeseriesIdsDeleted")
+    }
+  }
+}
