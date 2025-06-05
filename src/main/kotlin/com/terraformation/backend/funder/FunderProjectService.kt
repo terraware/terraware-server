fe2ff6bcbfa840b2caacc0195a17f964b68d2bf1
Isaac Hudson <isaac.hudson@terraformation.com>
2025-06-05 11:42:31 -0700

    SW-7051 Retrieve Published Project Profile data for Funders (#3157)

--- src/main/kotlin/com/terraformation/backend/funder/FunderProjectService.kt
+++ src/main/kotlin/com/terraformation/backend/funder/FunderProjectService.kt
@@ -1,9 +1,7 @@
 package com.terraformation.backend.funder
 
-import com.terraformation.backend.accelerator.db.ProjectAcceleratorDetailsStore
-import com.terraformation.backend.accelerator.variables.AcceleratorProjectVariableValuesService
-import com.terraformation.backend.customer.model.SystemUser
 import com.terraformation.backend.customer.model.requirePermissions
+import com.terraformation.backend.db.ProjectNotFoundException
 import com.terraformation.backend.db.default_schema.ProjectId
 import com.terraformation.backend.funder.db.PublishedProjectDetailsStore
 import com.terraformation.backend.funder.model.FunderProjectDetailsModel
@@ -11,17 +9,13 @@ import jakarta.inject.Named
 
 @Named
 class FunderProjectService(
-    private val acceleratorProjectVariableValuesService: AcceleratorProjectVariableValuesService,
-    private val projectAcceleratorDetailsStore: ProjectAcceleratorDetailsStore,
-    private val systemUser: SystemUser,
     private val publishedProjectDetailsStore: PublishedProjectDetailsStore,
 ) {
   fun fetchByProjectId(projectId: ProjectId): FunderProjectDetailsModel {
     requirePermissions { readProjectFunderDetails(projectId) }
-    return systemUser.run {
-      val variableValues = acceleratorProjectVariableValuesService.fetchValues(projectId)
-      projectAcceleratorDetailsStore.fetchOneByIdForFunder(projectId, variableValues)
-    }
+
+    return publishedProjectDetailsStore.fetchOneById(projectId)
+        ?: throw ProjectNotFoundException(projectId)
   }
 
   fun publishProjectProfile(model: FunderProjectDetailsModel) {
--- src/test/kotlin/com/terraformation/backend/funder/FunderProjectServiceTest.kt
+++ src/test/kotlin/com/terraformation/backend/funder/FunderProjectServiceTest.kt
@@ -1,10 +1,6 @@
 package com.terraformation.backend.funder
 
 import com.terraformation.backend.RunsAsDatabaseUser
-import com.terraformation.backend.accelerator.db.ProjectAcceleratorDetailsStore
-import com.terraformation.backend.accelerator.model.ProjectAcceleratorVariableValuesModel
-import com.terraformation.backend.accelerator.variables.AcceleratorProjectVariableValuesService
-import com.terraformation.backend.customer.model.SystemUser
 import com.terraformation.backend.customer.model.TerrawareUser
 import com.terraformation.backend.db.DatabaseTest
 import com.terraformation.backend.db.ProjectNotFoundException
@@ -21,18 +17,12 @@ import org.junit.jupiter.api.assertThrows
 
 class FunderProjectServiceTest : DatabaseTest(), RunsAsDatabaseUser {
   override lateinit var user: TerrawareUser
-  private val systemUser by lazy { SystemUser(usersDao) }
 
-  val acceleratorProjectVariableValuesService: AcceleratorProjectVariableValuesService = mockk()
-  val projectAcceleratorDetailsStore: ProjectAcceleratorDetailsStore = mockk()
   val publishedProjectDetailsStore: PublishedProjectDetailsStore = mockk()
   lateinit var projectId: ProjectId
 
   private val service: FunderProjectService by lazy {
     FunderProjectService(
-        acceleratorProjectVariableValuesService = acceleratorProjectVariableValuesService,
-        projectAcceleratorDetailsStore = projectAcceleratorDetailsStore,
-        systemUser = systemUser,
         publishedProjectDetailsStore = publishedProjectDetailsStore,
     )
   }
@@ -42,13 +32,9 @@ class FunderProjectServiceTest : DatabaseTest(), RunsAsDatabaseUser {
     insertOrganization()
     projectId = insertProject()
 
-    val existingValues = ProjectAcceleratorVariableValuesModel(projectId = projectId)
     val existingDetails = FunderProjectDetailsModel(projectId = projectId)
 
-    every { acceleratorProjectVariableValuesService.fetchValues(projectId) } returns existingValues
-    every {
-      projectAcceleratorDetailsStore.fetchOneByIdForFunder(projectId, existingValues)
-    } returns existingDetails
+    every { publishedProjectDetailsStore.fetchOneById(projectId) } returns existingDetails
   }
 
   @Test
