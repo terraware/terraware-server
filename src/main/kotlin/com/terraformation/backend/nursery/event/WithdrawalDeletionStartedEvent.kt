339eb583460090e35cfe78053f325b0b57348436
Steven Grimm <steven@terraformation.com>
2025-06-16 11:03:27 -0700

    SW-7057 Set quantities to 0 on batch delete
    When the user deletes a batch that has had withdrawals, keep it around but set its
    remaining quantities to zero. This will remove it from the list of active batches
    in the web app, while preserving its entries in the withdrawal log.
    
    The reasoning is that users are using the delete function just to clear irrelevant
    batches out of their inventory, not to forget the fact that the batches ever
    existed.
    
    However, for batches that have had no withdrawals, we continue to delete them
    outright. This will have no effect on the nursery's withdrawal log, but will
    remove batches that were entered by mistake and never used for anything.

--- src/main/kotlin/com/terraformation/backend/nursery/event/WithdrawalDeletionStartedEvent.kt
+++
@@ -1,6 +0,0 @@
-package com.terraformation.backend.nursery.event
-
-import com.terraformation.backend.db.nursery.WithdrawalId
-
-/** Published when a withdrawal is about to be deleted from the database. */
-data class WithdrawalDeletionStartedEvent(val withdrawalId: WithdrawalId)
