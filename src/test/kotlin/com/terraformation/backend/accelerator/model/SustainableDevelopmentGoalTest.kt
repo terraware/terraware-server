89aab5b62e107f4a49d371a721ef75a2c7ee133c
Isaac Hudson <isaac.hudson@terraformation.com>
2025-07-02 09:42:24 -0700

    SW-7113 Fix Seed Fund Reports with SDGs

--- src/test/kotlin/com/terraformation/backend/accelerator/model/SustainableDevelopmentGoalTest.kt
+++ src/test/kotlin/com/terraformation/backend/accelerator/model/SustainableDevelopmentGoalTest.kt
@@ -24,4 +24,60 @@ class SustainableDevelopmentGoalTest {
     assertEquals(
         SustainableDevelopmentGoal.Partnerships, SustainableDevelopmentGoal.forJsonValue("17"))
   }
+
+  @Test
+  fun `forJsonValue works correctly for names`() {
+    assertEquals(
+        SustainableDevelopmentGoal.NoPoverty, SustainableDevelopmentGoal.forJsonValue("NoPoverty"))
+    assertEquals(
+        SustainableDevelopmentGoal.ZeroHunger,
+        SustainableDevelopmentGoal.forJsonValue("ZeroHunger"))
+    assertEquals(
+        SustainableDevelopmentGoal.GoodHealth,
+        SustainableDevelopmentGoal.forJsonValue("GoodHealth"))
+    assertEquals(
+        SustainableDevelopmentGoal.QualityEducation,
+        SustainableDevelopmentGoal.forJsonValue("QualityEducation"))
+    assertEquals(
+        SustainableDevelopmentGoal.GenderEquality,
+        SustainableDevelopmentGoal.forJsonValue("GenderEquality"))
+    assertEquals(
+        SustainableDevelopmentGoal.CleanWater,
+        SustainableDevelopmentGoal.forJsonValue("CleanWater"))
+    assertEquals(
+        SustainableDevelopmentGoal.AffordableEnergy,
+        SustainableDevelopmentGoal.forJsonValue("AffordableEnergy"))
+    assertEquals(
+        SustainableDevelopmentGoal.DecentWork,
+        SustainableDevelopmentGoal.forJsonValue("DecentWork"))
+    assertEquals(
+        SustainableDevelopmentGoal.Industry, SustainableDevelopmentGoal.forJsonValue("Industry"))
+    assertEquals(
+        SustainableDevelopmentGoal.ReducedInequalities,
+        SustainableDevelopmentGoal.forJsonValue("ReducedInequalities"))
+    assertEquals(
+        SustainableDevelopmentGoal.SustainableCities,
+        SustainableDevelopmentGoal.forJsonValue("SustainableCities"))
+    assertEquals(
+        SustainableDevelopmentGoal.ResponsibleConsumption,
+        SustainableDevelopmentGoal.forJsonValue("ResponsibleConsumption"))
+    assertEquals(
+        SustainableDevelopmentGoal.ClimateAction,
+        SustainableDevelopmentGoal.forJsonValue("ClimateAction"))
+    assertEquals(
+        SustainableDevelopmentGoal.LifeBelowWater,
+        SustainableDevelopmentGoal.forJsonValue("LifeBelowWater"))
+    assertEquals(
+        SustainableDevelopmentGoal.LifeOnLand,
+        SustainableDevelopmentGoal.forJsonValue("LifeOnLand"))
+    assertEquals(SustainableDevelopmentGoal.Peace, SustainableDevelopmentGoal.forJsonValue("Peace"))
+    assertEquals(
+        SustainableDevelopmentGoal.Partnerships,
+        SustainableDevelopmentGoal.forJsonValue("Partnerships"))
+  }
+
+  @Test
+  fun `forJsonValue throws when doesn't exist`() {
+    assertThrows<IllegalArgumentException> { SustainableDevelopmentGoal.forJsonValue("BadValue") }
+  }
 }
