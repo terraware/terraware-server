526dc3ae39eb424095ff4bf96a53e21df24ec5a5
Isaac Hudson <isaac.hudson@terraformation.com>
2025-07-23 13:19:27 -0700

    SW-7184 Add variable value options to search api (#3235)
    Add support for link variables.
    Add support for select variables.
    Add tests.

--- src/main/kotlin/com/terraformation/backend/search/table/ProjectVariableValuesTable.kt
+++ src/main/kotlin/com/terraformation/backend/search/table/ProjectVariableValuesTable.kt
@@ -6,6 +6,10 @@ import com.terraformation.backend.db.default_schema.ProjectId
 import com.terraformation.backend.db.default_schema.tables.references.PROJECTS
 import com.terraformation.backend.db.docprod.VariableId
 import com.terraformation.backend.db.docprod.VariableValueId
+import com.terraformation.backend.db.docprod.tables.references.VARIABLE_LINK_VALUES
+import com.terraformation.backend.db.docprod.tables.references.VARIABLE_SELECTS
+import com.terraformation.backend.db.docprod.tables.references.VARIABLE_SELECT_OPTIONS
+import com.terraformation.backend.db.docprod.tables.references.VARIABLE_SELECT_OPTION_VALUES
 import com.terraformation.backend.db.docprod.tables.references.VARIABLE_VALUES
 import com.terraformation.backend.search.SearchTable
 import com.terraformation.backend.search.SublistField
@@ -19,7 +23,22 @@ class ProjectVariableValuesTable(tables: SearchTables) : SearchTable() {
   override val primaryKey: TableField<out Record, out Any?>
     get() = PROJECT_VARIABLE_VALUES.PROJECT_VARIABLE_VALUE_ID
 
-  override val sublists: List<SublistField> by lazy { with(tables) { emptyList() } }
+  override val sublists: List<SublistField> by lazy {
+    with(tables) {
+      listOf(
+          variableSelectOptions.asMultiValueSublist(
+              "options",
+              DSL.exists(
+                  DSL.selectOne()
+                      .from(VARIABLE_SELECT_OPTION_VALUES)
+                      .where(
+                          VARIABLE_SELECT_OPTION_VALUES.VARIABLE_VALUE_ID.eq(
+                              PROJECT_VARIABLE_VALUES.VARIABLE_VALUE_ID))
+                      .and(VARIABLE_SELECT_OPTION_VALUES.OPTION_ID.eq(VARIABLE_SELECT_OPTIONS.ID))),
+          ),
+      )
+    }
+  }
 
   override val fields: List<SearchField> =
       listOf(
@@ -55,6 +74,21 @@ class ProjectVariableValuesTable(tables: SearchTables) : SearchTable() {
                   DSL.select(VARIABLE_VALUES.DATE_VALUE)
                       .from(VARIABLE_VALUES)
                       .where(VARIABLE_VALUES.ID.eq(PROJECT_VARIABLE_VALUES.VARIABLE_VALUE_ID)))),
+          textField(
+              "linkValue",
+              DSL.field(
+                  DSL.select(VARIABLE_LINK_VALUES.URL)
+                      .from(VARIABLE_LINK_VALUES)
+                      .where(
+                          VARIABLE_LINK_VALUES.VARIABLE_VALUE_ID.eq(
+                              PROJECT_VARIABLE_VALUES.VARIABLE_VALUE_ID)))),
+          booleanField(
+              "isMultiSelect",
+              DSL.field(
+                  DSL.select(VARIABLE_SELECTS.IS_MULTIPLE)
+                      .from(VARIABLE_SELECTS)
+                      .where(
+                          VARIABLE_SELECTS.VARIABLE_ID.eq(PROJECT_VARIABLE_VALUES.VARIABLE_ID)))),
       )
 
   override fun conditionForVisibility(): Condition {
