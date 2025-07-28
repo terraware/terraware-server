25f5ddb5e766652d1d7984c30798367c72d83362
Isaac Hudson <isaac.hudson@terraformation.com>
2025-07-28 15:20:07 -0700

    Cleanup payload

--- src/main/kotlin/com/terraformation/backend/search/api/SearchController.kt
+++ src/main/kotlin/com/terraformation/backend/search/api/SearchController.kt
@@ -75,7 +75,7 @@ class SearchController(
         searchService.search(
             rootPrefix,
             payload.fields.map { rootPrefix.resolve(it) },
-            mapOf(rootPrefix to payload.toSearchNode(rootPrefix)),
+            payload.toSearchCriteria(rootPrefix),
             payload.getSearchSortFields(rootPrefix),
             payload.cursor,
             count))
@@ -208,6 +208,7 @@ data class SearchRequestPayload(
                            { "operation": "field", "field": "remainingUnits", "values": ["Seeds"] },
                            { "operation": "field", "field": "remainingQuantity", "type": "Range", "values": ["30", "40"] } ] } ] } ] }""")
     override val search: SearchNodePayload? = null,
+    override val sublistSearch: List<SublistSearchPayload>? = null,
     @Schema(
         description =
             "Maximum number of top-level search results to return. The system may impose a limit " +
@@ -223,4 +224,9 @@ data class SearchRequestPayload(
                 "from where it left off. This should be the value of the cursor that was " +
                 "returned in the response to a previous search.")
     val cursor: String? = null,
-) : HasSearchFields, HasSearchNode, HasSortOrder
+) : HasSearchFields, HasSearchNode, HasSortOrder, HasSearchCriteria
+
+data class SublistSearchPayload(
+    val prefix: String,
+    override val search: SearchNodePayload,
+) : HasSearchNode
