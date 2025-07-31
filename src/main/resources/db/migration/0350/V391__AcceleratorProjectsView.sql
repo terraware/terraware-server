ce496146a1c0d37bb49b902928db9bd15d267378
Isaac Hudson <isaac.hudson@terraformation.com>
2025-07-31 09:43:05 -0700

    Create Accelerator Projects DB View (#3255)
    Create a DB view to consolidate all the definitions of accelerator projects/orgs.
    
    This may later be dropped in favor of data model changes to simplify the definition.

---
+++ src/main/resources/db/migration/0350/V391__AcceleratorProjectsView.sql
@@ -0,0 +1,20 @@
+CREATE VIEW accelerator.accelerator_projects AS
+SELECT *
+FROM (SELECT p.id    as project_id,
+             a.id    as application_id,
+             part.id as participant_id,
+             part.cohort_id,
+             EXISTS (SELECT 1
+                     FROM organization_internal_tags oit
+                     WHERE oit.organization_id = p.organization_id
+                       AND oit.internal_tag_id = 4)
+                     as has_org_accelerator_tag
+      FROM projects p
+               LEFT JOIN accelerator.applications a ON p.id = a.project_id
+               LEFT JOIN accelerator.participants part ON p.participant_id = part.id
+               LEFT JOIN accelerator.project_accelerator_details pad
+                         ON p.id = pad.project_id) as accelerator_projects
+WHERE application_id IS NOT NULL
+   OR participant_id IS NOT NULL
+   OR has_org_accelerator_tag IS TRUE
+;
