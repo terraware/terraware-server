a7e5bc50242fabf319a0cded99e14fdc988735a7
Isaac Hudson <isaac.hudson@terraformation.com>
2025-09-09 14:38:58 -0700

    Add Survival Rate comments to DB columns

--- src/main/resources/db/migration/R__Comments.sql
+++ src/main/resources/db/migration/R__Comments.sql
@@ -442,6 +442,7 @@ COMMENT ON TABLE tracking.observed_plot_species_totals IS 'Aggregated per-monito
 COMMENT ON COLUMN tracking.observed_plot_species_totals.cumulative_dead IS 'If this is a permanent monitoring plot, total number of dead plants observed in all observations including the current one.';
 COMMENT ON COLUMN tracking.observed_plot_species_totals.mortality_rate IS 'If this is a permanent monitoring plot, percentage of plants of the species observed in this plot, in either this observation or in previous ones, that were dead. Null if this is not a permanent monitoring plot in the current observation.';
 COMMENT ON COLUMN tracking.observed_plot_species_totals.permanent_live IS 'If this is a permanent monitoring plot, the number of live and existing plants observed. 0 otherwise.';
+COMMENT ON COLUMN tracking.observed_plot_species_totals.survival_rate IS 'If this is a permanent monitoring plot, percentage of plants of the species observed in this plot, in either this observation or in previous ones, that have survived since the t0 point. Null if this is not a permanent monitoring plot in the current observation.';
 
 COMMENT ON TABLE tracking.observation_plot_statuses IS '(Enum) The status of an observation plot.';
 
@@ -449,16 +450,19 @@ COMMENT ON TABLE tracking.observed_site_species_totals IS 'Aggregated per-planti
 COMMENT ON COLUMN tracking.observed_site_species_totals.cumulative_dead IS 'Total number of dead plants of the species observed, both in this observation and in all previous ones, in plots that are included as permanent plots in this observation.';
 COMMENT ON COLUMN tracking.observed_site_species_totals.mortality_rate IS 'Percentage of plants of the species observed in permanent monitoring plots in the planting site, in either this observation or in previous ones, that were dead.';
 COMMENT ON COLUMN tracking.observed_site_species_totals.permanent_live IS 'The number of live and existing plants observed in permanent monitoring plots.';
+COMMENT ON COLUMN tracking.observed_site_species_totals.survival_rate IS 'Percentage of plants of the species observed in permanent monitoring plots in the planting site, in either this observation or in previous ones, that have survived since the t0 point.';
 
 COMMENT ON TABLE tracking.observed_subzone_species_totals IS 'Aggregated per-planting-subzone, per-species totals of plants recorded during observations.';
 COMMENT ON COLUMN tracking.observed_subzone_species_totals.cumulative_dead IS 'Total number of dead plants of the species observed, both in this observation and in all previous ones, in plots in this subzone that are included as permanent plots in this observation.';
 COMMENT ON COLUMN tracking.observed_subzone_species_totals.mortality_rate IS 'Percentage of plants of the species observed in permanent monitoring plots in the planting subzone, in either the current observation or in previous ones, that were dead.';
 COMMENT ON COLUMN tracking.observed_subzone_species_totals.permanent_live IS 'The number of live and existing plants observed in permanent monitoring plots.';
+COMMENT ON COLUMN tracking.observed_subzone_species_totals.survival_rate IS 'Percentage of plants of the species observed in permanent monitoring plots in the planting subzone, in either the current observation or in previous ones, that have survived since the t0 point.';
 
 COMMENT ON TABLE tracking.observed_zone_species_totals IS 'Aggregated per-planting-zone, per-species totals of plants recorded during observations.';
 COMMENT ON COLUMN tracking.observed_zone_species_totals.cumulative_dead IS 'Total number of dead plants of the species observed, both in this observation and in all previous ones, in plots in this zone that are included as permanent plots in this observation.';
 COMMENT ON COLUMN tracking.observed_zone_species_totals.mortality_rate IS 'Percentage of plants of the species observed in permanent monitoring plots in the planting zone, in either the current observation or in previous ones, that were dead.';
 COMMENT ON COLUMN tracking.observed_zone_species_totals.permanent_live IS 'The number of live and existing plants observed in permanent monitoring plots.';
+COMMENT ON COLUMN tracking.observed_zone_species_totals.survival_rate IS 'Percentage of plants of the species observed in permanent monitoring plots in the planting zone, in either the current observation or in previous ones, that have survived since the t0 point.';
 
 COMMENT ON TABLE tracking.planting_seasons IS 'Start and end dates of planting seasons for planting sites.';
 COMMENT ON COLUMN tracking.planting_seasons.end_date IS 'What day the planting season ends. This is the last day of the season, not the day after the season, that is, if the planting season is the month of January, this will be January 31, not February 1.';
