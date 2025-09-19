af1df02da61c030202c7dbb61753ad48294ef43b
Isaac Hudson <isaac.hudson@terraformation.com>
2025-09-19 10:10:00 -0700

    Remove report survival rate system metrics

--- src/main/resources/db/migration/0400/V410__RemoveSurvivalRateSystemMetric.sql
+++ src/main/resources/db/migration/0400/V410__RemoveSurvivalRateSystemMetric.sql
@@ -1,2 +1,8 @@
+DELETE FROM accelerator.report_system_metrics
+    WHERE system_metric_id = 7;
+
+DELETE FROM funder.published_report_system_metrics
+    WHERE system_metric_id = 7;
+
 DELETE FROM accelerator.system_metrics
 WHERE id = 7;
