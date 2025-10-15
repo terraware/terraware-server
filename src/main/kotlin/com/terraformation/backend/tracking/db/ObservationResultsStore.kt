19cbcf0fc77322e1f9b15358b73e4f1e880a942e
Isaac Hudson <isaac.hudson@terraformation.com>
2025-10-15 11:35:08 -0700

    Refactor latest observation for subzone query (#3509)
    Move the query used to get the latest observation for each subzone to a common place.

--- src/main/kotlin/com/terraformation/backend/tracking/db/ObservationResultsStore.kt
+++ src/main/kotlin/com/terraformation/backend/tracking/db/ObservationResultsStore.kt
@@ -29,7 +29,6 @@ import com.terraformation.backend.db.tracking.tables.references.OBSERVATION_BIOM
 import com.terraformation.backend.db.tracking.tables.references.OBSERVATION_PHOTOS
 import com.terraformation.backend.db.tracking.tables.references.OBSERVATION_PLOTS
 import com.terraformation.backend.db.tracking.tables.references.OBSERVATION_PLOT_CONDITIONS
-import com.terraformation.backend.db.tracking.tables.references.OBSERVATION_REQUESTED_SUBZONES
 import com.terraformation.backend.db.tracking.tables.references.OBSERVED_PLOT_COORDINATES
 import com.terraformation.backend.db.tracking.tables.references.OBSERVED_PLOT_SPECIES_TOTALS
 import com.terraformation.backend.db.tracking.tables.references.OBSERVED_SITE_SPECIES_TOTALS
@@ -1478,32 +1477,6 @@ class ObservationResultsStore(private val dslContext: DSLContext) {
       )
     }
   }
-
-  private fun latestObservationForSubzoneField(observationIdField: Field<ObservationId?>) =
-      with(OBSERVED_SUBZONE_SPECIES_TOTALS) {
-        DSL.select(OBSERVATIONS.ID)
-            .from(OBSERVATIONS)
-            .join(OBSERVATION_REQUESTED_SUBZONES)
-            .on(OBSERVATIONS.ID.eq(OBSERVATION_REQUESTED_SUBZONES.OBSERVATION_ID))
-            .where(OBSERVATION_REQUESTED_SUBZONES.PLANTING_SUBZONE_ID.eq(PLANTING_SUBZONE_ID))
-            .and(
-                OBSERVATIONS.COMPLETED_TIME.le(
-                    DSL.select(OBSERVATIONS.COMPLETED_TIME)
-                        .from(OBSERVATIONS)
-                        .where(OBSERVATIONS.ID.eq(observationIdField))
-                )
-            )
-            .and(OBSERVATIONS.IS_AD_HOC.isFalse)
-            .orderBy(
-                // Prefer results from the same observation as the `observationId` if it exists.
-                // True is considered greater than false, so sorting by an "=" expression in
-                // descending order means the matches come first.
-                OBSERVATION_ID.eq(observationIdField).desc(),
-                // Otherwise take the results from the next latest observation for that area.
-                OBSERVATIONS.COMPLETED_TIME.desc(),
-            )
-            .limit(1)
-      }
 
   private fun fetchByCondition(
       condition: Condition,
