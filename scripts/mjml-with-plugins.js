99401b4fff07195c511e25e23864d655ef0e4e81
Steven Grimm <1248649+sgrimm@users.noreply.github.com>
2025-10-23 09:38:09 -0700

    Upgrade to MJML 4.16.1 (#3536)
    This requires reworking the wrapper script we use to load MJML plugins; the
    previous version of the script has an issue with what order various packages
    are loaded that causes it to try to register the plugins before they're
    fully initialized.

--- scripts/mjml-with-plugins.js
+++ scripts/mjml-with-plugins.js
@@ -1,12 +1,32 @@
 #!/usr/bin/env node
+/**
+ * Wrapper for the MJML command-line interface that loads plugins. The plugins we use have some
+ * circular dependencies that, if they're loaded normally, cause them to be half-initialized
+ * when they're registered with MJML. The wrapper script loads everything in the right order
+ * such that it can register fully-initialized plugins.
+ */
 
-// This needs to be loaded before the plugins to avoid circular dependencies if they try
-// to require it themselves.
-require('mjml');
+const plugins = [
+    require("mjml-bullet-list/lib/MjList"),
+    require("mjml-bullet-list/lib/MjLi"),
+];
 
-// If you add more plugins, also update the list of packages that gets passed to the
-// mjml command in .mjmlconfig in the repo root directory.
-require('mjml-bullet-list');
+// Load the vendored copies of the MJML core libraries that mjml-cli uses.
+const { createRequire } = require("module");
+const mjmlCliRequire = createRequire(require.resolve("mjml-cli/package.json"));
+const cliCore = mjmlCliRequire("mjml-core");
+const cliValidator = mjmlCliRequire("mjml-validator");
+const cliPreset = mjmlCliRequire("mjml-preset-core");
+
+cliCore.assignComponents(cliCore.components, cliPreset.components);
+cliValidator.registerDependencies(cliPreset.dependencies);
+
+plugins
+    .map((moduleExport) => moduleExport.default || moduleExport)
+    .forEach((component) => {
+        cliCore.registerComponent(component);
+        cliValidator.registerDependencies(component.dependencies || {});
+    });
 
 // This needs to come last.
-require('mjml-cli');
+require("mjml-cli");
