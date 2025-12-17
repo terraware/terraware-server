8acd7571b8ced33622380aa74a957998762c7a35
Isaac Hudson <isaac.hudson@terraformation.com>
2025-12-17 13:57:30 -0800

    Only delete mangroves survival rate

--- src/main/resources/db/migration/0400/V431__DeleteMortalityRateMetric.sql
+++ src/main/resources/db/migration/0400/V431__DeleteMortalityRateMetric.sql
@@ -4,10 +4,10 @@ DELETE FROM funder.published_report_system_metrics WHERE system_metric_id = 5;
 DELETE FROM accelerator.system_metrics WHERE id = 5;
 
 -- delete project metrics that were setup temporarily for survival rate
-DELETE FROM accelerator.report_project_metrics WHERE project_metric_id IN (
-    SELECT id FROM accelerator.project_metrics WHERE name LIKE 'Survival rate%'
+DELETE FROM accelerator.report_project_metrics WHERE project_metric_id = (
+    SELECT id FROM accelerator.project_metrics WHERE name = 'Survival rate (%) of planted mangroves'
 );
-DELETE FROM funder.published_report_project_metrics WHERE project_metric_id IN (
-    SELECT id FROM accelerator.project_metrics WHERE name LIKE 'Survival rate%'
+DELETE FROM funder.published_report_project_metrics WHERE project_metric_id = (
+    SELECT id FROM accelerator.project_metrics WHERE name = 'Survival rate (%) of planted mangroves'
 );
-DELETE FROM accelerator.project_metrics WHERE name LIKE 'Survival rate%';
+DELETE FROM accelerator.project_metrics WHERE name = 'Survival rate (%) of planted mangroves';
