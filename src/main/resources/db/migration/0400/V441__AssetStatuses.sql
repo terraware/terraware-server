95001dff6f0ba23920eeaaed6ca50a555fe131b1
Steven Grimm <steven@terraformation.com>
2026-01-22 08:48:50 -0800

    Rename constraints too

--- src/main/resources/db/migration/0400/V441__AssetStatuses.sql
+++ src/main/resources/db/migration/0400/V441__AssetStatuses.sql
@@ -1,2 +1,5 @@
 ALTER TABLE mux_asset_statuses RENAME TO asset_statuses;
+ALTER TABLE asset_statuses RENAME CONSTRAINT mux_asset_statuses_pkey TO asset_statuses_pkey;
+ALTER TABLE asset_statuses RENAME CONSTRAINT mux_asset_statuses_name_key TO asset_statuses_name_key;
+
 ALTER TABLE mux_assets RENAME COLUMN mux_asset_status_id TO asset_status_id;
