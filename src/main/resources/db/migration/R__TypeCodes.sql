6cb01c8a185e4d5a4e8049df8da97bab40232ea9
ice1080 <25037168+ice1080@users.noreply.github.com>
2026-03-11 16:23:48 +0000

    SW-8096 Add precision to indicators (#4120)
    We want to support decimals for some indicators, and display them based on (semi-)configurable precision. Add a precision column to the indicator tables. Most of the time we want these values to be integers, so make the default precision 0. Update Hectares Planted to have a precision of 2.
    
    Replaces PR #4050 (and cherry picked many of the changes from it).
    
    Co-authored-by: Tommy Lau <8563294+tommylau523@users.noreply.github.com>

--- src/main/resources/db/migration/R__TypeCodes.sql
+++ src/main/resources/db/migration/R__TypeCodes.sql
@@ -544,13 +544,13 @@ VALUES (1, 'Carbon'),
        (12, 'Values Alignment')
 ON CONFLICT (id) DO UPDATE SET name = excluded.name;
 
-INSERT INTO accelerator.auto_calculated_indicators (id, name, description, class_id, level_id, category_id, ref_id, is_publishable, unit)
-VALUES (1, 'Seeds Collected', 'Total seed count checked-into accessions.', 1, 2, 2, '1.1', false, 'Seeds'),
-       (2, 'Seedlings', 'Plants in the nursery, including those provided by partners, where available. Not applicable for mangrove projects (input 0).', 1, 2, 2, '1.2', true, 'Seedlings'),
-       (3, 'Trees Planted', 'Total trees (and plants) planted in the field.', 1, 2, 2, '1.3', true, 'Trees'),
-       (4, 'Species Planted', 'Total species of the plants/trees planted.', 2, 2, 2, '1.4', true, 'Species'),
-       (6, 'Hectares Planted', 'This is the hectares marked as “Planting Complete” within the Project Area.', 1, 2, 2, '1.1.1.1', true, 'Hectares'),
-       (7, 'Survival Rate', 'Survival rate of plantings.', 2, 3, 2, '2', true, '%')
+INSERT INTO accelerator.auto_calculated_indicators (id, name, description, class_id, level_id, category_id, ref_id, is_publishable, unit, precision)
+VALUES (1, 'Seeds Collected', 'Total seed count checked-into accessions.', 1, 2, 2, '1.1', false, 'Seeds', 0),
+       (2, 'Seedlings', 'Plants in the nursery, including those provided by partners, where available. Not applicable for mangrove projects (input 0).', 1, 2, 2, '1.2', true, 'Seedlings', 0),
+       (3, 'Trees Planted', 'Total trees (and plants) planted in the field.', 1, 2, 2, '1.3', true, 'Trees', 0),
+       (4, 'Species Planted', 'Total species of the plants/trees planted.', 2, 2, 2, '1.4', true, 'Species', 0),
+       (6, 'Hectares Planted', 'This is the hectares marked as “Planting Complete” within the Project Area.', 1, 2, 2, '1.1.1.1', true, 'Hectares', 2),
+       (7, 'Survival Rate', 'Survival rate of plantings.', 2, 3, 2, '2', true, '%', 0)
 ON CONFLICT (id) DO UPDATE
 SET name = excluded.name,
     description = excluded.description,
@@ -559,7 +559,8 @@ SET name = excluded.name,
     category_id = excluded.category_id,
     ref_id = excluded.ref_id,
     is_publishable = excluded.is_publishable,
-    unit = excluded.unit;
+    unit = excluded.unit,
+    precision = excluded.precision;
 
 INSERT INTO seed_fund_report_statuses (id, name)
 VALUES (1, 'New'),
