02d582957d454a754edc3ee59a9b5e1c81e8285d
Steven Grimm <steven@terraformation.com>
2026-03-26 09:49:19 -0700

    Buildkite: Assume role in tier account before deploying to ECS

--- .buildkite/scripts/deploy-ecs.sh
+++ .buildkite/scripts/deploy-ecs.sh
@@ -6,10 +6,12 @@ case "$TIER" in
     PROD)
         ECS_CLUSTER=prod-terraware-cluster
         ECS_SERVICE=prod-terraware-backend
+        ASSUME_ROLE="${PROD_AWS_ROLE}"
         ;;
     STAGING)
         ECS_CLUSTER=staging-terraware-cluster
         ECS_SERVICE=staging-terraware-backend
+        ASSUME_ROLE="${STAGING_AWS_ROLE}"
         ;;
     *)
         echo "--- :amazon-ecs: No ECS deploy needed for ${TIER} builds."
@@ -18,6 +20,16 @@ case "$TIER" in
 esac
 
 .buildkite/scripts/install-deps.sh --tools
+
+echo "--- :aws: Assuming role in target tier"
+
+credentials=$(aws sts assume-role --role-arn "$ASSUME_ROLE" --role-session-name buildkite-deploy)
+AWS_ACCESS_KEY_ID=$(echo "$credentials" | jq -r '.Credentials.AccessKeyId');\
+AWS_SECRET_ACCESS_KEY=$(echo "$credentials" | jq -r '.Credentials.SecretAccessKey');\
+AWS_SESSION_TOKEN=$(echo "$credentials" | jq -r '.Credentials.SessionToken');
+export AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY
+export AWS_SESSION_TOKEN
 
 echo "--- :amazon-ecs: Deploying to ECS cluster=${ECS_CLUSTER} service=${ECS_SERVICE}"
 
