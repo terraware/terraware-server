e07b526b6c378e0bfb8ea69b4067a64c50e40dc7
Steven Grimm <1248649+sgrimm@users.noreply.github.com>
2026-03-27 11:35:40 -0700

    SW-8202 Improve cache usage between steps (#4202)
    Restore the node_modules directory in steps that run Gradle so the Yarn
    task isn't considered out of date.
    
    Don't mutate gradle.properties to set the Java home directory; Gradle will
    figure out the correct Java home on its own.
    
    Always use manifests for the Gradle home and node_modules cache entries
    so Buildkite can skip re-uploading them if they haven't changed.

--- .buildkite/scripts/install-deps.sh
+++ .buildkite/scripts/install-deps.sh
@@ -11,8 +11,7 @@ NODE_VERSION=22
 PYTHON_VERSION=3.13
 
 install_corretto() {
-    JAVA_HOME="/usr/lib/jvm/java-${JAVA_VERSION}-amazon-corretto"
-    if [ -f "$JAVA_HOME/bin/java" ]; then
+    if [ -f "/usr/lib/jvm/java-${JAVA_VERSION}-amazon-corretto/bin/java" ]; then
         return
     fi
 
@@ -20,9 +19,6 @@ install_corretto() {
     sudo rpm --import https://yum.corretto.aws/corretto.key
     sudo curl -sL -o /etc/yum.repos.d/corretto.repo https://yum.corretto.aws/corretto.repo
     sudo dnf install -y "java-${JAVA_VERSION}-amazon-corretto-devel"
-
-    echo "JAVA_HOME=${JAVA_HOME}" >> "$BUILDKITE_ENV_FILE"
-    echo "org.gradle.java.installations.paths=${JAVA_HOME}" >> gradle.properties
 }
 
 install_jq() {
