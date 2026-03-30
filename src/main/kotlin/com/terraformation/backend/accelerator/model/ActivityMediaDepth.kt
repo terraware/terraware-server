6cd74f158b1e53037f3f194a9dce758e5176bca4
tommylau523 <8563294+tommylau523@users.noreply.github.com>
2026-03-30 18:50:08 +0000

    Use depth in admin media API and remove serialized names (#4210)

--- src/main/kotlin/com/terraformation/backend/accelerator/model/ActivityMediaDepth.kt
+++ src/main/kotlin/com/terraformation/backend/accelerator/model/ActivityMediaDepth.kt
@@ -1,9 +1,7 @@
 package com.terraformation.backend.accelerator.model
 
-import com.fasterxml.jackson.annotation.JsonValue
-
-enum class ActivityMediaDepth(@get:JsonValue val jsonValue: String) {
-  None("None"),
-  CoverPhotos("Cover Photos"),
-  All("All"),
+enum class ActivityMediaDepth {
+  None,
+  CoverPhotos,
+  All,
 }
