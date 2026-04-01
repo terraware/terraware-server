cbb10168f3df937b7e59171498ce954dbda758bf
Steven Grimm <steven@terraformation.com>
2026-04-01 09:19:23 -0700

    Don't cache venv, just pip downloads

--- .buildkite/scripts/check-scripts.sh
+++ .buildkite/scripts/check-scripts.sh
@@ -10,26 +10,16 @@ echo "--- :python: Install packages"
 
 cd scripts
 
+PIP_CACHE="$(pwd)/.pip-cache"
 VENV_DIR="$(pwd)/.venv"
 
-if [ -f "${VENV_DIR}/bin/python" ]; then
-    venv_python_version=$("${VENV_DIR}/bin/python" -V | cut -d' ' -f2 | cut -d. -f1-2)
-
-    if [ "$venv_python_version" != "$PYTHON_VERSION" ]; then
-        echo "venv Python version is ${venv_python_version}; want ${PYTHON_VERSION}"
-        rm -rf "$VENV_DIR"
-    fi
-fi
-
-if [ ! -d "$VENV_DIR" ]; then
-    echo "Creating venv with $(python$PYTHON_VERSION -V)"
-    python$PYTHON_VERSION -m venv "$VENV_DIR"
-fi
+echo "Creating venv with $(python$PYTHON_VERSION -V)"
+python$PYTHON_VERSION -m venv "$VENV_DIR"
 
 PATH="${VENV_DIR}/bin:$PATH"
 export PATH
 
-pip3 install -r requirements.txt
+PIP_CACHE="$PIP_CACHE" pip3 install -r requirements.txt
 
 echo "--- :python: Check formatting"
 
