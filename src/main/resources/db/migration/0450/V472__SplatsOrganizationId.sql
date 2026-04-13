c8ac2855bcfe75694d6367a84322e29ed8302551
Steven Grimm <steven@terraformation.com>
2026-04-13 13:45:37 -0700

    SW-8274 Associate splats with organizations
    To support efficiently querying an organization's list of virtual walkthroughs
    once we start supporting non-observation-based walkthroughs, add an organization
    ID column to the splats table and populate it at splat creation time.

---
+++ src/main/resources/db/migration/0450/V472__SplatsOrganizationId.sql
@@ -0,0 +1,13 @@
+ALTER TABLE splats ADD COLUMN organization_id BIGINT REFERENCES organizations;
+
+CREATE INDEX ON splats (organization_id);
+
+UPDATE splats s
+SET organization_id = (
+    SELECT mp.organization_id
+    FROM tracking.observation_media_files omf
+    JOIN tracking.monitoring_plots mp ON omf.monitoring_plot_id = mp.id
+    WHERE omf.file_id = s.file_id
+);
+
+ALTER TABLE splats ALTER COLUMN organization_id SET NOT NULL;
