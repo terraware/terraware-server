b8f23357ffab9e2643f8915266188482c8983eed
Steven Grimm <steven@terraformation.com>
2026-04-13 15:42:42 -0700

    Delete splats rows whose media files have already been deleted

--- src/main/resources/db/migration/0450/V472__SplatsOrganizationId.sql
+++ src/main/resources/db/migration/0450/V472__SplatsOrganizationId.sql
@@ -2,6 +2,13 @@ ALTER TABLE splats ADD COLUMN organization_id BIGINT REFERENCES organizations;
 
 CREATE INDEX ON splats (organization_id);
 
+DELETE FROM splats s
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM observation_media_files omf
+    WHERE omf.file_id = s.file_id
+);
+
 UPDATE splats s
 SET organization_id = (
     SELECT mp.organization_id
