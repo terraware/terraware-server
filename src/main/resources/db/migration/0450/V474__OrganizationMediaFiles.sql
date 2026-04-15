47a6e1fed483af6d984bb159f06968dc0722a56f
Steven Grimm <steven@terraformation.com>
2026-04-15 08:59:19 -0700

    Don't do cascading delete on organization

--- src/main/resources/db/migration/0450/V474__OrganizationMediaFiles.sql
+++ src/main/resources/db/migration/0450/V474__OrganizationMediaFiles.sql
@@ -1,6 +1,6 @@
 CREATE TABLE organization_media_files (
     file_id BIGINT PRIMARY KEY REFERENCES files ON DELETE CASCADE,
-    organization_id BIGINT NOT NULL REFERENCES organizations ON DELETE CASCADE,
+    organization_id BIGINT NOT NULL REFERENCES organizations,
     caption TEXT
 );
 
