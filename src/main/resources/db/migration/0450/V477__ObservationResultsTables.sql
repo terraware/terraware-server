6eb0f0f65179de75e6adcf2d84a65e8b4354f591
Tommy Lau <8563294+tommylau@users.noreply.github.com>
2026-04-29 13:11:10 -0700

    Added history columns

--- src/main/resources/db/migration/0450/V477__ObservationResultsTables.sql
+++ src/main/resources/db/migration/0450/V477__ObservationResultsTables.sql
@@ -1,6 +1,7 @@
 CREATE TABLE tracking.observation_plot_results(
     observation_id BIGINT NOT NULL REFERENCES tracking.observations ON DELETE CASCADE,
     monitoring_plot_id BIGINT NOT NULL REFERENCES tracking.monitoring_plots ON DELETE CASCADE,
+    monitoring_plot_history_id BIGINT NOT NULL REFERENCES tracking.monitoring_plot_histories ON DELETE CASCADE,
     total_live INT NOT NULL,
     total_dead INT NOT NULL,
     total_existing INT NOT NULL,
@@ -11,10 +12,13 @@ CREATE TABLE tracking.observation_plot_results(
     PRIMARY KEY (observation_id, monitoring_plot_id),
     FOREIGN KEY (observation_id, monitoring_plot_id) REFERENCES tracking.observation_plots ON DELETE CASCADE
 );
+CREATE INDEX ON tracking.observation_plot_results(monitoring_plot_history_id);
+CREATE INDEX ON tracking.observation_plot_results(observation_id, monitoring_plot_history_id);
 
 CREATE TABLE tracking.observation_substratum_results(
     observation_id BIGINT NOT NULL REFERENCES tracking.observations ON DELETE CASCADE,
     substratum_id BIGINT NOT NULL REFERENCES tracking.substrata ON DELETE CASCADE,
+    substratum_history_id BIGINT NOT NULL REFERENCES tracking.substratum_histories ON DELETE CASCADE,
     total_live INT NOT NULL,
     total_dead INT NOT NULL,
     total_existing INT NOT NULL,
@@ -26,10 +30,12 @@ CREATE TABLE tracking.observation_substratum_results(
 
     PRIMARY KEY (observation_id, substratum_id)
 );
+CREATE INDEX ON tracking.observation_substratum_results(substratum_id);
 
 CREATE TABLE tracking.observation_stratum_results(
     observation_id BIGINT NOT NULL REFERENCES tracking.observations ON DELETE CASCADE,
     stratum_id BIGINT NOT NULL REFERENCES tracking.strata ON DELETE CASCADE,
+    stratum_history_id BIGINT REFERENCES tracking.stratum_histories ON DELETE CASCADE,
     total_live INT NOT NULL,
     total_dead INT NOT NULL,
     total_existing INT NOT NULL,
@@ -41,6 +47,7 @@ CREATE TABLE tracking.observation_stratum_results(
 
     PRIMARY KEY (observation_id, stratum_id)
 );
+CREATE INDEX ON tracking.observation_stratum_results(stratum_id);
 
 CREATE TABLE tracking.observation_site_results(
     observation_id BIGINT NOT NULL REFERENCES tracking.observations ON DELETE CASCADE,
