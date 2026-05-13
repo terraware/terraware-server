e9cbd7d5337a0b23e6b79517e454d0ad2584e642
tommylau523 <8563294+tommylau523@users.noreply.github.com>
2026-05-13 21:43:48 +0000

    Added search tables for withdrawal photos (#4380)

---
+++ src/main/kotlin/com/terraformation/backend/search/field/LocalDateTimeField.kt
@@ -0,0 +1,67 @@
+package com.terraformation.backend.search.field
+
+import com.terraformation.backend.search.FieldNode
+import com.terraformation.backend.search.SearchFilterType
+import com.terraformation.backend.search.SearchTable
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
+import java.util.EnumSet
+import org.jooq.Condition
+import org.jooq.Record
+import org.jooq.TableField
+import org.jooq.impl.DSL
+
+/** Search field for columns that have local date time. */
+class LocalDateTimeField(
+    override val fieldName: String,
+    override val databaseField: TableField<*, LocalDateTime?>,
+    override val table: SearchTable,
+) : SingleColumnSearchField<LocalDateTime>() {
+  override val localize: Boolean
+    get() = false
+
+  override val supportedFilterTypes: Set<SearchFilterType>
+    get() = EnumSet.of(SearchFilterType.Exact, SearchFilterType.Range)
+
+  override fun getCondition(fieldNode: FieldNode): Condition {
+    val dateTimeValues =
+        try {
+          fieldNode.values.map { if (it != null) LocalDateTime.parse(it) else null }
+        } catch (e: DateTimeParseException) {
+          throw IllegalArgumentException(
+              "Timestamps must be in ISO-8601 format (example: 2021-05-28T18:45:30)"
+          )
+        }
+    val nonNullLocalDateTime = dateTimeValues.filterNotNull()
+
+    return when (fieldNode.type) {
+      SearchFilterType.Exact ->
+          DSL.or(
+              listOfNotNull(
+                  if (nonNullLocalDateTime.isNotEmpty()) databaseField.`in`(nonNullLocalDateTime)
+                  else null,
+                  if (fieldNode.values.any { it == null }) databaseField.isNull else null,
+              )
+          )
+      SearchFilterType.Partial ->
+          throw IllegalArgumentException("Partial search not supported for timestamps")
+      SearchFilterType.PartialOrFuzzy,
+      SearchFilterType.Fuzzy ->
+          throw IllegalArgumentException("Fuzzy search not supported for timestamps")
+      SearchFilterType.PhraseMatch ->
+          throw IllegalArgumentException("Phrase match not supported for timestamps")
+      SearchFilterType.Range -> rangeCondition(dateTimeValues)
+    }
+  }
+
+  override fun computeValue(record: Record): String? =
+      record.get(databaseField)?.format(OUTPUT_FORMATTER)
+
+  // Timestamp values are always machine-readable.
+  override fun raw(): SearchField? = null
+
+  companion object {
+    private val OUTPUT_FORMATTER: DateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+  }
+}
---
+++ src/main/kotlin/com/terraformation/backend/search/table/NurseryWithdrawalPhotosTable.kt
@@ -0,0 +1,43 @@
+package com.terraformation.backend.search.table
+
+import com.terraformation.backend.db.default_schema.FileId
+import com.terraformation.backend.db.nursery.tables.references.WITHDRAWAL_PHOTOS
+import com.terraformation.backend.db.nursery.tables.references.WITHDRAWAL_SUMMARIES
+import com.terraformation.backend.search.SearchTable
+import com.terraformation.backend.search.SublistField
+import com.terraformation.backend.search.field.SearchField
+import org.jooq.Record
+import org.jooq.SelectJoinStep
+import org.jooq.TableField
+
+class NurseryWithdrawalPhotosTable(private val tables: SearchTables) : SearchTable() {
+  override val primaryKey: TableField<out Record, out Any?>
+    get() = WITHDRAWAL_PHOTOS.FILE_ID
+
+  override val sublists: List<SublistField> by lazy {
+    with(tables) {
+      listOf(
+          nurseryWithdrawals.asSingleValueSublist(
+              "withdrawal",
+              WITHDRAWAL_PHOTOS.WITHDRAWAL_ID.eq(WITHDRAWAL_SUMMARIES.ID),
+          ),
+      )
+    }
+  }
+
+  override val fields: List<SearchField> =
+      listOf(
+          idWrapperField("fileId", WITHDRAWAL_PHOTOS.FILE_ID) { FileId(it) },
+          geometryField("gpsCoordinate", WITHDRAWAL_PHOTOS.files.GEOLOCATION),
+          localDateTimeField("capturedLocalTime", WITHDRAWAL_PHOTOS.files.CAPTURED_LOCAL_TIME),
+      )
+
+  override val inheritsVisibilityFrom: SearchTable
+    get() = tables.nurseryWithdrawals
+
+  override fun <T : Record> joinForVisibility(query: SelectJoinStep<T>): SelectJoinStep<T> {
+    return query
+        .join(WITHDRAWAL_SUMMARIES)
+        .on(WITHDRAWAL_SUMMARIES.ID.eq(WITHDRAWAL_PHOTOS.WITHDRAWAL_ID))
+  }
+}
