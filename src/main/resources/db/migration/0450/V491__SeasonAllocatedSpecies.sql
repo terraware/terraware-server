1a3edf3de7848f24e31b9ea96a43010219993d55
Isaac Hudson <isaac.hudson@terraformation.com>
2026-06-01 11:11:00 -0700

    add created/modified columns

--- src/main/resources/db/migration/0450/V491__SeasonAllocatedSpecies.sql
+++ src/main/resources/db/migration/0450/V491__SeasonAllocatedSpecies.sql
@@ -2,6 +2,10 @@ CREATE TABLE tracking.planting_season_allocated_species (
     planting_season_id BIGINT NOT NULL REFERENCES tracking.planting_seasons ON DELETE CASCADE,
     species_id BIGINT NOT NULL REFERENCES species ON DELETE CASCADE,
     quantity INTEGER NOT NULL CHECK (quantity > 0),
+    created_by BIGINT NOT NULL REFERENCES users,
+    created_time TIMESTAMP WITH TIME ZONE NOT NULL,
+    modified_by BIGINT NOT NULL REFERENCES users,
+    modified_time TIMESTAMP WITH TIME ZONE NOT NULL,
 
     PRIMARY KEY (planting_season_id, species_id)
 );
