a15c6e8494e9181c3b4130f9783a10ab2260a4c3
Steven Grimm <1248649+sgrimm@users.noreply.github.com>
2026-06-02 16:11:29 -0700

    SW-8427 Persistent events for scheduled planting dates (#4523)
    Add a set of persistent event classes for operations on scheduled planting dates.

--- src/main/kotlin/com/terraformation/backend/eventlog/EventLogPayloadTransformer.kt
+++ src/main/kotlin/com/terraformation/backend/eventlog/EventLogPayloadTransformer.kt
@@ -18,6 +18,7 @@ import com.terraformation.backend.eventlog.api.MonitoringSpeciesSubjectPayload
 import com.terraformation.backend.eventlog.api.ObservationPlotMediaSubjectPayload
 import com.terraformation.backend.eventlog.api.ObservationPlotSubjectPayload
 import com.terraformation.backend.eventlog.api.OrganizationSubjectPayload
+import com.terraformation.backend.eventlog.api.PlantingSeasonScheduledDateSubjectPayload
 import com.terraformation.backend.eventlog.api.PlantingSeasonSubjectPayload
 import com.terraformation.backend.eventlog.api.ProjectSubjectPayload
 import com.terraformation.backend.eventlog.api.RecordedTreeSubjectPayload
@@ -26,6 +27,7 @@ import com.terraformation.backend.eventlog.model.EventLogEntry
 import com.terraformation.backend.i18n.Messages
 import com.terraformation.backend.log.perClassLogger
 import com.terraformation.backend.plantingmanagement.event.PlantingSeasonPersistentEvent
+import com.terraformation.backend.plantingmanagement.event.PlantingSeasonScheduledDatePersistentEvent
 import com.terraformation.backend.tracking.event.BiomassDetailsPersistentEvent
 import com.terraformation.backend.tracking.event.BiomassQuadratPersistentEvent
 import com.terraformation.backend.tracking.event.BiomassQuadratSpeciesPersistentEvent
@@ -100,6 +102,8 @@ class EventLogPayloadTransformer(
       is ObservationPlotPersistentEvent -> ObservationPlotSubjectPayload.forEvent(event, context)
       is OrganizationPersistentEvent -> OrganizationSubjectPayload.forEvent(event, context)
       is PlantingSeasonPersistentEvent -> PlantingSeasonSubjectPayload.forEvent(event, context)
+      is PlantingSeasonScheduledDatePersistentEvent ->
+          PlantingSeasonScheduledDateSubjectPayload.forEvent(event, context)
       is ProjectPersistentEvent -> ProjectSubjectPayload.forEvent(event, context)
       is RecordedTreePersistentEvent -> RecordedTreeSubjectPayload.forEvent(event, context)
       else -> {
