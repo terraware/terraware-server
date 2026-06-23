b8e82f89a7f5c4eb3ea400b68b2452942e9cf0c7
Isaac Hudson <isaac.hudson@terraformation.com>
2026-06-23 10:22:09 -0700

    change delete to post

--- src/main/kotlin/com/terraformation/backend/plantingmanagement/api/PlantingSeasonNotificationsController.kt
+++ src/main/kotlin/com/terraformation/backend/plantingmanagement/api/PlantingSeasonNotificationsController.kt
@@ -14,8 +14,8 @@ import com.terraformation.backend.plantingmanagement.PlantingSeasonNotificationT
 import com.terraformation.backend.plantingmanagement.db.PlantingSeasonNotificationsService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Schema
-import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -65,7 +65,7 @@ class PlantingSeasonNotificationsController(
               "specified event log id. The event log id should be the lastEventLogId returned by " +
               "the notifications listing endpoint.",
   )
-  @DeleteMapping("/notifications")
+  @PostMapping("/notifications")
   fun dismissPlantingSeasonNotifications(
       @RequestBody payload: DismissPlantingSeasonNotificationsRequestPayload,
   ): SimpleSuccessResponsePayload {
