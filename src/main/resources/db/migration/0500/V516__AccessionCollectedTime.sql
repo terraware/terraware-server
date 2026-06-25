6da3ca3f9f572a376730e16259382b5b9116d075
Isaac Hudson <isaac.hudson@terraformation.com>
2026-06-25 08:29:02 -0700

    Prefer user or org timezone over UTC

--- src/main/resources/db/migration/0500/V516__AccessionCollectedTime.sql
+++ src/main/resources/db/migration/0500/V516__AccessionCollectedTime.sql
@@ -1,5 +1,16 @@
 ALTER TABLE seedbank.accessions
     ADD COLUMN collected_time TIMESTAMP WITH TIME ZONE;
 
-UPDATE seedbank.accessions
-    SET collected_time = collected_date::TIMESTAMP AT TIME ZONE 'UTC';
+WITH tz_data AS (
+    SELECT
+        a.id,
+        COALESCE(u.time_zone, o.time_zone, 'UTC') AS time_zone
+    FROM seedbank.accessions a
+    JOIN users u ON u.id = a.created_by
+    LEFT JOIN facilities f ON f.id = a.facility_id
+    LEFT JOIN organizations o ON o.id = f.organization_id
+)
+UPDATE seedbank.accessions a
+    SET collected_time = a.collected_date::TIMESTAMP AT TIME ZONE tz_data.time_zone
+    FROM tz_data
+    WHERE a.id = tz_data.id;
