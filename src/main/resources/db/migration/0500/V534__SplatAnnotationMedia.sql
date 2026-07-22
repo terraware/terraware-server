2272e9750c550dc4213c6169a9c5a061c247722a
Isaac Hudson <isaac.hudson@terraformation.com>
2026-07-22 09:38:32 +0200

    greater than 0 check

--- src/main/resources/db/migration/0500/V534__SplatAnnotationMedia.sql
+++ src/main/resources/db/migration/0500/V534__SplatAnnotationMedia.sql
@@ -1,7 +1,7 @@
 CREATE TABLE splat_annotation_media (
     file_id BIGINT PRIMARY KEY REFERENCES files,
     splat_annotation_id BIGINT NOT NULL REFERENCES splat_annotations ON DELETE CASCADE,
-    position INTEGER NOT NULL,
+    position INTEGER NOT NULL CHECK (position >= 0),
     created_by BIGINT NOT NULL REFERENCES users,
     created_time TIMESTAMP WITH TIME ZONE NOT NULL,
 
