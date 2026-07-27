3a3c67a399d5dc3202731162feb7a590c0360e09
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2026-07-27 11:10:28 -0700

    SW-7604 Withdrawal updated event log (#4794)

--- src/main/kotlin/com/terraformation/backend/seedbank/event/WithdrawalPersistentEvent.kt
+++ src/main/kotlin/com/terraformation/backend/seedbank/event/WithdrawalPersistentEvent.kt
@@ -2,12 +2,16 @@ package com.terraformation.backend.seedbank.event
 
 import com.terraformation.backend.db.default_schema.FacilityId
 import com.terraformation.backend.db.default_schema.OrganizationId
+import com.terraformation.backend.db.default_schema.UserId
 import com.terraformation.backend.db.nursery.BatchId
 import com.terraformation.backend.db.seedbank.AccessionId
 import com.terraformation.backend.db.seedbank.WithdrawalId
 import com.terraformation.backend.db.seedbank.WithdrawalPurpose
 import com.terraformation.backend.eventlog.EntityCreatedPersistentEvent
+import com.terraformation.backend.eventlog.FieldsUpdatedPersistentEvent
 import com.terraformation.backend.eventlog.PersistentEvent
+import com.terraformation.backend.i18n.Messages
+import com.terraformation.backend.i18n.currentLocale
 import com.terraformation.backend.seedbank.model.SeedQuantityModel
 import java.time.LocalDate
 
@@ -33,3 +37,54 @@ data class WithdrawalCreatedEventV1(
 ) : EntityCreatedPersistentEvent, WithdrawalPersistentEvent
 
 typealias WithdrawalCreatedEvent = WithdrawalCreatedEventV1
+
+/** Published when the user edits an existing withdrawal. */
+data class WithdrawalUpdatedEventV1(
+    val changedFrom: Values,
+    val changedTo: Values,
+    override val withdrawalId: WithdrawalId,
+    override val accessionId: AccessionId,
+    override val facilityId: FacilityId,
+    override val organizationId: OrganizationId,
+) : FieldsUpdatedPersistentEvent, WithdrawalPersistentEvent {
+  data class Values(
+      val date: LocalDate? = null,
+      val withdrawnQuantity: SeedQuantityModel? = null,
+      val purpose: WithdrawalPurpose? = null,
+      val destination: String? = null,
+      val notes: String? = null,
+      val staffResponsible: String? = null,
+      val withdrawnByUserId: UserId? = null,
+  )
+
+  override fun listUpdatedFields(messages: Messages) =
+      listOfNotNull(
+          createUpdatedField("date", changedFrom.date?.toString(), changedTo.date?.toString()),
+          createUpdatedField(
+              "withdrawnQuantity",
+              changedFrom.withdrawnQuantity?.toString(),
+              changedTo.withdrawnQuantity?.toString(),
+          ),
+          createUpdatedField(
+              "purpose",
+              changedFrom.purpose?.getDisplayName(currentLocale()),
+              changedTo.purpose?.getDisplayName(currentLocale()),
+          ),
+          createUpdatedField("destination", changedFrom.destination, changedTo.destination),
+          createUpdatedField("notes", changedFrom.notes, changedTo.notes),
+          createUpdatedField(
+              "staffResponsible",
+              changedFrom.staffResponsible,
+              changedTo.staffResponsible,
+          ),
+          createUpdatedField(
+              "withdrawnByUserId",
+              changedFrom.withdrawnByUserId?.toString(),
+              changedTo.withdrawnByUserId?.toString(),
+          ),
+      )
+}
+
+typealias WithdrawalUpdatedEvent = WithdrawalUpdatedEventV1
+
+typealias WithdrawalUpdatedEventValues = WithdrawalUpdatedEventV1.Values
