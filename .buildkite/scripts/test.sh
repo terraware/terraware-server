76877bfbcc7473590438f02dc2d7ffba51b24635
Steven Grimm <steven@terraformation.com>
2026-09-10 09:05:23 -0700

    Move OpenAPI comparison to its own Buildkite step
    Move the "generate OpenAPI schema to test that the server can start up,
    and compare the result against staging" part of the CI pipeline to its
    own pipeline step and put it after the JUnit test runs, not before.
    When a CI run fails, it's usually because of a failing test, so this
    will give us faster failures when there's a problem.

--- .buildkite/scripts/test.sh
+++ .buildkite/scripts/test.sh
@@ -3,24 +3,6 @@ set -euo pipefail
 
 .buildkite/scripts/install-deps.sh --java --tools --node
 
-echo "--- :openapi: Generate OpenAPI docs to test that server can start up"
-./gradlew generateOpenApiDocs
-
-echo "--- :openapi: Diff OpenAPI docs against staging"
-if curl -f -s https://staging.terraware.io/v3/api-docs.yaml > staging.yaml; then
-  for f in openapi.yaml staging.yaml; do
-    yq -i '
-      .info.version = null |
-      .servers[0].url = null |
-      .components.securitySchemes.openId.openIdConnectUrl = null' "$f"
-  done
-
-  # Indent the diff output so the "---" in the diff header isn't treated as a section header.
-  diff -u staging.yaml openapi.yaml | sed 's/^/ /' || true
-else
-  echo "Unable to fetch OpenAPI schema from staging"
-fi
-
 echo "--- :junit: Run tests"
 
 # Suppress some debug log messages that add up to megabytes of test output and slow down annotating
